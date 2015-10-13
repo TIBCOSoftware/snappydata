7210672e1e08efa48768021bf6a4f1fc2d706583
Hemant Bhanawat <hbhanawat@pivotal.io>
2015-10-13 15:04:56 +0530

    Added callback classes and functions to call snappy code from gemxd. This is inspired from GfxdObjectFactoriesProvider.

---
+++ snappy-tools/src/main/scala/io/snappydata/gemxd/ClusterCallbacksImpl.scala
@@ -0,0 +1,19 @@
+package io.snappydata.gemxd
+
+import com.pivotal.gemfirexd.internal.snappy.ClusterCallbacks
+
+/**
+ * Callbacks that are sent by GemXD to Snappy for cluster management
+ *
+ * Created by hemantb on 10/12/15.
+ */
+object ClusterCallbacksImpl extends ClusterCallbacks {
+
+  def launchExecutor = {
+
+  }
+
+  def killExecutor ={
+
+  }
+}
