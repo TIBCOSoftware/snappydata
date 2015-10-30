7ccc21ec3b48a4ab565244b345c84eef830f63b6
ymahajan <ymahajan@pivotal.io>
2015-10-30 12:31:32 +0530

    Added Twitter Kafka Streaming quickstart

---
+++ snappy-tools/src/test/scala/io/snappydata/app/twitter/KafkaProducer.scala
@@ -0,0 +1,43 @@
+package io.snappydata.app.twitter
+
+import java.util.Properties
+
+import kafka.javaapi.producer.Producer
+import kafka.producer.{KeyedMessage, ProducerConfig}
+import io.snappydata.app.twitter.TwitterStream.OnTweetPosted
+import twitter4j.{Status, FilterQuery}
+//import twitter4j.
+/**
+ * Created by ymahajan on 28/10/15.
+ */
+
+object KafkaProducer {
+
+
+  val KafkaTopic = "tweets"
+
+  val kafkaProducer = {
+    val props = new Properties()
+    props.put("metadata.broker.list", "localhost:9092")//conf.getString("kafka.brokers"))
+    props.put("request.required.acks", "1")
+    val config = new ProducerConfig(props)
+    new Producer[String,  Array[Byte]](config)
+  }
+
+  val filterUsOnly = new FilterQuery().locations(Array(
+    Array(-126.562500,30.448674),
+    Array(-61.171875,44.087585)))
+
+
+  def main (args: Array[String]) {
+    val twitterStream = TwitterStream.getStream
+    twitterStream.addListener(new OnTweetPosted(s => sendToKafka(s)))
+    twitterStream.filter(filterUsOnly)
+  }
+
+  private def sendToKafka(s: Status) {
+    val msg = new KeyedMessage[String, Array[Byte]](KafkaTopic, s.toString.getBytes)//TwitterObjectFactory.getRawJSON(s))
+    kafkaProducer.send(msg)
+  }
+
+}
