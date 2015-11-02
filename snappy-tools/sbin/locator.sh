cd0c9fd7b03826efb35dd304e193dc04d8e79d53
Hemant Bhanawat <hbhanawat@pivotal.io>
2015-11-02 17:09:26 +0530

    Added first draft of scripts to start locators, leads and servers on multiple machines. These are based on spark scripts 1. start-snappy-all starts locators leads and servers 2. locators.sh start/stops/queries status of locator on all the nodes that are specified in conf/locators file 3. servers.sh start/stops/queries status of servers on all the nodes that are specified in conf/servers file 4. leads.sh start/stops/queries status of leads on all the nodes that are specified in conf/leads file
    Both snappy-env.sh and spark-env.sh will be read for environment settings.
    Both snappy-config.sh abd spark-config.sh will be read for config settings.
    
    There is an IllegalAccessError thrown while calling functions in parent class which is in java from scala. Added a overriden function in GfxdLauncherOverrides.scala to overcome this error.
    
    Updated the build-script to copy the script files.

---
+++ snappy-tools/sbin/locator.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Starts a locator on the machine this script is executed on.
+#
+
+usage="Usage: locator.sh"
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+mode=$1
+shift
+
+. "$sbin/snappy-config.sh"
+. "$sbin/spark-config.sh"
+
+. "$SPARK_PREFIX/bin/load-snappy-env.sh"
+. "$SPARK_PREFIX/bin/load-spark-env.sh"
+
+
+# Start up  the locator instance
+function start_instance {
+  echo "Locator instance : " "$mode"
+  "$SPARK_PREFIX"/bin/snappy locator "$mode" "$@"
+}
+
+start_instance "$@"
+
