aeb07dfc58f8406e3638e785fd4536ef030f6a66
Hemant Bhanawat <hbhanawat@pivotal.io>
2015-11-07 16:31:05 +0530

    Used master url from the SparkContext.

--- snappy-core/src/main/scala/org/apache/spark/scheduler/cluster/SnappyEmbeddedModeClusterManager.scala
+++ snappy-core/src/main/scala/org/apache/spark/scheduler/cluster/SnappyEmbeddedModeClusterManager.scala
@@ -12,24 +12,18 @@ import org.apache.spark.scheduler.{SchedulerBackend, TaskSchedulerImpl, TaskSche
 object SnappyEmbeddedModeClusterManager extends ExternalClusterManager {
 
   var schedulerBackend: Option[SnappyCoarseGrainedSchedulerBackend] = None
-  var masterUrl : String = _
   def createTaskScheduler(sc: SparkContext): TaskScheduler = {
     // If there is an application that is trying to join snappy
     // as lead in embedded mode, we need the locator to connect
     // to the snappy distributed system and hence the locator is
     // passed in masterurl itself.
-    if (masterUrl.startsWith("snappydata://"))
-      sc.conf.set("snappydata.store.locators", masterUrl.replaceFirst("snappydata://", ""))
+    if (sc.master.startsWith("snappydata://"))
+      sc.conf.set("snappydata.store.locators", sc.master.replaceFirst("snappydata://", ""))
     new TaskSchedulerImpl(sc)
   }
 
   def canCreate(masterURL: String): Boolean =
-    if (masterURL.startsWith("snappydata")) {
-      masterUrl = masterURL
-      true
-    }
-    else
-      false
+    if (masterURL.startsWith("snappydata")) true else false
 
   def createSchedulerBackend(sc: SparkContext,
       scheduler: TaskScheduler): SchedulerBackend = {
