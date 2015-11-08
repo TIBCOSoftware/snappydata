5fab7c29e7de79f8d3bd860d91e2553081546b0d
rmishra <rmishra@pivotal.io>
2015-11-08 15:54:03 +0530

    Added comments

--- snappy-core/src/main/scala/org/apache/spark/sql/store/StoreProperties.scala
+++ snappy-core/src/main/scala/org/apache/spark/sql/store/StoreProperties.scala
@@ -14,7 +14,7 @@ object StoreProperties {
   def defaultStoreURL(sc : SparkContext): String = {
 
    if (sc.master.startsWith("snappydata") || sc.conf.contains("snappydata.store.locators")) {
-     DEFAULT_EMBEDDED_URL // Embedded mode. Already connected to
+     DEFAULT_EMBEDDED_URL // Embedded mode. Already connected to Snappydata in embedded , nonembedded and local mode
     } else {
      sys.error("Option 'url' not specified")
     }
