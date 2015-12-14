de338da4c5fbfee2793b80c145e5528dc5e30912
Shirish Deshmukh <sdeshmukh@pivotal.io>
2015-12-14 17:09:41 +0530

    Creating a common to all SQLCLob object in SparkSQLExecuteImpl and calling SQLClob.toDataForOptimizedResultHolder for clob data. Earlier data was written using DataSerializer.writeString on the lead node but read using SQLChar.fromDataForOptimizedResultHolder on XD node in case of query routing causing UTFDataFormatException

--- snappy-tools/src/main/scala/io/snappydata/gemxd/SparkSQLExecuteImpl.scala
+++ snappy-tools/src/main/scala/io/snappydata/gemxd/SparkSQLExecuteImpl.scala
@@ -8,6 +8,7 @@ import com.gemstone.gemfire.internal.{HeapDataOutputStream, InternalDataSerializ
 import com.pivotal.gemfirexd.internal.engine.Misc
 import com.pivotal.gemfirexd.internal.engine.distributed.utils.GemFireXDUtils
 import com.pivotal.gemfirexd.internal.engine.distributed.{ActiveColumnBits, GfxdHeapDataOutputStream}
+import com.pivotal.gemfirexd.internal.iapi.types.SQLClob
 import com.pivotal.gemfirexd.internal.shared.common.StoredFormatIds
 import com.pivotal.gemfirexd.internal.snappy.{LeadNodeExecutionContext, SparkSQLExecute}
 
@@ -40,6 +41,8 @@ class SparkSQLExecuteImpl(val sql: String,
   private var rowsSent = 0
 
   private lazy val totalRows = rows.length
+
+  private lazy val clobColData = new SQLClob
 
   // using the gemfirexd way of sending results where in the number of
   // columns in each row is divided into sets of 8 columns. Per eight column group a
@@ -184,7 +187,11 @@ class SparkSQLExecuteImpl(val sql: String,
       case t: DecimalType => DataSerializer.writeObject(row.getDecimal(colIndex), hdos)
       case FloatType => hdos.writeFloat(row.getFloat(colIndex))
       case DoubleType => hdos.writeDouble(row.getDouble(colIndex))
-      case StringType => DataSerializer.writeString(row.getString(colIndex), hdos)
+      case StringType => {
+        clobColData.setValue(row.getString(colIndex))
+        clobColData.toDataForOptimizedResultHolder(hdos)
+        clobColData.restoreToNull
+      }
       // TODO: KN add varchar when that data type is identified
       // case VarCharType => StoredFormatIds.SQL_VARCHAR_ID
     }
