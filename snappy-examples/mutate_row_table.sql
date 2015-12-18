9dd849fe9bea270cc411ba411bc4afe4c2048db9
spillai <spillai@pivotal.io>
2015-12-18 16:45:48 +0530

    Corrected the query.

--- snappy-examples/mutate_row_table.sql
+++ snappy-examples/mutate_row_table.sql
@@ -1,2 +1,2 @@
 ----Suppose a particular Airline company say 'Jet Airways Limited' having CODE '9W' acquires 'Titan Airways' having CODE '02Q' ,then we need to update the snappy row table as below:----
- UPDATE AIRLINE_REF SET DESCRIPTION='Jet Airways Limited' WHERE CODE='02Q';
+UPDATE AIRLINEREF SET DESCRIPTION='Jet Airways Limited' WHERE CAST(CODE AS VARCHAR(25))='02Q';
