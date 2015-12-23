a6e0db97591211211afa1fbbf0d357aa1a99cdba
spillai <spillai@pivotal.io>
2015-12-23 16:28:01 +0530

    -Changed the Update query -Modified airline job and airline app.

--- snappy-examples/mutate_row_table.sql
+++
@@ -1,2 +0,0 @@
-----Suppose a particular Airline company say 'Jet Airways Limited' having CODE '9W' acquires 'Titan Airways' having CODE '02Q' ,then we need to update the snappy row table as below:----
-UPDATE AIRLINEREF SET DESCRIPTION='Jet Airways Limited' WHERE CAST(CODE AS VARCHAR(25))='02Q';
--- snappy-examples/quickstart/scripts/oltp_queries.sql
+++ snappy-examples/quickstart/scripts/oltp_queries.sql
@@ -1,20 +1,7 @@
-elapsedtime on;
+--- Suppose a particular Airline company say 'Delta Air Lines Inc.' re-brands itself as 'Delta America'
+--- the airline code can be updated in the row table
+UPDATE AIRLINEREF SET DESCRIPTION='Delta America' WHERE CAST(CODE AS VARCHAR(25))='DL';
 
----- Find Flights to SAN that has been delayed(Arrival/Dep)  5 or more times in a row ----
-SELECT FlightNum, COUNT(ArrDelay) as ARR_DEL
-  WHERE CAST(Dest AS VARCHAR(24)) = 'SAN'
-  FROM airline
-  GROUP BY FlightNum
-  HAVING COUNT(ArrDelay) >= 5;
+--- Query to see the updated result ---
+SELECT * FROM AIRLINEREF WHERE CAST(CODE AS VARCHAR(25))='DL';
 
----- Query to get Averag ARR_DELAY with Description from AIRLINE and AIRLINE_REF table.----
-SELECT AVG(ArrDelay) as AVGDELAY, count(*) as TOTALCNT, UniqueCarrier, airlineref.DESCRIPTION, Year_, Month_
-  FROM airline, airlineref 
-  WHERE airline.UniqueCarrier = airlineref.CODE 
-  GROUP BY UniqueCarrier, DESCRIPTION, Year_,Month_;
-
----- List the flights and its details ,that are affected due to delays caused by weather in a particular state ----
-SELECT FlightNum, airlineref.DESCRIPTION 
-  FROM airline, airlineref 
-  WHERE airline.UniqueCarrier = airlineref.CODE AND  Origin like '%SMF%' AND WeatherDelay > 0 
-  GROUP BY FlightNum ,DESCRIPTION;
