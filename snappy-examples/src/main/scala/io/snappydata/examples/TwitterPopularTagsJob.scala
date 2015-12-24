0094a3b6ba2a8bf76899b6d7e967a6a0421d8f61
Rajesh Kumar <rkumar@snappydata.io>
2015-12-24 16:44:20 +0530

    Replaced registertopk to createtopk and used new refactored savestream api.

--- snappy-examples/src/main/scala/io/snappydata/examples/TwitterPopularTagsJob.scala
+++ snappy-examples/src/main/scala/io/snappydata/examples/TwitterPopularTagsJob.scala
@@ -63,17 +63,15 @@ object TwitterPopularTagsJob extends SnappyStreamingJob {
 
 
     val topKOption = Map(
-        "key" -> "hashtag",
         "epoch" -> System.currentTimeMillis(),
         "timeInterval" -> 2000,
         "size" -> 10
       )
-    snsc.snappyContext.registerTopK("topktable", schema, topKOption, false)
+    snsc.snappyContext.createTopK("topktable", "hashtag", schema, topKOption, false)
 
     snsc.snappyContext.saveStream(rowStream,
       Seq("topktable"),
-      (rdd: RDD[Row], struct: StructType) => rdd,
-      schema
+      Some((rdd: RDD[Row]) => rdd)
     )
 
 
@@ -95,22 +93,24 @@ object TwitterPopularTagsJob extends SnappyStreamingJob {
       val end = System.currentTimeMillis + 90000
       while (end > System.currentTimeMillis()) {
         Thread.sleep(2000)
-        pw.println("********Top 10 hash tags for the last interval *******")
+        pw.println("********Top 10 hash tags for the last interval *******\n")
+
         snsc.snappyContext.queryTopK("topktable",System.currentTimeMillis - 10000, System.currentTimeMillis).collect.foreach(result => {
           pw.println(result.toString)
         })
 
-        pw.println("******* Top 10 hash tags until now *******")
+        pw.println("\n******* Top 10 hash tags until now *******")
 
         snsc.snappyContext.queryTopK("topktable",System.currentTimeMillis - 90000, System.currentTimeMillis).collect.foreach(result => {
           pw.println(result.toString)
         })
 
-        pw.println("******* Top 10 hash tags until now using gemxd query *******")
+        pw.println("\n******* Top 10 hash tags until now using gemxd query *******")
         snsc.snappyContext.sql(s"select hashtag, count(*) as tagcount from  ${tableName} group " +
           " by hashtag order by tagcount desc limit 10").collect.foreach(row => {
           pw.println(row.toString())
         })
+        pw.println("\n##############################################################")
       }
     } finally {
       pw.close()
