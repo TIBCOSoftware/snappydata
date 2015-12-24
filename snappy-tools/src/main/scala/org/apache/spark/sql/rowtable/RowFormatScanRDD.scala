ba4734c169f706e2cb4966eb85da483139da94c1
nthanvi <nthanvi@snappydata.io>
2015-12-24 12:26:59 +0530

    refactored RowFormatScanRDD so that iterator and  compute can be reutilized.

--- snappy-tools/src/main/scala/org/apache/spark/sql/rowtable/RowFormatScanRDD.scala
+++ snappy-tools/src/main/scala/org/apache/spark/sql/rowtable/RowFormatScanRDD.scala
@@ -12,13 +12,14 @@ import org.apache.spark.sql.catalyst.util.DateTimeUtils
 import org.apache.spark.sql.collection.MultiExecutorLocalPartition
 import org.apache.spark.sql.columnar.{ConnectionProperties, CachedBatch}
 import org.apache.spark.sql.execution.datasources.jdbc.JDBCRDD
+import org.apache.spark.sql.execution.datasources.jdbc.JDBCRDD._
 import org.apache.spark.sql.sources._
 import org.apache.spark.sql.store.StoreFunctions._
 import org.apache.spark.sql.store.StoreUtils
-import org.apache.spark.sql.types.{Decimal, StructType}
+import org.apache.spark.sql.types._
 import org.apache.spark.storage.BlockManagerId
 import org.apache.spark.unsafe.types.UTF8String
-import org.apache.spark.{Partition, SparkContext, TaskContext}
+import org.apache.spark.{Logging, Partition, SparkContext, TaskContext}
 import org.apache.spark.sql.catalyst.InternalRow
 
 /**
@@ -116,7 +117,7 @@ class RowFormatScanRDD(@transient sc: SparkContext,
    */
   override def compute(thePart: Partition, context: TaskContext): Iterator[InternalRow] = {
     val (conn, stmt, rs) = computeResultSet(thePart)
-    new InternalRowIteratorOnRS(conn,stmt, rs, context).asInstanceOf[Iterator[InternalRow]]
+    new InternalRowIteratorOnRS(conn,stmt, rs, context, schema).asInstanceOf[Iterator[InternalRow]]
   }
 
 
@@ -137,131 +138,172 @@ class RowFormatScanRDD(@transient sc: SparkContext,
         }
     })
   }
+}
 
-  class InternalRowIteratorOnRS (conn: Connection,
-      stmt: Statement, rs: ResultSet , context: TaskContext) extends Iterator[ org.apache.spark.sql.catalyst.InternalRow] {
-      private val closed = false
-      private var finished = false
-      private var gotNext = false
-      private var nextValue: InternalRow = null.asInstanceOf[InternalRow]
 
-      context.addTaskCompletionListener { context => close() }
 
-      val conversions = getConversions(schema)
-      val mutableRow = new SpecificMutableRow(schema.fields.map(x => x.dataType))
+class InternalRowIteratorOnRS (conn: Connection,
+    stmt: Statement, rs: ResultSet , context: TaskContext , schema:StructType)
+    extends Iterator[ org.apache.spark.sql.catalyst.InternalRow]
+   with  Logging {
+  private val closed = false
+  private var finished = false
+  private var gotNext = false
+  private var nextValue: InternalRow = null.asInstanceOf[InternalRow]
 
-      def getNext: InternalRow = {
-        if (rs.next()) {
-          var i = 0
-          while (i < conversions.length) {
-            val pos = i + 1
-            conversions(i) match {
-              case BooleanConversion => mutableRow.setBoolean(i, rs.getBoolean(pos))
-              case DateConversion =>
-                // DateTimeUtils.fromJavaDate does not handle null value, so we need to check it.
-                val dateVal = rs.getDate(pos)
-                if (dateVal != null) {
-                  mutableRow.setInt(i, DateTimeUtils.fromJavaDate(dateVal))
-                } else {
-                  mutableRow.update(i, null)
-                }
-              // When connecting with Oracle DB through JDBC, the precision and scale of BigDecimal
-              // object returned by ResultSet.getBigDecimal is not correctly matched to the table
-              // schema reported by ResultSetMetaData.getPrecision and ResultSetMetaData.getScale.
-              // If inserting values like 19999 into a column with NUMBER(12, 2) type, you get through
-              // a BigDecimal object with scale as 0. But the dataframe schema has correct type as
-              // DecimalType(12, 2). Thus, after saving the dataframe into parquet file and then
-              // retrieve it, you will get wrong result 199.99.
-              // So it is needed to set precision and scale for Decimal based on JDBC metadata.
-              case DecimalConversion(p, s) =>
-                val decimalVal = rs.getBigDecimal(pos)
-                if (decimalVal == null) {
-                  mutableRow.update(i, null)
-                } else {
-                  mutableRow.update(i, Decimal(decimalVal, p, s))
-                }
-              case DoubleConversion => mutableRow.setDouble(i, rs.getDouble(pos))
-              case FloatConversion => mutableRow.setFloat(i, rs.getFloat(pos))
-              case IntegerConversion => mutableRow.setInt(i, rs.getInt(pos))
-              case LongConversion => mutableRow.setLong(i, rs.getLong(pos))
-              // TODO(davies): use getBytes for better performance, if the encoding is UTF-8
-              case StringConversion => mutableRow.update(i, UTF8String.fromString(rs.getString(pos)))
-              case TimestampConversion =>
-                val t = rs.getTimestamp(pos)
-                if (t != null) {
-                  mutableRow.setLong(i, DateTimeUtils.fromJavaTimestamp(t))
-                } else {
-                  mutableRow.update(i, null)
-                }
-              case BinaryConversion => mutableRow.update(i, rs.getBytes(pos))
-              case BinaryLongConversion =>
-                val bytes = rs.getBytes(pos)
-                var ans = 0L
-                var j = 0
-                while (j < bytes.size) {
-                  ans = 256 * ans + (255 & bytes(j))
-                  j = j + 1
-                }
-                mutableRow.setLong(i, ans)
+  context.addTaskCompletionListener { context => close() }
+
+  val conversions = getConversions(schema)
+  val mutableRow = new SpecificMutableRow(schema.fields.map(x => x.dataType))
+
+  def getNext: InternalRow = {
+    if (rs.next()) {
+      var i = 0
+      while (i < conversions.length) {
+        val pos = i + 1
+        conversions(i) match {
+          case BooleanConversion => mutableRow.setBoolean(i, rs.getBoolean(pos))
+          case DateConversion =>
+            // DateTimeUtils.fromJavaDate does not handle null value, so we need to check it.
+            val dateVal = rs.getDate(pos)
+            if (dateVal != null) {
+              mutableRow.setInt(i, DateTimeUtils.fromJavaDate(dateVal))
+            } else {
+              mutableRow.update(i, null)
             }
-            if (rs.wasNull) mutableRow.setNullAt(i)
-            i = i + 1
-          }
-          mutableRow.asInstanceOf[InternalRow]
-        } else {
-          finished = true
-          null.asInstanceOf[InternalRow]
-        }
-      }
-
-      def close() {
-        if (closed) return
-        try {
-          if (null != rs) {
-            rs.close()
-          }
-        } catch {
-          case e: Exception => logWarning("Exception closing resultset", e)
-        }
-        try {
-          if (null != stmt) {
-            stmt.close()
-          }
-        } catch {
-          case e: Exception => logWarning("Exception closing statement", e)
-        }
-        try {
-          if (null != conn) {
-            conn.close()
-          }
-          logDebug("closed connection for task " + context.partitionId())
-        } catch {
-          case e: Exception => logWarning("Exception closing connection", e)
-        }
-      }
-
-      override def hasNext: Boolean = {
-        if (!finished) {
-          if (!gotNext) {
-            nextValue = getNext
-            if (finished) {
-              close()
+          // When connecting with Oracle DB through JDBC, the precision and scale of BigDecimal
+          // object returned by ResultSet.getBigDecimal is not correctly matched to the table
+          // schema reported by ResultSetMetaData.getPrecision and ResultSetMetaData.getScale.
+          // If inserting values like 19999 into a column with NUMBER(12, 2) type, you get through
+          // a BigDecimal object with scale as 0. But the dataframe schema has correct type as
+          // DecimalType(12, 2). Thus, after saving the dataframe into parquet file and then
+          // retrieve it, you will get wrong result 199.99.
+          // So it is needed to set precision and scale for Decimal based on JDBC metadata.
+          case DecimalConversion(p, s) =>
+            val decimalVal = rs.getBigDecimal(pos)
+            if (decimalVal == null) {
+              mutableRow.update(i, null)
+            } else {
+              mutableRow.update(i, Decimal(decimalVal, p, s))
             }
-            gotNext = true
-          }
+          case DoubleConversion => mutableRow.setDouble(i, rs.getDouble(pos))
+          case FloatConversion => mutableRow.setFloat(i, rs.getFloat(pos))
+          case IntegerConversion => mutableRow.setInt(i, rs.getInt(pos))
+          case LongConversion => mutableRow.setLong(i, rs.getLong(pos))
+          // TODO(davies): use getBytes for better performance, if the encoding is UTF-8
+          case StringConversion => mutableRow.update(i, UTF8String.fromString(rs.getString(pos)))
+          case TimestampConversion =>
+            val t = rs.getTimestamp(pos)
+            if (t != null) {
+              mutableRow.setLong(i, DateTimeUtils.fromJavaTimestamp(t))
+            } else {
+              mutableRow.update(i, null)
+            }
+          case BinaryConversion => mutableRow.update(i, rs.getBytes(pos))
+          case BinaryLongConversion =>
+            val bytes = rs.getBytes(pos)
+            var ans = 0L
+            var j = 0
+            while (j < bytes.size) {
+              ans = 256 * ans + (255 & bytes(j))
+              j = j + 1
+            }
+            mutableRow.setLong(i, ans)
         }
-        !finished
+        if (rs.wasNull) mutableRow.setNullAt(i)
+        i = i + 1
       }
+      mutableRow.asInstanceOf[InternalRow]
+    } else {
+      finished = true
+      null.asInstanceOf[InternalRow]
+    }
+  }
 
-      override def next(): InternalRow = {
-        if (!hasNext) {
-          throw new NoSuchElementException("End of stream")
+  def close() {
+    if (closed) return
+    try {
+      if (null != rs) {
+        rs.close()
+      }
+    } catch {
+      case e: Exception => logWarning("Exception closing resultset", e)
+    }
+    try {
+      if (null != stmt) {
+        stmt.close()
+      }
+    } catch {
+      case e: Exception => logWarning("Exception closing statement", e)
+    }
+    try {
+      if (null != conn) {
+        conn.close()
+      }
+      logDebug("closed connection for task " + context.partitionId())
+    } catch {
+      case e: Exception => logWarning("Exception closing connection", e)
+    }
+  }
+
+  override def hasNext: Boolean = {
+    if (!finished) {
+      if (!gotNext) {
+        nextValue = getNext
+        if (finished) {
+          close()
         }
-        gotNext = false
-        nextValue
+        gotNext = true
       }
     }
+    !finished
+  }
 
+  override def next(): InternalRow = {
+    if (!hasNext) {
+      throw new NoSuchElementException("End of stream")
+    }
+    gotNext = false
+    nextValue
+  }
+
+  // Each JDBC-to-Catalyst conversion corresponds to a tag defined here so that
+  // we don't have to potentially poke around in the Metadata once for every
+  // row.
+  // Is there a better way to do this?  I'd rather be using a type that
+  // contains only the tags I define.
+  abstract class JDBCConversion
+  case object BooleanConversion extends JDBCConversion
+  case object DateConversion extends JDBCConversion
+  case class  DecimalConversion(precision: Int, scale: Int) extends JDBCConversion
+  case object DoubleConversion extends JDBCConversion
+  case object FloatConversion extends JDBCConversion
+  case object IntegerConversion extends JDBCConversion
+  case object LongConversion extends JDBCConversion
+  case object BinaryLongConversion extends JDBCConversion
+  case object StringConversion extends JDBCConversion
+  case object TimestampConversion extends JDBCConversion
+  case object BinaryConversion extends JDBCConversion
+
+  /**
+   * Maps a StructType to a type tag list.
+   */
+  def getConversions(schema: StructType): Array[JDBCConversion] = {
+    schema.fields.map(sf => sf.dataType match {
+      case BooleanType => BooleanConversion
+      case DateType => DateConversion
+      case DecimalType.Fixed(p, s) => DecimalConversion(p, s)
+      case DoubleType => DoubleConversion
+      case FloatType => FloatConversion
+      case IntegerType => IntegerConversion
+      case LongType =>
+        if (sf.metadata.contains("binarylong")) BinaryLongConversion else LongConversion
+      case StringType => StringConversion
+      case TimestampType => TimestampConversion
+      case BinaryType => BinaryConversion
+      case _ => throw new IllegalArgumentException(s"Unsupported field $sf")
+    }).toArray
+  }
 }
 
 
