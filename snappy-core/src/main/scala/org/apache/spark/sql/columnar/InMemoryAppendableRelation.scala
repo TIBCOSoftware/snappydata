286c30be2ce30b8937c4f28bd1679a6d5d63484c
Neeraj Kumar <kneeraj@snappydata.io>
2016-01-08 19:20:01 +0530

    added snappydata header and apache license to snappy-core files. clearspring will be taken next.

--- snappy-core/src/main/scala/org/apache/spark/sql/columnar/InMemoryAppendableRelation.scala
+++ snappy-core/src/main/scala/org/apache/spark/sql/columnar/InMemoryAppendableRelation.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2010-2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+
 package org.apache.spark.sql.columnar
 
 /**
@@ -5,27 +22,7 @@ package org.apache.spark.sql.columnar
  * Append creates new CachedBatches like a normal buildBuffers as required,
  * all of which are tracked in driver as separate RDD[CachedBatch] and a
  * union over all existing is used for a query execution.
- *
- * Created by Soubhik on 5/22/15.
  */
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.util.concurrent.locks.ReentrantReadWriteLock
 
 import scala.collection.mutable.ArrayBuffer
