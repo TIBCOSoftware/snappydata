766e24139bd69ffc34af477608ca8b6b9a03b93e
swati <ssawant@snappydata.io>
2016-01-13 15:26:37 +0530

    Incorporated review comments

--- snappy-examples/quickstart/scripts/olap_approx_queries.sql
+++ snappy-examples/quickstart/scripts/olap_approx_queries.sql
@@ -8,7 +8,7 @@ select  count(*) flightRecCount, description AirlineName, UniqueCarrier carrierC
    where airline.UniqueCarrier = airlineref.code
    group by UniqueCarrier,description, Year_
    order by flightRecCount desc limit 10
-   with error 0.07 confidence 0.82;
+   with error 0.20 confidence 0.80;
 
 -------------------------------------------------------------
 ---- Which Airlines Arrive On Schedule? ----
@@ -16,7 +16,7 @@ select  count(*) flightRecCount, description AirlineName, UniqueCarrier carrierC
 select AVG(ArrDelay) arrivalDelay, absolute_error(arrivalDelay) abs_err, relative_error(arrivalDelay) rel_err, UniqueCarrier carrier from airline
    group by UniqueCarrier
    order by arrivalDelay
-   with error 0.07 confidence 0.95;
+   with error 0.20 confidence 0.80;
 
 -------------------------------------------------------------
 ---- Which Airlines Arrive On Schedule? JOIN with reference table ----
@@ -26,7 +26,7 @@ select AVG(ArrDelay) arrivalDelay, absolute_error(arrivalDelay) abs_err, relativ
   where airline.UniqueCarrier = airlineref.Code
   group by UniqueCarrier, description
   order by arrivalDelay
-  with error 0.07 confidence 0.90;
+  with error 0.20 confidence 0.80;
 
 -------------------------------------------------------------
 ---- What is the trend in arrival delays across all airlines in the US? ----
@@ -35,14 +35,4 @@ select AVG(ArrDelay) ArrivalDelay, absolute_error(arrivalDelay) abs_err, relativ
   from airline
   group by Year_
   order by Year_
-  with error 0.17 confidence 0.87;
-
--------------------------------------------------------------
----- Which airline out of SanFrancisco had most delays due to weather ----
--------------------------------------------------------------
-SELECT sum(WeatherDelay) totalWeatherDelay, lower_bound(totalWeatherDelay), upper_bound(totalWeatherDelay), absolute_error(totalWeatherDelay) abs_err, relative_error(totalWeatherDelay) rel_err, airlineref.DESCRIPTION
-  FROM airline, airlineref
-  WHERE airline.UniqueCarrier = airlineref.CODE AND  Origin like '%SFO%' AND WeatherDelay > 0
-  GROUP BY DESCRIPTION
-  limit 20
-  with error 0.13 confidence 0.85;
+  with error 0.20 confidence 0.80;
