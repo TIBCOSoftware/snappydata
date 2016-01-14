6ea0328c8a325bb687cadd57c18a6a8855e7fc0e
Rajesh Kumar <rkumar@snappydata.io>
2016-01-14 20:11:45 +0530

    Added sql script for streaming quickstart.
    run: precheckin
    Reviewed by: Hemant

--- snappy-core/src/main/scala/org/apache/spark/sql/streaming/TweetToRowsConverter.scala
+++ snappy-core/src/main/scala/org/apache/spark/sql/streaming/TweetToRowsConverter.scala
@@ -1,8 +1,10 @@
 package org.apache.spark.sql.streaming
 
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.json.{JSONArray, JSONObject}
 import twitter4j.Status
 
-import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.{InternalRow}
 import org.apache.spark.unsafe.types.UTF8String
 
 class TweetToRowsConverter extends StreamToRowsConverter with Serializable {
@@ -17,4 +19,63 @@ class TweetToRowsConverter extends StreamToRowsConverter with Serializable {
         status.getHashtagEntities.mkString(",")))))
   }
 
+}
+class TweetToHashtagRow extends StreamToRowsConverter with Serializable {
+
+  override def toRows(message: Any): Seq[InternalRow] = {
+    val schema = StructType(List(StructField("hashtag", StringType)))
+    var json: JSONObject = null
+    var arr: Array[InternalRow] = null
+    if (message.isInstanceOf[String]) {
+      //for file stream
+      json = new JSONObject(message.asInstanceOf[String])
+      val hashArray = json.get("hashtagEntities").asInstanceOf[JSONArray]
+      arr = new Array[InternalRow](hashArray.length())
+      for (i <- 0 until hashArray.length()) {
+        val a = hashArray.getJSONObject(i)
+        val b = a.getString("text")
+        arr(i) = InternalRow.fromSeq(Seq(UTF8String.fromString(b)))
+      }
+    } else {
+      //for twitter stream
+      val status = message.asInstanceOf[Status]
+      val hashArray = status.getHashtagEntities
+      arr = new Array[InternalRow](hashArray.length)
+      for (i <- 0 until hashArray.length) {
+        val b = hashArray(i).getText
+
+        arr(i) = InternalRow.fromSeq(Seq(UTF8String.fromString(b)))
+      }
+    }
+
+    arr.toSeq
+  }
+}
+
+class TweetToRetweetRow extends StreamToRowsConverter with Serializable {
+
+  override def toRows(message: Any): Seq[InternalRow] = {
+    var json: JSONObject = null
+    var retweetCnt: Int = 0
+    var retweetTxt: String = null
+    if (message.isInstanceOf[String]) {
+      //for file stream
+      json = new JSONObject(message.asInstanceOf[String])
+      if (json != null && json.has("retweetedStatus")) {
+        val retweetedSts = json.getJSONObject("retweetedStatus")
+        retweetTxt = retweetedSts.get("text").asInstanceOf[String]
+        retweetCnt = retweetedSts.get("retweetCount").asInstanceOf[Int]
+      }
+    } else {
+      //for twitter stream
+      val status = message.asInstanceOf[Status]
+      if (status.getRetweetedStatus != null) {
+        retweetTxt = status.getRetweetedStatus.getText
+        retweetCnt = status.getRetweetedStatus.getRetweetCount
+      }
+    }
+    val sampleRow = new Array[InternalRow](1)
+    sampleRow(0) = InternalRow.fromSeq(Seq(retweetCnt, UTF8String.fromString(retweetTxt)))
+    sampleRow.toSeq
+  }
 }
