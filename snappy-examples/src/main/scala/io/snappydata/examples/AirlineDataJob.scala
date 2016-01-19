b205a5813dc57379d2288366b54223fbcd0b565c
Hemant Bhanawat <hbhanawat@pivotal.io>
2016-01-19 10:00:08 +0530

    Fixed formatting.

--- snappy-examples/src/main/scala/io/snappydata/examples/AirlineDataJob.scala
+++ snappy-examples/src/main/scala/io/snappydata/examples/AirlineDataJob.scala
@@ -1,6 +1,8 @@
 package io.snappydata.examples
 
 import java.io.{PrintWriter}
+import scala.util.{Failure, Success, Try}
+
 import com.typesafe.config.Config
 import org.apache.spark.sql.SnappyAQP._
 import org.apache.spark.sql.{DataFrame, SnappySQLJob}
@@ -22,68 +24,89 @@ object AirlineDataJob extends SnappySQLJob {
     val sampleTable = "AIRLINE_SAMPLE"
 
     def getCurrentDirectory = new java.io.File( "." ).getCanonicalPath
+    // scalastyle:off println
     val pw = new PrintWriter("AirlineDataJob.out")
+    Try {
+      // Get the already created tables
+      val airlineDF: DataFrame = snc.table(colTable)
+      val airlineCodeDF: DataFrame = snc.table(rowTable)
+      val airlineParquetDF: DataFrame = snc.table(parquetTable)
+      val sampleDF: DataFrame = snc.table(sampleTable)
 
-    // Get the tables that were created using sql scripts via snappy-shell
-    val airlineDF: DataFrame = snc.table(colTable)
-    val airlineCodeDF: DataFrame = snc.table(rowTable)
-    val airlineParquetDF: DataFrame = snc.table(parquetTable)
-    val sampleDF: DataFrame = snc.table(sampleTable)
+      // Cache the airline data in a Spark table as well.
+      // We will use this to compare against the Snappy Store table
+      airlineParquetDF.cache()
+      airlineParquetDF.count()
 
-    // Cache the airline data in a Spark table as well
-    airlineParquetDF.cache()
-    airlineParquetDF.count()
+      // Query Snappy Store's Airline table :Which Airlines Arrive
+      // On Schedule? JOIN with reference table
+      val actualResult = airlineDF.join(airlineCodeDF,
+        airlineDF.col("UniqueCarrier").equalTo(airlineCodeDF("CODE"))).
+          groupBy(airlineDF("UniqueCarrier"), airlineCodeDF("DESCRIPTION")).
+          agg("ArrDelay" -> "avg").orderBy("avg(ArrDelay)")
 
-    // Data Frame query on Airline table :Which Airlines Arrive On Schedule? JOIN with reference table
-    val actualResult = airlineDF.join(airlineCodeDF, airlineDF.col("UniqueCarrier").
-        equalTo(airlineCodeDF("CODE"))).groupBy(airlineDF("UniqueCarrier"),
-      airlineCodeDF("DESCRIPTION")).agg("ArrDelay" -> "avg").orderBy("avg(ArrDelay)")
-    val start = System.currentTimeMillis
-    val result = actualResult.collect()
-    val totalTime = (System.currentTimeMillis - start)
-    pw.println(s"****** Query Execution on Airline Snappy table took ${totalTime}ms ******")
-    result.foreach(rs => {
-      pw.println(rs.toString)
-    })
+      val start = System.currentTimeMillis
+      val result = actualResult.collect()
+      val totalTime = (System.currentTimeMillis - start)
+      pw.println(s"****** Query Execution on Airline Snappy " +
+          s"table took ${totalTime}ms ******")
+      result.foreach(rs => {
+        pw.println(rs.toString)
+      })
 
-    // Data Frame query on Spark table :Which Airlines Arrive On Schedule? JOIN with reference table
-    val parquetResult = airlineParquetDF.join(airlineCodeDF, airlineParquetDF.col("UniqueCarrier").
-        equalTo(airlineCodeDF("CODE"))).groupBy(airlineParquetDF("UniqueCarrier"),
-      airlineCodeDF("DESCRIPTION")).agg("ArrDelay" -> "avg").orderBy("avg(ArrDelay)")
-    val startP = System.currentTimeMillis
-    val resultP = parquetResult.collect()
-    val totalTimeP = (System.currentTimeMillis - startP)
-    pw.println(s"\n****** Query Execution on Airline Spark table took ${totalTimeP}ms******")
-    resultP.foreach(rs => {
-      pw.println(rs.toString)
-    })
+      // Query Spark cached DataFrame : Which Airlines
+      // Arrive On Schedule? JOIN with reference table
+      val parquetResult = airlineParquetDF.join(airlineCodeDF,
+        airlineParquetDF.col("UniqueCarrier").equalTo(airlineCodeDF("CODE"))).
+          groupBy(airlineParquetDF("UniqueCarrier"), airlineCodeDF("DESCRIPTION")).
+          agg("ArrDelay" -> "avg").orderBy("avg(ArrDelay)")
 
-    // Data Frame query on Sample table :Which Airlines Arrive On Schedule? JOIN with reference table
-    val sampleResult = sampleDF.join(airlineCodeDF, sampleDF.col("UniqueCarrier").
-        equalTo(airlineCodeDF("CODE"))).groupBy(sampleDF("UniqueCarrier"),
-      airlineCodeDF("DESCRIPTION")).agg("ArrDelay" -> "avg").orderBy("avg(ArrDelay)")
-    val startS = System.currentTimeMillis
-    val resultS = sampleResult.collect()
-    val totalTimeS = (System.currentTimeMillis - startS)
-    pw.println(s"\n****** Query Execution on Airline Sample table took ${totalTimeS}ms******")
-    resultS.foreach(rs => {
-      pw.println(rs.toString)
-    })
+      val startP = System.currentTimeMillis
+      val resultP = parquetResult.collect()
+      val totalTimeP = (System.currentTimeMillis - startP)
+      pw.println(s"\n****** Query Execution on Airline Spark " +
+          s"table took ${totalTimeP}ms******")
+      resultP.foreach(rs => {
+        pw.println(rs.toString)
+      })
 
-    //Sampling on Base table(i.e Airline table) with error and confidence clause
-    val sampleBaseTableResult = airlineDF.groupBy(airlineDF("Year_"))
-        .agg("ArrDelay" -> "avg")
-        .orderBy("Year_").withError(0.20,0.80)
-    val startSB = System.currentTimeMillis
-    val resultSB = sampleBaseTableResult.collect()
-    val totalTimeSB = (System.currentTimeMillis - startSB)
-    pw.println(s"\n****** Query Execution with sampling on Airline table took ${totalTimeSB}ms******")
-    resultSB.foreach(rs => {
-      pw.println(rs.toString)
-    })
+      // Query Snappy Store's Sample table :Which Airlines
+      // Arrive On Schedule? JOIN with reference table
+      val sampleResult = sampleDF.join(airlineCodeDF,
+        sampleDF.col("UniqueCarrier").equalTo(airlineCodeDF("CODE"))).
+          groupBy(sampleDF("UniqueCarrier"), airlineCodeDF("DESCRIPTION")).
+          agg("ArrDelay" -> "avg").orderBy("avg(ArrDelay)")
 
-    pw.close()
-    Map("The output of the queries is in the following file: " -> s"${getCurrentDirectory}/AirlineDataJob.out")
+      val startS = System.currentTimeMillis
+      val resultS = sampleResult.collect()
+      val totalTimeS = (System.currentTimeMillis - startS)
+      pw.println(s"\n****** Query Execution on Airline Sample " +
+          s"table took ${totalTimeS}ms******")
+      resultS.foreach(rs => {
+        pw.println(rs.toString)
+      })
+
+      // Query Snappy Store's Airline table with error clause.
+      // This fetches the results directly from the sample table
+      // if the error is within the specified limit
+      val sampleBaseTableResult = airlineDF.groupBy(airlineDF("Year_"))
+          .agg("ArrDelay" -> "avg")
+          .orderBy("Year_").withError(0.20, 0.80)
+      val startSB = System.currentTimeMillis
+      val resultSB = sampleBaseTableResult.collect()
+      val totalTimeSB = (System.currentTimeMillis - startSB)
+      pw.println(s"\n****** Query Execution with sampling on " +
+          s"Airline table took ${totalTimeSB}ms******")
+      resultSB.foreach(rs => {
+        pw.println(rs.toString)
+      })
+    } match {
+      case Success(v) => pw.close()
+        s"See ${getCurrentDirectory}/AirlineDataJob.out"
+      case Failure(e) => pw.close();
+        throw e;
+    }
+    // scalastyle:on println
   }
 
   override def validate(sc: C, config: Config): SparkJobValidation = {
