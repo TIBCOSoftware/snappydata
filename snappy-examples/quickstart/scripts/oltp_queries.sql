fa61624925985669c9700703d3c858161c2f94a7
spillai <spillai@pivotal.io>
2016-01-20 15:12:11 +0530

    - Modified the AirlineSparkApp to use SnappyContext.update API. - Removed casting from the oltp queries.

--- snappy-examples/quickstart/scripts/oltp_queries.sql
+++ snappy-examples/quickstart/scripts/oltp_queries.sql
@@ -1,6 +1,6 @@
 --- Suppose a particular Airline company say 'Delta Air Lines Inc.' re-brands itself as 'Delta America'
 --- the airline code can be updated in the row table
-UPDATE AIRLINEREF SET DESCRIPTION='Delta America' WHERE CAST(CODE AS VARCHAR(25))='DL';
+UPDATE AIRLINEREF SET DESCRIPTION='Delta America' WHERE CODE='DL';
 
 --- Query to see the updated result ---
 SELECT * FROM AIRLINEREF WHERE CAST(CODE AS VARCHAR(25))='DL';
