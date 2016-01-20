94d12121e6a722f38fb7ac44795f5a3bc777be9b
rmishra <rmishra@pivotal.io>
2016-01-20 15:32:21 +0530

    Adding scaladoc to SnappyContext Added a test to check update and createTable APIS.

--- snappy-tools/src/test/scala/org/apache/spark/sql/store/JDBCMutableRelationAPISuite.scala
+++ snappy-tools/src/test/scala/org/apache/spark/sql/store/JDBCMutableRelationAPISuite.scala
@@ -24,7 +24,7 @@ import org.scalatest.BeforeAndAfterAll
 
 //scalastyle:ignore
 
-import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.{Row, SaveMode}
 
 /**
  * Tests for non-GFXD JDBC tables.
@@ -75,5 +75,31 @@ class JDBCMutableRelationAPISuite
         .options(props).saveAsTable("TEST_JDBC_TABLE_1")
     val count = dataDF.count()
     assert(count === data.length)
+  }
+
+  test("Create table in an external DataStore in Non-Embedded mode using schemaDDL") {
+    val props = Map(
+      "url" -> s"jdbc:derby:$path",
+      "driver" -> "org.apache.derby.jdbc.EmbeddedDriver",
+      "poolImpl" -> "tomcat",
+      "user" -> "app",
+      "password" -> "app"
+    )
+    snc.sql("DROP TABLE IF EXISTS TEST_JDBC_TABLE_2")
+
+    val data = Seq(Seq(1, 2, 3), Seq(7, 8, 9), Seq(9, 2, 3), Seq(4, 2, 3), Seq(5, 6, 7))
+    val rdd = sc.parallelize(data, data.length).map(s => new Data(s(0), s(1), s(2)))
+    val dataDF = snc.createDataFrame(rdd)
+    val schemaDDL = "(OrderId INT NOT NULL PRIMARY KEY,ItemId INT, ITEMREF INT)"
+    snc.createTable("TEST_JDBC_TABLE_2", "jdbc", schemaDDL, props)
+    dataDF.write.format("jdbc").mode(SaveMode.Append).saveAsTable("TEST_JDBC_TABLE_2")
+    val tableDF = snc.sql("select * from TEST_JDBC_TABLE_2")
+    val count = tableDF.count()
+    assert(count === data.length)
+
+    println(snc.update("TEST_JDBC_TABLE_2", "ITEMREF = 3" , Row(99) , "ITEMREF" ))
+
+    val cdf = snc.sql("Select * from TEST_JDBC_TABLE_2 where ITEMREF = 99")
+    assert(cdf.count() === 3)
   }
 }
