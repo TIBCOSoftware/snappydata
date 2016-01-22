d31177e724f1c0a604c2eca5de96a4b65307356d
Asif Shahid <asif.shahid@pivotal.io>
2016-01-22 11:44:50 -0800

    Fix for the NPE issue in TopK

--- snappy-dunits/src/test/scala/io/snappydata/dunit/streaming/StreamingDUnitTest.scala
+++ snappy-dunits/src/test/scala/io/snappydata/dunit/streaming/StreamingDUnitTest.scala
@@ -18,6 +18,68 @@ class StreamingDUnitTest(val s: String) extends ClusterManagerTestBase(s) {
     DriverManager.getConnection(url)
   }
 
+  //Temporarily disabled
+  def _testStreamingAdhocSQL_NPEBug(): Unit = {
+    val netPort1 = AvailablePortHelper.getRandomAvailableTCPPort
+    vm1.invoke(classOf[ClusterManagerTestBase], "startNetServer", netPort1)
+    val conn = getANetConnection(netPort1)
+    val s = conn.createStatement()
+    s.execute("streaming init 2")
+    s.execute("create stream table tweetsTable " +
+        "(id long, text string, fullName string, " +
+        "country string, retweets int, hashtag string) " +
+        "using twitter_stream options (" +
+        "consumerKey '***REMOVED***', " +
+        "consumerSecret '***REMOVED***', " +
+        "accessToken '***REMOVED***', " +
+        "accessTokenSecret '***REMOVED***', " +
+        "rowConverter 'io.snappydata.dunit.streaming.TweetToRowsConverter')")
+    s.execute("create topk table topkTweets on tweetsTable options(" +
+        s"key 'hashtag', epoch '${System.currentTimeMillis()}', " +
+        "timeInterval '30000ms', size '10')")
+
+    s.execute("streaming start")
+
+    for (a <- 1 to 5) {
+
+      Thread.sleep(2000)
+
+      s.execute("select hashtag, count(hashtag) as cht from tweetsTable " +
+          "group by hashtag order by cht desc limit 10")
+      println("\n\n-----  TOP Tweets  -----\n")
+      var rs = s.getResultSet
+      if (a == 5) assert(rs.next)
+      while (rs.next()) {
+        println(s"${rs.getString(1)} ; ${rs.getInt(2)}")
+      }
+
+      s.execute("select * from topkTweets order by EstimatedValue desc")
+      println("\n\n-----  TOPK Tweets  -----\n")
+      rs = s.getResultSet
+      var numResults = 0
+      while (rs.next()) {
+        println(s"${rs.getString(1)} ; ${rs.getLong(2)} ; ${rs.getString(3)}")
+        numResults += 1
+      }
+      println(s"Num results=$numResults")
+      assert(numResults <= 10)
+
+      s.execute("select text, fullName from tweetsTable where text like '%e%'")
+      rs = s.getResultSet
+      if (a == 5) assert(rs.next)
+      while (rs.next()) {
+        rs.getString(1)
+        rs.getString(2)
+      }
+    }
+    s.execute("streaming stop")
+    s.execute("drop table tweetsTable")
+    s.execute("drop table topktweets")
+
+    conn.close()
+  }
+
+
   def testStreamingAdhocSQL(): Unit = {
     val netPort1 = AvailablePortHelper.getRandomAvailableTCPPort
     vm1.invoke(classOf[ClusterManagerTestBase], "startNetServer", netPort1)
