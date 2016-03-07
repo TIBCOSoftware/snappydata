783b0b2b8f1715bdf377630e60f0558f90fd82af
ymahajan <ymahajan@snappydata.io>
2016-03-07 10:14:41 +0530

    + Adserver log aggregator related enhancements

--- snappy-examples/src/main/scala/io/snappydata/aggr/LogAggregator.scala
+++ snappy-examples/src/main/scala/io/snappydata/aggr/LogAggregator.scala
@@ -3,23 +3,21 @@
 package io.snappydata.aggr
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.{SaveMode, SnappyContext}
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.streaming.{SnappyStreamingContext, StreamToRowsConverter}
-import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext}
+import org.apache.spark.sql.{SaveMode, SnappyContext}
+import org.apache.spark.streaming.Milliseconds
 import org.apache.spark.unsafe.types.UTF8String
 
 object LogAggregator extends App {
 
   val sparkConf = new org.apache.spark.SparkConf()
       .setAppName("logAggregator")
-      // .set("snappydata.store.locators", "localhost:10101")
-      // .setMaster("snappydata://localhost:10334")
       .setMaster("local[2]")
-  // .set("snappydata.embedded", "true")
+  // .setMaster("snappydata://localhost:10334")
 
   val sc = new SparkContext(sparkConf)
-  val ssnc = SnappyStreamingContext(SnappyContext.getOrCreate(sc), Milliseconds(30000))
+  val ssnc = SnappyStreamingContext(SnappyContext.getOrCreate(sc), Milliseconds(10000))
 
   ssnc.sql("create stream table impressionlog (timestamp long, publisher string," +
       " advertiser string, " +
@@ -27,27 +25,31 @@ object LogAggregator extends App {
       "using directkafka_stream options " +
       "(storagelevel 'MEMORY_AND_DISK_SER_2', " +
       "rowConverter 'io.snappydata.aggr.KafkaStreamToRowsConverter' ," +
-      " kafkaParams 'metadata.broker.list->localhost:9092'," +
+      " kafkaParams 'metadata.broker.list->localhost:9092,localhost:9093'," +
       " topics 'adnetwork-topic'," +
-      " key 'java.lang.String'," +
-      " value 'io.snappydata.aggr.ImpressionLog', " +
-      " keyDecoder 'kafka.serializer.StringDecoder', " +
-      " valueDecoder 'io.snappydata.aggr.ImpressionLogAvroDecoder')")
+      " K 'java.lang.String'," +
+      " V 'io.snappydata.aggr.ImpressionLog', " +
+      " KD 'kafka.serializer.StringDecoder', " +
+      " VD 'io.snappydata.aggr.ImpressionLogAvroDecoder')")
 
+  ssnc.sql("create table snappyStoreTable(timestamp long, publisher string," +
+      " geo string, avg_bid double) " +
+      "using column " +
+      "options(PARTITION_BY 'timestamp')")
 
-  val tableStream = ssnc.getSchemaDStream("impressionlog")
-  tableStream.foreachRDD(print(_))
-
-  tableStream.foreachDataFrame( df => {
-    df.write.format("column").mode(SaveMode.Append)
-        .options(Map.empty[String, String]).saveAsTable("gemxdColumnTable")
-  })
-  // start rolling!
+  ssnc.registerCQ("select timestamp, publisher, geo, avg(bid) as avg_bid" +
+      " from impressionlog window (duration '10' seconds, slide '10' seconds)" +
+      " where geo != 'unknown' group by publisher, geo, timestamp")
+      .foreachDataFrame(df => {
+        df.show
+        df.write.format("column").mode(SaveMode.Append)
+            .options(Map.empty[String, String]).saveAsTable("snappyStoreTable")
+      })
   ssnc.sql("STREAMING START")
 
-  ssnc.awaitTerminationOrTimeout(1800* 1000)
+  ssnc.awaitTerminationOrTimeout(1800 * 1000)
 
-  ssnc.sql("select count(*) from gemxdColumnTable").show()
+  ssnc.sql("select count(*) from snappyStoreTable").show()
 
   ssnc.sql("STREAMING STOP")
 
