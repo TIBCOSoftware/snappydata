fa9ebc30ae4b219ffbf2e7f5eff48e0b173b5816
Sumedh Wale <swale@snappydata.io>
2016-04-15 02:04:47 +0530

    [SNAP-606] fixes to allow working core, store against Apache Spark (#216)
    ## Changes proposed in this pull request
    
    - Rewritten SQL parser based using parboiled2.org which ls also combinatorial PEG grammar like current Spark SQL one. Optimized version which is more than two orders of magnitude faster than Spark SQL combinatorial parser.
    - Refactored code to remove dependencies on snappy-spark extensions everywhere in core. Moved store dependencies to be inside core and everything else apart from cluster and memory related additions.
    - Introduced reflection in CodeGeneration to access some private methods since there does not seem to be a cleaner way to handle that.
    - Moved around code from ToolsCallback to ServiceUtils. Moved code from Services/ServiceManager into core to allow starting server. Leads/locators will continue to be available only in cluster.
    - Moved the WHERE subquery, EXISTS/IN support into SnappyExtendedParser only available in cluster mode
    - Build changes to allow core compilation with stock Spark 1.6.1. Fixes to DataWriter usage, appendable temp tables and others to allow doing this. (see more details in https://github.com/SnappyDataInc/snappydata/commit/62b34efdc37e4196878fdd5d660f6f55faae8ffb)
    
    Includes pending code reorganization:
    
    ```
    snappy-aqp               => aqp (currently defunct)
    snappy-core              => core
    snappy-tools             => cluster, datasource implementations moved into core
    snappy-examples          => examples
    snappy-spark             => spark
    snappy-store             => store
    snappy-tools/benchmark   => tests/benchmark
    tests-common             => tests/common
    snappy-dunits            => tests/dunit
    ```
    
    Moved around tests that are supposed to work only in embedded mode into cluster project, vs others that should work against stock Spark into core project. Dunits are still all running in embedded mode though some could be further split which can be done later.
    
    ## Patch testing
    
    All existing tests in core as well as most from store now run against stock Spark. Added a test "compare parser performance" in ColumnTableTest which checks for a decently complex TPCH q7 correctness and performance of Spark SQL vs Snappy vs GemXD vs Hive.
    
    ## Other PRs 
    
    Minor changes to store in https://github.com/SnappyDataInc/snappy-store/pull/40

---
+++ cluster/src/main/scala/io/snappydata/ServiceManager.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+package io.snappydata
+
+import com.pivotal.gemfirexd.FabricService
+import com.pivotal.gemfirexd.internal.engine.fabricservice.FabricServiceImpl
+import io.snappydata.gemxd.ClusterCallbacksImpl
+import io.snappydata.impl.{LeadImpl, LocatorImpl}
+
+object ServiceManager {
+
+  ClusterCallbacksImpl.initialize()
+
+  private def contextLock = ServerManager.contextLock
+
+  /**
+   * Get the singleton instance of `Server`.
+   */
+  def getServerInstance: Server = ServerManager.getServerInstance
+
+  /**
+   * Get the singleton instance of `Locator`.
+   */
+  def getLocatorInstance: Locator = {
+    var instance: FabricService = FabricServiceImpl.getInstance
+    if (instance != null) {
+      return checkLocatorInstance(instance)
+    }
+    contextLock.synchronized {
+      instance = FabricServiceImpl.getInstance
+      if (instance == null) {
+        val locator: Locator = new LocatorImpl
+        FabricServiceImpl.setInstance(locator)
+        return locator
+      }
+      return checkLocatorInstance(instance)
+    }
+  }
+
+  /**
+   * Get the singleton instance of `Lead`.
+   */
+  def getLeadInstance: Lead = {
+    var instance: FabricService = FabricServiceImpl.getInstance
+    if (instance != null) {
+      return checkLeadInstance(instance)
+    }
+    contextLock.synchronized {
+      instance = FabricServiceImpl.getInstance
+      if (instance == null) {
+        val lead: Lead = new LeadImpl
+        FabricServiceImpl.setInstance(lead)
+        return lead
+      }
+      return checkLeadInstance(instance)
+    }
+  }
+
+  /**
+   * Get the current instance of either `Server` or `Locator` or `Lead`.
+   * This can be null if neither of `getServerInstance` or `getLeadInstance` or
+   * `getLocatorInstance` have been invoked, or the instance has been stopped.
+   */
+  def currentFabricServiceInstance: FabricService = FabricServiceImpl.getInstance
+
+  private def checkLocatorInstance(instance: FabricService): Locator = {
+    instance match {
+      case locator: Locator => locator
+      case _ => throw new IllegalStateException(
+        s"Found an instance of another snappy component $instance.")
+    }
+  }
+
+  private def checkLeadInstance(instance: FabricService): Lead = {
+    instance match {
+      case lead: Lead => lead
+      case _ => throw new IllegalStateException(
+        s"Found an instance of another snappy component $instance.")
+    }
+  }
+}
---
+++ core/src/main/scala/org/apache/spark/sql/streaming/SchemaDStream.scala
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+package org.apache.spark.sql.streaming
+
+import org.apache.spark.api.java.function.{VoidFunction => JVoidFunction, VoidFunction2 => JVoidFunction2}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
+import org.apache.spark.sql.catalyst.plans.logical._
+import org.apache.spark.sql.catalyst.{CatalystTypeConverters, InternalRow}
+import org.apache.spark.sql.execution._
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.{Duration, SnappyStreamingContext, Time}
+
+/**
+  * A SQL based DStream with support for schema/Product
+  * This class offers the ability to manipulate SQL query on DStreams
+  * It is similar to SchemaRDD, which offers the similar functions
+  * Internally, RDD of each batch duration is treated as a small
+  * table and CQs are evaluated on those small tables
+  * Some of the abstraction and code is borrowed from the project:
+  * https://github.com/Intel-bigdata/spark-streamingsql
+  * @param snsc
+  * @param queryExecution
+  *
+  */
+final class SchemaDStream(@transient val snsc: SnappyStreamingContext,
+    @transient val queryExecution: QueryExecution)
+    extends DStream[Row](snsc) {
+
+  @transient private val snappyContext: SnappyContext = snsc.snappyContext
+
+  @transient private val catalog = snsc.snappyContext.catalog
+
+  def this(ssc: SnappyStreamingContext, logicalPlan: LogicalPlan) =
+    this(ssc, ssc.snappyContext.executePlan(logicalPlan))
+
+  /**
+    * Apply a function to each DataFrame in this SchemaDStream. This is an output operator, so
+    * 'this' SchemaDStream will be registered as an output stream and therefore materialized.
+    */
+  def foreachDataFrame(foreachFunc: DataFrame => Unit): Unit = {
+    foreachDataFrame(foreachFunc, needsConversion = true)
+  }
+
+  /**
+    * Apply a function to each DataFrame in this SchemaDStream. This is an output operator, so
+    * 'this' SchemaDStream will be registered as an output stream and therefore materialized.
+    */
+  def foreachDataFrame(foreachFunc: DataFrame => Unit, needsConversion: Boolean): Unit = {
+    val func = (rdd: RDD[Row]) => {
+      foreachFunc(snappyContext.createDataFrame(rdd, this.schema, needsConversion))
+    }
+    this.foreachRDD(func)
+  }
+
+  /**
+   * Apply a function to each DataFrame in this SchemaDStream. This is an output operator, so
+   * 'this' SchemaDStream will be registered as an output stream and therefore materialized.
+   */
+  def foreachDataFrame(foreachFunc: JVoidFunction[DataFrame]): Unit = {
+    val func = (rdd: RDD[Row]) => {
+      foreachFunc.call(snappyContext.createDataFrame(rdd, this.schema, needsConversion = true))
+    }
+    this.foreachRDD(func)
+  }
+
+  /**
+   * Apply a function to each DataFrame in this SchemaDStream. This is an output operator, so
+   * 'this' SchemaDStream will be registered as an output stream and therefore materialized.
+   */
+  def foreachDataFrame(foreachFunc: JVoidFunction[DataFrame], needsConversion: Boolean): Unit = {
+    val func = (rdd: RDD[Row]) => {
+      foreachFunc.call(snappyContext.createDataFrame(rdd, this.schema, needsConversion))
+    }
+    this.foreachRDD(func)
+  }
+
+  /**
+   * Apply a function to each DataFrame in this SchemaDStream. This is an output operator, so
+   * 'this' SchemaDStream will be registered as an output stream and therefore materialized.
+   */
+  def foreachDataFrame(foreachFunc: JVoidFunction2[DataFrame, Time]): Unit = {
+    val func = (rdd: RDD[Row], time: Time) => {
+      foreachFunc.call(snappyContext.createDataFrame(rdd, this.schema, needsConversion = true),
+        time)
+    }
+    this.foreachRDD(func)
+  }
+
+
+  /**
+    * Apply a function to each DataFrame in this SchemaDStream. This is an output operator, so
+    * 'this' SchemaDStream will be registered as an output stream and therefore materialized.
+    */
+  def foreachDataFrame(foreachFunc: (DataFrame, Time) => Unit): Unit = {
+    foreachDataFrame(foreachFunc, needsConversion = true)
+  }
+
+  /**
+    * Apply a function to each DataFrame in this SchemaDStream. This is an output operator, so
+    * 'this' SchemaDStream will be registered as an output stream and therefore materialized.
+    */
+  def foreachDataFrame(foreachFunc: (DataFrame, Time) => Unit, needsConversion : Boolean): Unit = {
+    val func = (rdd: RDD[Row], time: Time) => {
+      foreachFunc(snappyContext.createDataFrame(rdd, this.schema, needsConversion), time)
+    }
+    this.foreachRDD(func)
+  }
+
+  /** Persist the RDDs of this SchemaDStream with the given storage level */
+  override def persist(level: StorageLevel): SchemaDStream = {
+    super.persist(level)
+    this
+  }
+
+  /** Persist RDDs of this SchemaDStream with the default storage level (MEMORY_ONLY_SER) */
+  override def persist(): SchemaDStream = persist(StorageLevel.MEMORY_ONLY_SER)
+
+  /** Persist RDDs of this SchemaDStream with the default storage level (MEMORY_ONLY_SER) */
+  override def cache(): SchemaDStream = persist()
+
+  /**
+    * Enable periodic checkpointing of RDDs of this SchemaDStream
+    * @param interval Time interval after which generated RDD will be checkpointed
+    */
+  override def checkpoint(interval: Duration): SchemaDStream = {
+    super.checkpoint(interval)
+    this
+  }
+
+  /** Return a new SchemaDStream containing only the elements that satisfy a predicate. */
+  override def filter(filterFunc: Row => Boolean): SchemaDStream = {
+    snsc.createSchemaDStream(filter(filterFunc), schema)
+  }
+
+  /**
+    * Return a new SchemaDStream with an increased or decreased level of parallelism.
+    * Each RDD in the returned SchemaDStream has exactly numPartitions partitions.
+    */
+  override def repartition(numPartitions: Int): SchemaDStream = {
+    snsc.createSchemaDStream(transform(_.repartition(numPartitions)), schema)
+  }
+
+  /** Registers this SchemaDStream as a table in the catalog. */
+  def registerAsTable(tableName: String): Unit = {
+    catalog.registerTable(
+      catalog.newQualifiedTableName(tableName),
+      logicalPlan)
+  }
+
+  /** Returns the schema of this SchemaDStream (represented by
+    * a [[StructType]]). */
+  def schema: StructType = queryExecution.analyzed.schema
+
+  /** List of parent DStreams on which this DStream depends on */
+  override def dependencies: List[DStream[InternalRow]] = parentStreams.toList
+
+  /** Time interval after which the DStream generates a RDD */
+  override def slideDuration: Duration = {
+    parentStreams.head.slideDuration
+  }
+
+  @transient val logicalPlan: LogicalPlan = queryExecution.logical
+  match {
+    case _: InsertIntoTable =>
+      throw new IllegalStateException(s"logical plan ${queryExecution.logical} " +
+          s"is not supported currently")
+    case _ => queryExecution.logical
+  }
+
+  /** Method that generates a RDD for the given time */
+  override def compute(validTime: Time): Option[RDD[Row]] = {
+    StreamBaseRelation.setValidTime(validTime)
+    val converter = CatalystTypeConverters.createToScalaConverter(schema)
+    Some(queryExecution.executedPlan.execute().map(converter(_).asInstanceOf[Row]))
+  }
+
+  @transient private lazy val parentStreams = {
+    def traverse(plan: SparkPlan): Seq[DStream[InternalRow]] = plan match {
+      case x: StreamPlan => x.rowStream :: Nil
+      case _ => plan.children.flatMap(traverse)
+    }
+    val streams = traverse(queryExecution.executedPlan)
+    streams
+  }
+
+  /**
+    * Returns all column names as an array.
+    */
+  def columns: Array[String] = schema.fields.map(_.name)
+
+  def printSchema(): Unit = {
+    println(schema.treeString) // scalastyle:ignore
+  }
+}
