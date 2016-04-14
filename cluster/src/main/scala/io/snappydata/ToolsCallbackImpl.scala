4c67330e81d177022bda25aad3fdc5562fedec93
Sumedh Wale <swale@snappydata.io>
2016-04-15 02:04:47 +0530

    [SNAP-606] fixes to allow working core, store against Apache Spark (#216)
    ## Changes proposed in this pull request
    
    - Rewritten SQL parser based using parboiled2.org which ls also combinatorial PEG grammar like current Spark SQL one. Optimized version which is more than two orders of magnitude faster than Spark SQL combinatorial parser.
    - Refactored code to remove dependencies on snappy-spark extensions everywhere in core. Moved store dependencies to be inside core and everything else apart from cluster and memory related additions.
    - Introduced reflection in CodeGeneration to access some private methods since there does not seem to be a cleaner way to handle that.
    - Moved around code from ToolsCallback to ServiceUtils. Moved code from Services/ServiceManager into core to allow starting server. Leads/locators will continue to be available only in cluster.
    - Moved the WHERE subquery, EXISTS/IN support into SnappyExtendedParser only available in cluster mode
    - Build changes to allow core compilation with stock Spark 1.6.1. Fixes to DataWriter usage, appendable temp tables and others to allow doing this. (see more details in https://github.com/SnappyDataInc/snappydata/commit/20dc6c27509013c4a3966f03efa1695dc55ab493)
    
    Includes pending code reorganization:
    
    ```
    snappy-aqp               => aqp (currently defunct)
    snappy-core              => core
    snappy-tools             => cluster, datasource implementations moved into core
    snappy-examples          => examples
    snappy-spark             => spark
    snappy-store             => store
    snappy-tools/benchmark   => tests/benchmark
    tests-common             => tests/common
    snappy-dunits            => tests/dunit
    ```
    
    Moved around tests that are supposed to work only in embedded mode into cluster project, vs others that should work against stock Spark into core project. Dunits are still all running in embedded mode though some could be further split which can be done later.
    
    ## Patch testing
    
    All existing tests in core as well as most from store now run against stock Spark. Added a test "compare parser performance" in ColumnTableTest which checks for a decently complex TPCH q7 correctness and performance of Spark SQL vs Snappy vs GemXD vs Hive.
    
    ## Other PRs 
    
    Minor changes to store in https://github.com/SnappyDataInc/snappy-store/pull/40

---
+++ cluster/src/main/scala/io/snappydata/ToolsCallbackImpl.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+package io.snappydata
+
+import io.snappydata.impl.LeadImpl
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.catalyst.plans.physical.{OrderlessHashPartitioning, Partitioning}
+
+object ToolsCallbackImpl extends ToolsCallback {
+
+  override def invokeLeadStartAddonService(sc: SparkContext): Unit = {
+    LeadImpl.invokeLeadStartAddonService(sc)
+  }
+
+  def getOrderlessHashPartitioning(partitionColumns: Seq[Expression],
+      numPartitions: Int): Partitioning = OrderlessHashPartitioning(
+    partitionColumns, numPartitions)
+}
