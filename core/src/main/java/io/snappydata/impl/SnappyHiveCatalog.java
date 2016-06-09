b40a03a12b467ca1520c76a209df773694ce91b6
nthanvi <nthanvi@snappydata.io>
2016-06-09 10:41:34 +0530

    Fixed typo

--- core/src/main/java/io/snappydata/impl/SnappyHiveCatalog.java
+++ core/src/main/java/io/snappydata/impl/SnappyHiveCatalog.java
@@ -42,7 +42,6 @@ import org.apache.spark.sql.collection.Utils;
 import org.apache.spark.sql.hive.ExternalTableType;
 import org.apache.spark.sql.hive.QualifiedTableName;
 import org.apache.spark.sql.hive.SnappyStoreHiveCatalog;
-import org.apache.spark.sql.store.SnappyTable;
 import org.apache.spark.sql.store.StoreUtils;
 import org.apache.spark.sql.types.StructType;
 import org.apache.thrift.TException;
