abb05c55b62f5eef6f0ada64da1e4e9997194cf1
Swati Sawant <ssawant@snappydata.io>
2016-06-20 12:23:04 +0530

    Added support for starting of jobs by allowing passing of properties using APP_PROPS (#280)
    * - Added support for starting of jobs by allowing passing of properties using APP_PROPS
    
    * - Removed unnecessary log statements
    - Added support for passing APP_PROPS while executing snappy jobs
    - Added support for deleting streaming data generated during test run
    
    * Minor fomatting changes.

--- dtests/src/test/java/io/snappydata/hydra/cluster/SnappyPrms.java
+++ dtests/src/test/java/io/snappydata/hydra/cluster/SnappyPrms.java
@@ -111,6 +111,122 @@ public class SnappyPrms extends BasePrms {
      */
     public static Long waitTimeBeforeNextCycleVM;
 
+    /**
+     * Parameter used to get the user APP_PROPS for snappy job.
+     * (VectosetValues of Strings) A list of values for snappy-job.
+     */
+    public static Long appPropsForJobServer;
+
+    /**
+     * (int) number of executor cores to be used in test
+     */
+    public static Long executorCores;
+
+    /**
+     * (String) Maximun Result Size for Driver. Defaults to 1GB if not provided.
+     */
+    public static Long driverMaxResultSize;
+
+    /**
+     * (String) Local Memory. Defaults to 1GB if not provided.
+     */
+    public static Long locatorMemory;
+
+    /**
+     * (String) Memory to be used while starting the Server process. Defaults to 1GB if not provided.
+     */
+    public static Long serverMemory;
+
+    /**
+     * (String) Memory to be used while starting the Lead process. Defaults to 1GB if not provided.
+     */
+    public static Long leadMemory;
+
+    /**
+     * (String) sparkSchedulerMode. Defaults to 'FAIR' if not provided.
+     */
+    public static Long sparkSchedulerMode;
+
+    /**
+     * (int) sparkSqlBroadcastJoinThreshold
+     */
+    public static Long sparkSqlBroadcastJoinThreshold;
+
+    /**
+     * (boolean) - whether in-memory Columnar store needs to be compressed . Defaults to false if not provided.
+     */
+    public static Long compressedInMemoryColumnarStorage;
+
+    /**
+     * (boolean) - whether to use conserveSockets. Defaults to false if not provided.
+     */
+    public static Long conserveSockets;
+
+    /**
+     * (int) number of shuffle partitions to be used in test
+     */
+    public static Long shufflePartitions;
+
+    public static int getExecutorCores() {
+        Long key = executorCores;
+        return tasktab().intAt(key, tab().intAt(key, 1));
+    }
+
+    public static String getDriverMaxResultSize() {
+        Long key = driverMaxResultSize;
+        return tab().stringAt(key, "1g").toLowerCase();
+    }
+
+    public static String getLocatorMemory() {
+        Long key = locatorMemory;
+        return tab().stringAt(key, "1G");
+    }
+
+    public static String getServerMemory() {
+        Long key = serverMemory;
+        return tab().stringAt(key, "4G");
+    }
+
+    public static String getLeadMemory() {
+        Long key = leadMemory;
+        return tab().stringAt(key, "1G");
+    }
+
+    public static String getSparkSchedulerMode() {
+        Long key = sparkSchedulerMode;
+        return tab().stringAt(key, "FAIR");
+    }
+
+    public static int getSparkSqlBroadcastJoinThreshold() {
+        Long key = sparkSqlBroadcastJoinThreshold;
+        return tasktab().intAt(key, tab().intAt(key, -1));
+    }
+
+    public static boolean getCompressedInMemoryColumnarStorage() {
+        Long key = compressedInMemoryColumnarStorage;
+        return tasktab().booleanAt(key, tab().booleanAt(key, false));
+    }
+
+    public static boolean getConserveSockets() {
+        Long key = conserveSockets;
+        return tasktab().booleanAt(key, tab().booleanAt(key, false));
+    }
+
+    public static int getShufflePartitions() {
+        Long key = shufflePartitions;
+        return tasktab().intAt(key, tab().intAt(key, 1));
+    }
+
+    public static String getCommaSepAPPProps() {
+        Long key = appPropsForJobServer;
+        return BasePrms.tasktab().stringAt(key, BasePrms.tab().stringAt(key, null));
+    }
+
+    public static boolean getTableDefaultDataPolicy() {
+        Long key = tableDefaultPartitioned;
+        return tasktab().booleanAt(key, tab().booleanAt(key, false));
+    }
+
     public static Vector getSQLScriptNamesForInitTask() {
         Long key = sqlScriptNamesForInitTask;
         return BasePrms.tasktab().vecAt(key, BasePrms.tab().vecAt(key, null));
