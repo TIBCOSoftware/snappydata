c7b61ab30e7ab63da46da3f132d1c5fb2101a51a
nthanvi <nthanvi@snappydata.io>
2016-07-15 09:38:58 +0530

    Snap 908 (#303)
    * synchronizing store
    
    * shows the correct row count for column table
    row buffer details are removed

--- core/src/main/scala/org/apache/spark/sql/execution/ui/SnappyStatsPage.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/ui/SnappyStatsPage.scala
@@ -33,29 +33,36 @@ import org.apache.spark.util.Utils
 private[ui] class SnappyStatsPage(parent: SnappyStatsTab)
     extends WebUIPage("") with Logging {
   val numFormatter = java.text.NumberFormat.getIntegerInstance
+
   def render(request: HttpServletRequest): Seq[Node] = {
     val uiDetails = StoreTableSizeProvider.getTableSizes
-    val snappyRowTable = UIUtils.listingTable(
-      rowHeader, rowTable, uiDetails.filter(row => !row.isColumnTable))
-    val snappyColumnTable = UIUtils.listingTable(
-      columnHeader, columnTable, uiDetails.filter(row => row.isColumnTable))
 
-    val content =
+    val rowList = uiDetails._1
+    val columnList = uiDetails._2
+
+    val rowNodes = if (!rowList.isEmpty) {
       <span>
-        <h4>Snappy Row Tables</h4>{snappyRowTable}<h4>Snappy Column Tables</h4>{snappyColumnTable}
+        <h4>Snappy Row Tables</h4>{UIUtils.listingTable(rowHeader, rowTable, rowList)}
       </span>
+    } else Nil
 
-    UIUtils.headerSparkPage("Snappy Store", content, parent, Some(500))
+    val colNodes = if (!columnList.isEmpty) {
+      <span>
+        <h4>Snappy Column Tables</h4>{UIUtils.listingTable(columnHeader, columnTable, columnList)}
+      </span>
+    } else Nil
+
+    UIUtils.headerSparkPage("Snappy Store", rowNodes ++ colNodes, parent, Some(500))
+
   }
 
-  private def rowHeader = Seq("TableName", "TotalSize" , "TotalRows")
+  private def rowHeader = Seq("Table Name", "Total Size" , "Total Rows")
 
-  private def columnHeader = Seq("TableName", "Row Buffer Size", "Row Buffer Rows",
-    "Column Store Size", "Column Store Rows", "TotalSize")
+  private def columnHeader = Seq("Table Name", "Total Size" , "Total Rows")
 
   private def rowTable(stats: UIAnalytics) = {
     <tr>
-      <td>
+      <td sorttable_customkey={stats.tableName}>
         {stats.tableName}
       </td>
       <td sorttable_customkey={stats.rowBufferSize.toString}>
@@ -69,24 +76,16 @@ private[ui] class SnappyStatsPage(parent: SnappyStatsTab)
 
   private def columnTable(stats: UIAnalytics) = {
     val totalSize = stats.rowBufferSize + stats.columnBufferSize
+    val totalrows = stats.rowBufferCount + stats.columnBufferCount
     <tr>
-      <td>
+      <td sorttable_customkey={stats.tableName}>
         {stats.tableName}
-      </td>
-      <td sorttable_customkey={stats.rowBufferSize.toString}>
-        {Utils.bytesToString(stats.rowBufferSize)}
-      </td>
-      <td sorttable_customkey={stats.rowBufferCount.toString}>
-        {numFormatter.format(stats.rowBufferCount)}
-      </td>
-      <td sorttable_customkey={stats.columnBufferSize.toString}>
-        {Utils.bytesToString(stats.columnBufferSize)}
-      </td>
-      <td sorttable_customkey={stats.columnBufferCount.toString}>
-        {numFormatter.format(stats.columnBufferCount)}
       </td>
       <td sorttable_customkey={totalSize.toString}>
         {Utils.bytesToString(totalSize)}
+      </td>
+      <td sorttable_customkey={totalrows.toString}>
+        {numFormatter.format(totalrows)}
       </td>
     </tr>
   }
