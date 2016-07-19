7d32d5a502cdd9c7aad4fdbcc502b29ac5c9cc31
Hemant Bhanawat <hemant@snappydata.io>
2016-07-19 14:56:17 +0530

    Simulated index support by duplicating tables and partitioning them on the column on which they are going to be joined.

--- cluster/src/test/scala/io/snappydata/benchmark/snappy/TPCH_Snappy_Query.scala
+++ cluster/src/test/scala/io/snappydata/benchmark/snappy/TPCH_Snappy_Query.scala
@@ -13,8 +13,8 @@ object TPCH_Snappy_Query extends SnappySQLJob{
    var sqlSparkProperties: Array[String] = _
    var queryPlan : Boolean = false
    var queries:Array[String] = _
-
-//  var avgFileStream: FileOutputStream = _
+    var useIndex: Boolean = _
+  //  var avgFileStream: FileOutputStream = _
 //  var avgPrintStream:PrintStream = _
 
    override def runJob(snc: C, jobConfig: Config): Any = {
@@ -34,7 +34,7 @@ object TPCH_Snappy_Query extends SnappySQLJob{
      }
 
      if (queryPlan) {
-       TPCH_Snappy.queryPlan(snc, isSnappy)
+       TPCH_Snappy.queryPlan(snc, isSnappy, useIndex)
      }
 
      println(s"****************queries : $queries")
@@ -42,28 +42,28 @@ object TPCH_Snappy_Query extends SnappySQLJob{
      for(i <- 1 to 1) {
        for(query <- queries)
          query match {
-           case "1" =>   TPCH_Snappy.execute("q1", snc, isResultCollection, isSnappy, i)
-           case "2" =>   TPCH_Snappy.execute("q2", snc,isResultCollection, isSnappy, i)//taking hours to execute in Snappy
-           case "3"=>   TPCH_Snappy.execute("q3", snc, isResultCollection, isSnappy, i)
-           case "4" =>   TPCH_Snappy.execute("q4", snc, isResultCollection, isSnappy, i)
-           case "5" =>   TPCH_Snappy.execute("q5", snc, isResultCollection, isSnappy, i)
-           case "6" =>   TPCH_Snappy.execute("q6", snc, isResultCollection, isSnappy, i)
-           case "7" =>   TPCH_Snappy.execute("q7", snc, isResultCollection, isSnappy, i)
-           case "8" =>   TPCH_Snappy.execute("q8", snc, isResultCollection, isSnappy, i)
-           case "9" =>   TPCH_Snappy.execute("q9", snc, isResultCollection, isSnappy, i) //taking hours to execute in Snappy
-           case "10" =>   TPCH_Snappy.execute("q10", snc, isResultCollection, isSnappy, i)
-           case "11" =>   TPCH_Snappy.execute("q11", snc, isResultCollection, isSnappy, i)
-           case "12" =>   TPCH_Snappy.execute("q12", snc, isResultCollection, isSnappy, i)
-           case "13" =>   TPCH_Snappy.execute("q13", snc, isResultCollection, isSnappy, i)
-           case "14" =>   TPCH_Snappy.execute("q14", snc, isResultCollection, isSnappy, i)
-           case "15" =>   TPCH_Snappy.execute("q15", snc, isResultCollection, isSnappy, i)
-           case "16" =>   TPCH_Snappy.execute("q16", snc, isResultCollection, isSnappy, i)
-           case "17" =>   TPCH_Snappy.execute("q17", snc, isResultCollection, isSnappy, i)
-           case "18" =>   TPCH_Snappy.execute("q18", snc, isResultCollection, isSnappy, i)
-           case "19" =>   TPCH_Snappy.execute("q19", snc,isResultCollection, isSnappy, i) //not working in local mode hence not executing it for cluster mode too
-           case "20" =>   TPCH_Snappy.execute("q20", snc, isResultCollection, isSnappy, i)
-           case "21" =>   TPCH_Snappy.execute("q21", snc,isResultCollection, isSnappy, i) //not working in local mode hence not executing it for cluster mode too
-           case "22" =>   TPCH_Snappy.execute("q22", snc, isResultCollection, isSnappy, i)
+           case "1" =>   TPCH_Snappy.execute("q1", snc, isResultCollection, isSnappy, i, useIndex)
+           case "2" =>   TPCH_Snappy.execute("q2", snc,isResultCollection, isSnappy, i, useIndex)//taking hours to execute in Snappy
+           case "3"=>   TPCH_Snappy.execute("q3", snc, isResultCollection, isSnappy, i, useIndex)
+           case "4" =>   TPCH_Snappy.execute("q4", snc, isResultCollection, isSnappy, i, useIndex)
+           case "5" =>   TPCH_Snappy.execute("q5", snc, isResultCollection, isSnappy, i, useIndex)
+           case "6" =>   TPCH_Snappy.execute("q6", snc, isResultCollection, isSnappy, i, useIndex)
+           case "7" =>   TPCH_Snappy.execute("q7", snc, isResultCollection, isSnappy, i, useIndex)
+           case "8" =>   TPCH_Snappy.execute("q8", snc, isResultCollection, isSnappy, i, useIndex)
+           case "9" =>   TPCH_Snappy.execute("q9", snc, isResultCollection, isSnappy, i, useIndex) //taking hours to execute in Snappy
+           case "10" =>   TPCH_Snappy.execute("q10", snc, isResultCollection, isSnappy, i, useIndex)
+           case "11" =>   TPCH_Snappy.execute("q11", snc, isResultCollection, isSnappy, i, useIndex)
+           case "12" =>   TPCH_Snappy.execute("q12", snc, isResultCollection, isSnappy, i, useIndex)
+           case "13" =>   TPCH_Snappy.execute("q13", snc, isResultCollection, isSnappy, i, useIndex)
+           case "14" =>   TPCH_Snappy.execute("q14", snc, isResultCollection, isSnappy, i, useIndex)
+           case "15" =>   TPCH_Snappy.execute("q15", snc, isResultCollection, isSnappy, i, useIndex)
+           case "16" =>   TPCH_Snappy.execute("q16", snc, isResultCollection, isSnappy, i, useIndex)
+           case "17" =>   TPCH_Snappy.execute("q17", snc, isResultCollection, isSnappy, i, useIndex)
+           case "18" =>   TPCH_Snappy.execute("q18", snc, isResultCollection, isSnappy, i, useIndex)
+           case "19" =>   TPCH_Snappy.execute("q19", snc,isResultCollection, isSnappy, i, useIndex) //not working in local mode hence not executing it for cluster mode too
+           case "20" =>   TPCH_Snappy.execute("q20", snc, isResultCollection, isSnappy, i, useIndex)
+           case "21" =>   TPCH_Snappy.execute("q21", snc,isResultCollection, isSnappy, i, useIndex) //not working in local mode hence not executing it for cluster mode too
+           case "22" =>   TPCH_Snappy.execute("q22", snc, isResultCollection, isSnappy, i, useIndex)
              println("---------------------------------------------------------------------------------")
          }
 
@@ -112,6 +112,12 @@ object TPCH_Snappy_Query extends SnappySQLJob{
        config.getString("queries")
      } else {
        return new SparkJobInvalid("Specify Query number to be executed")
+     }
+
+     useIndex = if (config.hasPath("useIndex")) {
+       config.getBoolean("useIndex")
+     } else {
+       return new SparkJobInvalid("Specify whether to use Index")
      }
 
      println(s"tempqueries : $tempqueries")
