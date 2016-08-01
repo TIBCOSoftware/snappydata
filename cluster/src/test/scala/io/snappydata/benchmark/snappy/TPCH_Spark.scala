65fcf588142b45f5f15abb1c6cca0614a9e83c24
kbachhav <kbachhav@snappydata.io>
2016-08-01 16:39:49 +0530

    -- TPCH Changes   1. Earlier Queries for snappy and spark were different. Now same query will be used for both.Commented code is still there.   2. warmupRuns and RunsforAverage are configurable for snappy, memsql, spark   3. plan generation using explain is removed. plan will be generated when query results will be collected   4. Q11 and Q15 are changed

--- cluster/src/test/scala/io/snappydata/benchmark/snappy/TPCH_Spark.scala
+++ cluster/src/test/scala/io/snappydata/benchmark/snappy/TPCH_Spark.scala
@@ -10,132 +10,78 @@ import org.apache.spark.{SparkConf, SparkContext}
  */
 object TPCH_Spark {
 
-  def main (args: Array[String]) {
-    val conf = new SparkConf().setAppName("TPCH_Spark")/*.set("snappydata.store.locators","localhost:10334")*/
-
-//    val urlString = s"jdbc:snappydata:;locators=${args(0)}:10334;persist-dd=false;" +
-//        "member-timeout=90000;jmx-manager-start=false;enable-time-statistics=true;statistic-sampling-enabled=true"
-//
-//    //URL 'jdbc:gemfirexd:;mcast-port=45672;persist-dd=false;'
-//    val usingOptionString = s"""
-//          USING row
-//          OPTIONS ()
-//              URL '$urlString',
-//              Driver 'com.pivotal.gemfirexd.jdbc.EmbeddedDriver'
-//          )"""
-//
-//    val props = Map(
-//      //"url" -> "jdbc:gemfirexd:;mcast-port=45672;persist-dd=false;user=app;password=app",
-//      "url" -> urlString,
-//      "driver" -> "com.pivotal.gemfirexd.jdbc.EmbeddedDriver",
-//      "poolImpl" -> "tomcat",
-//      "poolProperties" -> "maxActive=300"
-////      "user" -> "app",
-////      "password" -> "app"
-//    )
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("TPCH_Spark") /*.set("snappydata.store.locators","localhost:10334")*/
 
     val usingOptionString = null
     val props = null
-    val isResultCollection = false
-    val isSnappy = false
+    var isSnappy = false
     val sc = new SparkContext(conf)
     val snc = new SQLContext(sc)
-    val buckets= null
+    val buckets = null
     val useIndex = false
-//    var avgFileStream: FileOutputStream = new FileOutputStream(new File(s"Average.out"))
-//    var avgPrintStream:PrintStream = new PrintStream(avgFileStream)
 
     val path = args(0)
-    val queries = args(1).split(",")
-    val queryPlan = args(2).toBoolean
+    val queries = args(1).split("-")
+    var isResultCollection : Boolean = args(2).toBoolean
+    var warmup : Integer = args(3).toInt
+    var runsForAverage : Integer = args(4).toInt
+    println(s"KBKBKB : sqlProperties : ${args(5)}")
+    var sqlSparkProperties = args(5).split(",")
+
 
     TPCHColumnPartitionedTable.createAndPopulateOrderTable(props, snc, path, isSnappy, buckets)
     TPCHColumnPartitionedTable.createAndPopulateLineItemTable(props, snc, path, isSnappy, buckets)
     TPCHReplicatedTable.createPopulateRegionTable(usingOptionString, props, snc, path, isSnappy)
     TPCHReplicatedTable.createPopulateNationTable(usingOptionString, props, snc, path, isSnappy)
     TPCHReplicatedTable.createPopulateSupplierTable(usingOptionString, props, snc, path, isSnappy)
-//    TPCHRowPartitionedTable.createPopulateCustomerTable(usingOptionString, props, snc, path, isSnappy, buckets)
-//    TPCHRowPartitionedTable.createPopulatePartTable(usingOptionString, props, snc, path, isSnappy, buckets)
-//    TPCHRowPartitionedTable.createPopulatePartSuppTable(usingOptionString, props, snc, path, isSnappy, buckets)
     TPCHColumnPartitionedTable.createPopulateCustomerTable(usingOptionString, props, snc, path, isSnappy, buckets)
     TPCHColumnPartitionedTable.createPopulatePartTable(usingOptionString, props, snc, path, isSnappy, buckets)
     TPCHColumnPartitionedTable.createPopulatePartSuppTable(usingOptionString, props, snc, path, isSnappy, buckets)
-    if (useIndex) {
-      TPCHColumnPartitionedTable.createAndPopulateOrder_CustTable(props, snc, path, isSnappy, buckets)
-      TPCHColumnPartitionedTable.createAndPopulateLineItem_partTable(props, snc, path, isSnappy, buckets)
-    }
-    if (queryPlan) {
-      TPCH_Snappy.queryPlan(snc, isSnappy, useIndex)
+
+//    snc.sql(s"set spark.sql.shuffle.partitions=83")
+//    snc.sql(s"set spark.sql.inMemoryColumnarStorage.compressed=false")
+//    snc.sql(s"set spark.sql.autoBroadcastJoinThreshold=41943040")
+    //snc.sql(s"set spark.sql.crossJoin.enabled = true")
+    for(prop <- sqlSparkProperties) {
+      println(prop)
+      snc.sql(s"set $prop")
     }
 
-
-    for(i <- 1 to 1) {
-      for(query <- queries) {
+    for (i <- 1 to 1) {
+      println("KBKBKB : Going to execute queries")
+      for (query <- queries) {
         query match {
-          case "1" => TPCH_Snappy.execute("q1", snc, isResultCollection, isSnappy, i, useIndex)
-          case "2" => TPCH_Snappy.execute("q2", snc, isResultCollection, isSnappy, i, useIndex) //taking hours to execute in Snappy
-          case "3" => TPCH_Snappy.execute("q3", snc, isResultCollection, isSnappy, i, useIndex)
-          case "4" => TPCH_Snappy.execute("q4", snc, isResultCollection, isSnappy, i, useIndex)
-          case "5" => TPCH_Snappy.execute("q5", snc, isResultCollection, isSnappy, i, useIndex)
-          case "6" => TPCH_Snappy.execute("q6", snc, isResultCollection, isSnappy, i, useIndex)
-          case "7" => TPCH_Snappy.execute("q7", snc, isResultCollection, isSnappy, i, useIndex)
-          case "8" => TPCH_Snappy.execute("q8", snc, isResultCollection, isSnappy, i, useIndex)
-          case "9" => TPCH_Snappy.execute("q9", snc, isResultCollection, isSnappy, i, useIndex) //taking hours to execute in Snappy
-          case "10" => TPCH_Snappy.execute("q10", snc, isResultCollection, isSnappy, i, useIndex)
-          case "11" => TPCH_Snappy.execute("q11", snc, isResultCollection, isSnappy, i, useIndex)
-          case "12" => TPCH_Snappy.execute("q12", snc, isResultCollection, isSnappy, i, useIndex)
-          case "13" => TPCH_Snappy.execute("q13", snc, isResultCollection, isSnappy, i, useIndex)
-          case "14" => TPCH_Snappy.execute("q14", snc, isResultCollection, isSnappy, i, useIndex)
-          case "15" => TPCH_Snappy.execute("q15", snc, isResultCollection, isSnappy, i, useIndex)
-          case "16" => TPCH_Snappy.execute("q16", snc, isResultCollection, isSnappy, i, useIndex)
-          case "17" => TPCH_Snappy.execute("q17", snc, isResultCollection, isSnappy, i, useIndex)
-          case "18" => TPCH_Snappy.execute("q18", snc, isResultCollection, isSnappy, i, useIndex)
-          case "19" => TPCH_Snappy.execute("q19", snc, isResultCollection, isSnappy, i, useIndex) //not working in local mode hence not executing it for cluster mode too
-          case "20" => TPCH_Snappy.execute("q20", snc, isResultCollection, isSnappy, i, useIndex)
-          case "21" => TPCH_Snappy.execute("q21", snc, isResultCollection, isSnappy, i, useIndex) //not working in local mode hence not executing it for cluster mode too
-          case "22" => TPCH_Snappy.execute("q22", snc, isResultCollection, isSnappy, i, useIndex)
+          case "1" => TPCH_Snappy.execute("q1", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "2" => TPCH_Snappy.execute("q2", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "3" => TPCH_Snappy.execute("q3", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "4" => TPCH_Snappy.execute("q4", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "5" => TPCH_Snappy.execute("q5", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "6" => TPCH_Snappy.execute("q6", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "7" => TPCH_Snappy.execute("q7", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "8" => TPCH_Snappy.execute("q8", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "9" => TPCH_Snappy.execute("q9", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "10" => TPCH_Snappy.execute("q10", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "11" => TPCH_Snappy.execute("q11", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "12" => TPCH_Snappy.execute("q12", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "13" => TPCH_Snappy.execute("q13", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "14" => TPCH_Snappy.execute("q14", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "15" => TPCH_Snappy.execute("q15", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "16" => TPCH_Snappy.execute("q16", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "17" => TPCH_Snappy.execute("q17", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "18" => TPCH_Snappy.execute("q18", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "19" => TPCH_Snappy.execute("q19", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "20" => TPCH_Snappy.execute("q20", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "21" => TPCH_Snappy.execute("q21", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
+          case "22" => TPCH_Snappy.execute("q22", snc, isResultCollection, isSnappy, i, useIndex, warmup, runsForAverage)
             println("---------------------------------------------------------------------------------")
         }
       }
     }
-
-//    TPCH_Snappy.execute("q1", snc, isResultCollection, isSnappy,1)
-//    //TPCH_Snappy.execute("q2", snc,isResultCollection, isSnappy, 1)
-//    TPCH_Snappy.execute("q3", snc, isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q4", snc,isResultCollection, isSnappy, 1)
-//    TPCH_Snappy.execute("q5", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q6", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q7", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q8", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q9", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q10", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q11", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q12", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q13", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q14", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q15", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q16", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q17", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q18", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q19", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy.execute("q20", snc,isResultCollection, isSnappy,1)
-//    //TPCH_Snappy_Query.execute("q21", sc,isResultCollection, isSnappy)
-//    TPCH_Snappy.execute("q22", snc,isResultCollection, isSnappy,1)
-//    TPCH_Snappy_Query.execute("q1s", sc, isResultCollection, isSnappy)
-//    TPCH_Snappy_Query.execute("q3s", sc, isResultCollection, isSnappy)
-//    TPCH_Snappy_Query.execute("q5s", sc,isResultCollection, isSnappy)
-//    TPCH_Snappy_Query.execute("q6s", sc,isResultCollection, isSnappy)
-//    TPCH_Snappy_Query.execute("q10s", sc,isResultCollection, isSnappy)
-
-//
-//    while(true){
-//
-//    }
-
     TPCH_Snappy.close()
     sc.stop()
 
-}
+  }
 }
 
 
--- tests/benchmark/memsql/5_ExecuteQuery.sh
+++ tests/benchmark/memsql/5_ExecuteQuery.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source PerfRun.conf
 
-scala -cp "$TPCHJar:$mysqlConnectorJar" io.snappydata.benchmark.memsql.TPCH_Memsql_Query $aggregator $queries
+scala -cp "$TPCHJar:$mysqlConnectorJar" io.snappydata.benchmark.memsql.TPCH_Memsql_Query $aggregator $port $queries  $ResultCollection $WarmupRuns $AverageRuns
