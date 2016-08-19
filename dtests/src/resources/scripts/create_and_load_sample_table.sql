b0e11b30a039108830e27d70ec41062638ecb157
Swati Sawant <ssawant@snappydata.io>
2016-08-19 16:27:16 +0530

    Added support for starting multiple locators through framework and test for clusterRestartWithPersistentRecovery (#333)
    * - Added test for snappy cluster restart with the persistent partitioned column table
    
    * - Added test for snappy split mode cluster restart with columnar persistent tables
    
    * - Added support for starting multiple locators in the snappy cluster
    
    * - Added spark app to load data.
    
    * - Added support to wait for spark job until its done.
    
    * - Fixed issues with multiple locators configuration
    - Added job for loading csv file data in row and column tables
    
    * - Added support for providing inMemoryColumnarStorageBatchSize
    
    * Added a fix in job to lead data for all row tables.
    
    * - Added few parameters in test. e.g. serverMemory, leadmemory, etc.
    - Fixed few issues in spark job created for loading csv data into airline, airlineref and sample table.
    
    * Minor code formatting changes done.
    
    * - Code formatting changes done
    - Parameterized the data location
    
    * - Added search string parameter for spark job. There is no way to know the status of spark job, so, added a parameter to be specified while running spark job.
    - The parameter value is used to know the status of the spark job.
    
    * Modified the script as per the modified syntax for persistent clause.
    
    * - Added support in framework to wait for spark job completion before scheduling next task.
    
    * - Minor change in Error message
    
    * - Parameterized the persistence mode required while running sql scripts.
    - This will enable to use same scripts with different persistence mode in different tests.
    
    * - Incorporated few review comments.
    
    * Incorporated review comment
    
    * - Added SnappyCustomSparkListener in order to know the status of the spark application submitted through spark-submit.
    
    * - Removing unused scala app. This was added for reproducing reactive core issue with airline data. But now, we have data generated for reactive core use case.
    
    * - Renamed the bt and conf files in order to remove the customer name.

---
+++ dtests/src/resources/scripts/create_and_load_sample_table.sql
@@ -0,0 +1,19 @@
+--- DROP TABLE IF ALREADY EXISTS ---
+DROP TABLE IF EXISTS AIRLINE_SAMPLE ;
+
+--- CREATE SAMPLE TABLE ---
+CREATE SAMPLE TABLE AIRLINE_SAMPLE ON AIRLINE
+  OPTIONS(
+    buckets '7',
+    qcs 'UniqueCarrier, Year_, Month_',
+    fraction '0.03',
+    strataReservoirSize '50', PERSISTENT ':persistenceMode')
+  AS (
+    SELECT Year_, Month_ , DayOfMonth,
+      DayOfWeek, DepTime, CRSDepTime, ArrTime, CRSArrTime,
+      UniqueCarrier, FlightNum, TailNum, ActualElapsedTime,
+      CRSElapsedTime, AirTime, ArrDelay, DepDelay, Origin,
+      Dest, Distance, TaxiIn, TaxiOut, Cancelled, CancellationCode,
+      Diverted, CarrierDelay, WeatherDelay, NASDelay, SecurityDelay,
+      LateAircraftDelay, ArrDelaySlot
+    FROM AIRLINE);
