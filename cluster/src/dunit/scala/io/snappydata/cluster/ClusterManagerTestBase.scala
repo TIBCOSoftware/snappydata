6bf93e0f1394d75c477c6ea65a8f6be9372dd169
hbhanawat <hemant9379@gmail.com>
2016-09-01 17:23:23 +0530

    Snap 1006 (#354)
    When we configure the test, we configure the initialcapacity of the region. But, this was happening only after the test vms were started for the first time. So, the first time initialcapacity is set as the default value but on recovery the test sets initial capacity as a different value. If the region attributes change on recovery, the recovered entries are inserted into the region map (otherwise the recovered entries are marked for the current region).
    
    So the main issue is - When the recovered entries are inserted into the region map it tries to access the bucket region and fails because it has not been initialized. We need not worry about incompatibility during restarts as user has no control to do it when the system is restarted. 
    
    For the test made the configuration consistent to fix the issue at hand.

--- cluster/src/dunit/scala/io/snappydata/cluster/ClusterManagerTestBase.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/ClusterManagerTestBase.scala
@@ -76,6 +76,7 @@ class ClusterManagerTestBase(s: String) extends DistributedTestBase(s) {
 
   override def beforeClass(): Unit = {
     super.beforeClass()
+    dosetUp()
     val locNetPort = locatorNetPort
     val locNetProps = locatorNetProps
     val locPort = ClusterManagerTestBase.locPort
@@ -95,7 +96,6 @@ class ClusterManagerTestBase(s: String) extends DistributedTestBase(s) {
         logger.info("\n\n\n  STARTING TESTS IN " + getClass.getName + "\n\n")
       }
     })
-
     val nodeProps = bootProps
     val startNode = new SerializableRunnable() {
       override def run(): Unit = {
@@ -126,6 +126,10 @@ class ClusterManagerTestBase(s: String) extends DistributedTestBase(s) {
 
   override def setUp(): Unit = {
     super.setUp()
+    dosetUp()
+  }
+
+  private def dosetUp() : Unit = {
     val testName = getName
     val testClass = getClass
     // bootProps.setProperty(Attribute.SYS_PERSISTENT_DIR, s)
