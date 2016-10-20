682ee1859ec8359954ccfef172c9f76dbe003eaa
nthanvi <nthanvi@snappydata.io>
2016-10-20 13:04:04 +0530

    minor changes

--- core/src/main/scala/io/snappydata/impl/ServerImpl.scala
+++ core/src/main/scala/io/snappydata/impl/ServerImpl.scala
@@ -23,15 +23,13 @@ import com.pivotal.gemfirexd.internal.engine.fabricservice.FabricServerImpl
 import io.snappydata.{ProtocolOverrides, Server}
 
 /**
- * This class ties up few things that is Snappy specific.
- * for e.g. Connection url & ClusterCallback
- */
+  * This class ties up few things that is Snappy specific.
+  * for e.g. Connection url & ClusterCallback
+  */
 class ServerImpl extends FabricServerImpl with Server with ProtocolOverrides {
 
   @throws(classOf[SQLException])
-  override def start(bootProperties: Properties): Unit = {
-    start(bootProperties, false)
-  }
+  override def start(bootProperties: Properties): Unit = start(bootProperties, false)
 
   override def isServer: Boolean = true
 }
