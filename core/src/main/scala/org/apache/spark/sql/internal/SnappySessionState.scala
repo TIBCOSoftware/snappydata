54ee8c20a206248af5b61678641bc4e5ae04638a
Sumedh Wale <swale@snappydata.io>
2016-11-03 14:19:21 +0530

    use all prepare stages for CollectAggregateExec.doExecute
    refactored the "preparations" of QueryExecution to be in SnappySessionState

--- core/src/main/scala/org/apache/spark/sql/internal/SnappySessionState.scala
+++ core/src/main/scala/org/apache/spark/sql/internal/SnappySessionState.scala
@@ -32,8 +32,9 @@ import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, Cast}
 import org.apache.spark.sql.catalyst.plans.logical.{InsertIntoTable, LogicalPlan, Project}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.collection.Utils
+import org.apache.spark.sql.execution._
 import org.apache.spark.sql.execution.datasources.{DataSourceAnalysis, FindDataSourceTable, HadoopFsRelation, LogicalRelation, ResolveDataSource, StoreDataSourceStrategy}
-import org.apache.spark.sql.execution.{QueryExecution, SparkOptimizer, SparkPlan, SparkPlanner, datasources}
+import org.apache.spark.sql.execution.exchange.{EnsureRequirements, ReuseExchange}
 import org.apache.spark.sql.hive.SnappyStoreHiveCatalog
 import org.apache.spark.sql.sources.{BaseRelation, InsertableRelation, PutIntoTable, RowInsertableRelation, RowPutRelation, SchemaInsertableRelation, StoreStrategy}
 import org.apache.spark.sql.store.StoreUtils
@@ -152,6 +153,18 @@ class SnappySessionState(snappySession: SnappySession)
   override def executePlan(plan: LogicalPlan): QueryExecution = {
     clearExecutionData()
     contextFunctions.executePlan(snappySession, plan)
+  }
+
+  protected[sql] def queryPreparations: Seq[Rule[SparkPlan]] = Seq(
+    python.ExtractPythonUDFs,
+    PlanSubqueries(snappySession),
+    EnsureRequirements(snappySession.sessionState.conf),
+    CollapseCodegenStages(snappySession.sessionState.conf),
+    ReuseExchange(snappySession.sessionState.conf))
+
+  private[spark] def prepareExecution(plan: SparkPlan): SparkPlan = {
+    clearExecutionData()
+    queryPreparations.foldLeft(plan) { case (sp, rule) => rule.apply(sp) }
   }
 
   private[spark] def clearExecutionData(): Unit = {
