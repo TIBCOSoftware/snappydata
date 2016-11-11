14c6d66b16ee6f6dd602f6d84db9c2cf2fef614c
Sumedh Wale <swale@snappydata.io>
2016-11-11 13:51:49 +0530

    correcting scalastyle and formatting errors

--- cluster/src/test/scala/org/apache/spark/sql/execution/benchmark/TAQ/SnappyTaq.scala
+++ cluster/src/test/scala/org/apache/spark/sql/execution/benchmark/TAQ/SnappyTaq.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
 package org.apache.spark.sql.execution.benchmark.TAQ
 
 import java.io.{File, FileOutputStream, PrintStream}
@@ -7,20 +23,16 @@ import java.util.{Calendar, GregorianCalendar, Random}
 import com.typesafe.config.Config
 
 import org.apache.spark.sql._
-import org.apache.spark.sql.execution.benchmark.{Quote, Trade}
 import org.apache.spark.sql.types.{Decimal, StringType, StructField, StructType}
 
 
-/**
- * Created by kishor on 1/11/16.
- */
-object SnappyTaq extends SnappySQLJob{
+object SnappyTaq extends SnappySQLJob {
 
   var path: String = _
   val quoteSize = 34000000L
   val tradeSize = 5000000L
   val numDays = 1
-  val d="2016-06-06"
+  val d = "2016-06-06"
 
   var sqlSparkProperties: Array[String] = _
   var isResultCollection: Boolean = _
@@ -39,10 +51,10 @@ object SnappyTaq extends SnappySQLJob{
   override def runSnappyJob(session: SnappyContext, jobConfig: Config): Any = {
 
     import session.implicits._
-   var queryPerfFileStream: FileOutputStream = new FileOutputStream(new File("SnappyTaq.out"))
-   var queryPrintStream:PrintStream = new PrintStream(queryPerfFileStream)
-   var avgPerfFileStream: FileOutputStream = new FileOutputStream(new File("Average.out"))
-   var avgPrintStream:PrintStream = new PrintStream(avgPerfFileStream)
+    val queryPerfFileStream: FileOutputStream = new FileOutputStream(new File("SnappyTaq.out"))
+    val queryPrintStream: PrintStream = new PrintStream(queryPerfFileStream)
+    val avgPerfFileStream: FileOutputStream = new FileOutputStream(new File("Average.out"))
+    val avgPrintStream: PrintStream = new PrintStream(avgPerfFileStream)
 
     session.dropTable("QUOTE", ifExists = true)
     session.dropTable("TRADE", ifExists = true)
@@ -135,19 +147,24 @@ object SnappyTaq extends SnappySQLJob{
     quoteDataDF.write.insertInto("quote")
     tradeDataDF.write.insertInto("trade")
 
-    //session.sql(s"insert into S select sym from trade where date=add_months($d,0) group by sym order by count(*) desc fetch first 100 rows only")
+    // session.sql(s"insert into S select sym from trade " +
+    //   s"where date=add_months($d,0) group by sym order by count(*) desc " +
+    //   "fetch first 100 rows only")
     symDF.write.insertInto("S")
 
 
     try {
-       var queryCount = 0;
-      for(query<- queries) {
+      var queryCount = 0
+      for (query <- queries) {
         queryCount += 1
 
+        // scalastyle:off
         println(s"Started executing $query")
         queryPrintStream.println(s"Started executing $query")
+        // scalastyle:on
         if (isResultCollection) {
-          val resultSet = session.sql(query).collect
+          val resultSet = session.sql(query).collect()
+          // scalastyle:off
           println(s"$query : ${resultSet.length}")
 
           for (row <- resultSet) {
@@ -157,57 +174,58 @@ object SnappyTaq extends SnappySQLJob{
             }.mkString(","))
           }
           queryPrintStream.println()
+          // scalastyle:on
         } else {
           var totalTimeForLast5Iterations: Long = 0
-          var bestTime: Long=0
+          var bestTime: Long = 0
+          // scalastyle:off
           queryPrintStream.println(queryCount)
+          // scalastyle:on
           for (i <- 1 to (warmUp + runsForAverage)) {
             val startTime = System.currentTimeMillis()
             var cnts: Array[Row] = null
             if (i == 1) {
-              cnts = session.sql(query).collect
+              cnts = session.sql(query).collect()
             } else {
-              cnts = session.sql(query).collect
+              cnts = session.sql(query).collect()
             }
             for (s <- cnts) {
-              //just iterating over result
+              // just iterating over result
             }
             val endTime = System.currentTimeMillis()
             val iterationTime = endTime - startTime
-            if(i==1){
+            if (i == 1) {
               bestTime = iterationTime
-            }else{
-              if(iterationTime < bestTime)
-                bestTime = iterationTime
+            } else {
+              if (iterationTime < bestTime) bestTime = iterationTime
             }
+            // scalastyle:off
             queryPrintStream.println(s"$iterationTime")
+            // scalastyle:on
             if (i > warmUp) {
               totalTimeForLast5Iterations += iterationTime
             }
             cnts = null
           }
+          // scalastyle:off
           queryPrintStream.println(s"${totalTimeForLast5Iterations / runsForAverage}")
-          avgPrintStream.println(s"$queryCount,$bestTime / ${totalTimeForLast5Iterations /runsForAverage}")
+          avgPrintStream.println(s"$queryCount,$bestTime / " +
+              s"${totalTimeForLast5Iterations / runsForAverage}")
           println(s"Finished executing $queryCount")
+          // scalastyle:on
         }
       }
     } catch {
-      case e: Exception => {
+      case e: Exception =>
         e.printStackTrace(queryPrintStream)
         e.printStackTrace(avgPrintStream)
+        // scalastyle:off
         println(s" Exception while executing Taq Query")
-      }
+        // scalastyle:on
     } finally {
       queryPrintStream.close()
       queryPerfFileStream.close()
     }
-  }
-
-  private def collect(df: DataFrame): Unit = {
-    val result = df.collect()
-    // scalastyle:off
-    println(s"Count = ${result.length}")
-    // scalastyle:on
   }
 
   override def isValidJob(sc: SnappyContext, config: Config): SnappyJobValidation = {
@@ -217,30 +235,35 @@ object SnappyTaq extends SnappySQLJob{
       "/QASNAPPY/TPCH/DATA/1"
     }
 
-    var sqlSparkProps = if (config.hasPath("sparkSqlProps")) {
+    val sqlSparkProps = if (config.hasPath("sparkSqlProps")) {
       config.getString("sparkSqlProps")
-    }
-    else " "
+    } else " "
 
     sqlSparkProperties = sqlSparkProps.split(",")
 
     isResultCollection = if (config.hasPath("resultCollection")) {
       config.getBoolean("resultCollection")
     } else {
-      return new SnappyJobInvalid("Specify whether to to collect results")
+      return SnappyJobInvalid("Specify whether to to collect results")
     }
 
     warmUp = if (config.hasPath("warmUpIterations")) {
       config.getInt("warmUpIterations")
     } else {
-      return new SnappyJobInvalid("Specify number of warmup iterations ")
+      return SnappyJobInvalid("Specify number of warmup iterations ")
     }
     runsForAverage = if (config.hasPath("actualRuns")) {
       config.getInt("actualRuns")
     } else {
-      return new SnappyJobInvalid("Specify number of  iterations of which average result is calculated")
+      return SnappyJobInvalid(
+        "Specify number of  iterations of which average result is calculated")
     }
-    new SnappyJobValid()
     SnappyJobValid()
   }
 }
+
+case class Quote(sym: String, ex: String, bid: Double, time: Timestamp,
+    date: Date)
+
+case class Trade(sym: String, ex: String, price: String, time: Timestamp,
+    date: Date, size: Double)
