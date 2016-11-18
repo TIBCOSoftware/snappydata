b5b9bfa734a2abb7b4b7691bcd866ec8c3b975ac
rmishra <rmishra@pivotal.io>
2016-11-18 16:29:38 +0530

    Changed the folder for WorkingWithJson

--- examples/src/main/scala/org/apache/spark/examples/snappydata/WorkingWithJson.scala
+++ examples/src/main/scala/org/apache/spark/examples/snappydata/WorkingWithJson.scala
@@ -1,6 +1,23 @@
-package org.apache.spark.examples
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+package org.apache.spark.examples.snappydata
 
 import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.log4j.{Level, Logger}
 
 import org.apache.spark.sql.{SnappyContext, SnappyJobValid, SnappyJobValidation, SnappySQLJob, SnappySession, SparkSession}
 
@@ -44,6 +61,10 @@ object WorkingWithJson extends SnappySQLJob {
   }
 
   def main(args: Array[String]) {
+    // reducing the log level to minimize the messages on console
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
+
     val spark: SparkSession = SparkSession.builder.appName("WorkingWithJson").master("local[4]").getOrCreate
     val snSession = new SnappySession(spark.sparkContext, existingSharedState = None)
     val config = ConfigFactory.parseString("")
