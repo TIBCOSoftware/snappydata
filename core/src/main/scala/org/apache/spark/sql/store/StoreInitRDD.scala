e3083948570a90a8b3f23ce9491b4d1b7516eb11
Vivek Bhaskar <vbhaskar@snappydata.io>
2016-11-22 20:57:30 +0530

    Revert "Enabling redundancy for sample table being same as its base table."
    This reverts commit 6d991923da0e487285c6e511198bf396cb1e7a15.

--- core/src/main/scala/org/apache/spark/sql/store/StoreInitRDD.scala
+++ core/src/main/scala/org/apache/spark/sql/store/StoreInitRDD.scala
@@ -19,7 +19,6 @@ package org.apache.spark.sql.store
 import scala.collection.mutable.ArrayBuffer
 
 import com.gemstone.gemfire.distributed.internal.membership.InternalDistributedMember
-import com.gemstone.gemfire.internal.DSFIDFactory
 import com.gemstone.gemfire.internal.cache.{GemFireCacheImpl, LocalRegion}
 import com.pivotal.gemfirexd.internal.engine.Misc
 import io.snappydata.Constant
@@ -59,7 +58,6 @@ class StoreInitRDD(@transient private val sqlContext: SQLContext,
   override def compute(split: Partition,
       context: TaskContext): Iterator[(InternalDistributedMember,
       BlockManagerId)] = {
-    DSFIDFactory.aqpRegister("StoreInitRDD")
 
     // TODO:Suranjan Hackish as we have to register this store at each
     // executor, for storing the CachedBatch we are creating
