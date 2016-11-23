29804048bed4047dadbd766088259ff020e19564
Asif Shahid <ashahid@snappydata.io>
2016-11-22 22:51:40 -0800

    intermediate checkin

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/ExternalStoreUtils.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/ExternalStoreUtils.scala
@@ -443,11 +443,11 @@ object ExternalStoreUtils {
     parameters.getOrElse(BUCKETS, {
       val partitions = SnappyContext.getClusterMode(sc) match {
         case LocalMode(_, _) =>
-          if (!forSampleTable) DEFAULT_TABLE_BUCKETS_LOCAL_MODE
-          else DEFAULT_SAMPLE_TABLE_BUCKETS_LOCAL_MODE
+          if (forSampleTable) DEFAULT_SAMPLE_TABLE_BUCKETS_LOCAL_MODE
+          else DEFAULT_TABLE_BUCKETS_LOCAL_MODE
         case _ =>
-          if (!forSampleTable) DEFAULT_TABLE_BUCKETS
-          else DEFAULT_SAMPLE_TABLE_BUCKETS
+          if (forSampleTable)  DEFAULT_SAMPLE_TABLE_BUCKETS
+          else DEFAULT_TABLE_BUCKETS
       }
       if (forManagedTable) {
         if (forColumnTable) {
