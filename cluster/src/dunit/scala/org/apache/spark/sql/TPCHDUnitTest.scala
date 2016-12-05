9d816a322a2cc357e23b4f7bf3e2c69c152f1dff
Sumedh Wale <swale@snappydata.io>
2016-12-05 11:11:10 +0530

    fixing issues in JDBC query routing
    - clear the metadata flag on subquent uses of the same SnappyResultHolder
    - set batchSize to default explicitly in some tests that load substantial amount of data (default is 4 in tests)

--- cluster/src/dunit/scala/org/apache/spark/sql/TPCHDUnitTest.scala
+++ cluster/src/dunit/scala/org/apache/spark/sql/TPCHDUnitTest.scala
@@ -26,6 +26,8 @@ import org.apache.spark.SparkContext
 
 class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s) {
 
+  bootProps.setProperty("spark.sql.inMemoryColumnarStorage.batchSize", "10000")
+
   val queries = Array("q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9",
     "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19",
     "q20", "q21", "q22")
