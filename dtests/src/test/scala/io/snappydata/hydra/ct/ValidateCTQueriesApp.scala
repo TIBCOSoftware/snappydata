f499413cbb830a16c5568be8bb97fb14f512fd3a
Sonal Agarwal <sagarwal@snappydata.io>
2016-12-06 16:39:35 +0530

    ct test coverage (#446)
    * - Adding tests for CT usecase.
    
    * -Added support for configuring redundancy and persistence mode at run time.
    -Added support for full resultset validation.Modified few queries accordingly.
    -Added new confs for creating tables using scripts and spark app.
    * - Adding further changes for resultset validation and some code formating.
    
    * - Adding test requirement and test description to conf files.
    - Modifying test configuration to run in multihost environment.
    
    * - Removing fullResultSet validation, as it is incomplete.
    
    * - Changing signature as per new changes in master

---
+++ dtests/src/test/scala/io/snappydata/hydra/ct/ValidateCTQueriesApp.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+
+package io.snappydata.hydra.ct
+
+import java.io.{File, FileOutputStream, PrintWriter}
+
+import util.TestException
+
+import org.apache.spark.sql.{SQLContext, SnappyContext}
+import org.apache.spark.{SparkContext, SparkConf}
+
+object ValidateCTQueriesApp {
+  val conf = new SparkConf().setAppName("ValidateCTQueriesApp Application")
+  val sc = new SparkContext(conf)
+  val sqlContext = SQLContext.getOrCreate(sc)
+  val snc = SnappyContext(sc)
+
+  def main(args: Array[String]){
+    val threadID = Thread.currentThread().getId
+    val outputFile = "ValidateCTQueriesApp_thread_" + threadID + "_" + System.currentTimeMillis + ".out"
+    val pw = new PrintWriter(new FileOutputStream(new File(outputFile), true))
+    val dataFilesLocation = args(0)
+    snc.setConf("dataFilesLocation",dataFilesLocation)
+    pw.println(s"dataFilesLocation : ${dataFilesLocation}")
+    CTQueries.snc = snc
+    CTQueries.dataFilesLocation = dataFilesLocation
+    val tableType = args(1)
+    val fullResultSetValidation: Boolean = args(2).toBoolean
+
+    pw.println(s"Validation for queries with ${tableType} tables started")
+    val hasValidationFailed = CTTestUtil.executeQueries(snc, tableType, pw, fullResultSetValidation,sqlContext)
+    if(hasValidationFailed) {
+      pw.println(s"Validation failed for ${tableType}. Please check logs.")
+      //throw new TestException(s"Validation task failed for ${tableType}. Please check logs.")
+    }
+    pw.println(s"Validation for queries with ${tableType} tables completed successfully")
+    pw.close()
+  }
+}
