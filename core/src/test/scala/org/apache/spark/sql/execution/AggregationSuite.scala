fc02719cb4815f0601db16575bc52caafcb621a8
Sumedh Wale <swale@snappydata.io>
2016-12-07 13:24:09 +0530

    Fixing plan failure exception for nullable column group by query
    - SnappyHashAggregateExec: evaluate used input variables explicitly before evaluating
      aggregates since usedInputs returns empty by default
    - AggregationSuite: added tests for nullable group by, join and group by + join

--- core/src/test/scala/org/apache/spark/sql/execution/AggregationSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/execution/AggregationSuite.scala
@@ -18,29 +18,60 @@ package org.apache.spark.sql.execution
 
 import io.snappydata.SnappyFunSuite
 
-import org.apache.spark.sql.{QueryTest, SnappySession, SparkSession}
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.{DataFrame, QueryTest, Row, SnappySession, SparkSession}
 
 class AggregationSuite extends SnappyFunSuite {
 
-  test("AVG plan failure") {
-    val spark = new SparkSession(sc)
-    val snappy = new SnappySession(sc)
-
-    val checkDF = spark.range(1000)
-        .selectExpr("id", "(rand() * 1000.0) as k")
-    checkDF.cache()
-
-    val expectedAnswer = checkDF.selectExpr("avg(k)").collect().toSeq
-
-    val query = "select avg(k) from test"
-    snappy.sql("drop table if exists test")
-    snappy.sql("create table test (id bigint, k double) using column")
-    snappy.insert("test", checkDF.collect(): _*)
-    val result = snappy.sql(query)
-
-    QueryTest.checkAnswer(result, expectedAnswer) match {
+  private def checkAnswer(result: DataFrame, expected: Seq[Row]): Unit = {
+    QueryTest.checkAnswer(result, expected) match {
       case Some(errMessage) => throw new RuntimeException(errMessage)
       case None => // all good
     }
+  }
+
+  test("AVG plan failure for nullables") {
+    val spark = new SparkSession(sc)
+    val snappy = new SnappySession(sc)
+    snappy.sql(s"set ${SQLConf.COLUMN_BATCH_SIZE.key}=1000")
+
+    val checkDF = spark.range(10000).selectExpr("id", "(id * 12) as k",
+      "concat('val', cast((id % 100) as string)) as s")
+    val insertDF = snappy.range(10000).selectExpr("id", "(id * 12) as k",
+      "concat('val', cast((id % 100) as string)) as s")
+    val symDF = spark.range(20).selectExpr("concat('val', cast(id as string)) as s")
+    val sDF = snappy.range(20).selectExpr("concat('val', cast(id as string)) as s")
+
+    snappy.sql("drop table if exists test")
+    snappy.sql("create table test (id bigint, k bigint, s varchar(10)) " +
+        "using column options(buckets '3')")
+    insertDF.write.insertInto("test")
+    snappy.sql("drop table if exists sym")
+    snappy.sql("create table sym (s varchar(10))")
+    sDF.write.insertInto("sym")
+
+    // simple aggregation
+    var query = "select avg(k) from test"
+    var expectedAnswer = checkDF.selectExpr("avg(k)").collect().toSeq
+    var result = snappy.sql(query)
+    checkAnswer(result, expectedAnswer)
+
+    // group by aggregation
+    query = "select s, avg(k) from test group by s"
+    expectedAnswer = checkDF.groupBy("s").agg("k" -> "avg").collect().toSeq
+    result = snappy.sql(query)
+    checkAnswer(result, expectedAnswer)
+
+    // join aggregation
+    query = "select avg(k) from test natural join sym"
+    expectedAnswer = checkDF.join(symDF, "s").selectExpr("avg(k)").collect().toSeq
+    result = snappy.sql(query)
+    checkAnswer(result, expectedAnswer)
+
+    // join then group by aggregation
+    query = "select avg(k) from test natural join sym"
+    expectedAnswer = checkDF.join(symDF, "s").selectExpr("avg(k)").collect().toSeq
+    result = snappy.sql(query)
+    checkAnswer(result, expectedAnswer)
   }
 }
