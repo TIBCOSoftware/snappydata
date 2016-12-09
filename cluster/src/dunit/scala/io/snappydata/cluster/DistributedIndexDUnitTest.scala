78e56c4ecd1a3b33800505b306e9826f8dd15ba6
Vivek Bhaskar <vbhaskar@snappydata.io>
2016-12-09 20:31:17 +0530

    Added test for creating and dropping index on column table.

--- cluster/src/dunit/scala/io/snappydata/cluster/DistributedIndexDUnitTest.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/DistributedIndexDUnitTest.scala
@@ -16,11 +16,16 @@
  */
 package io.snappydata.cluster
 
-import scala.collection.mutable.ListBuffer
+import java.sql.{Connection, DriverManager}
 
+import scala.collection.mutable.ListBuffer
+import io.snappydata.test.dunit.AvailablePortHelper
+
+import org.apache.spark.Logging
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.store.CreateIndexTest
 import org.apache.spark.sql.{SaveMode, SnappyContext}
+import org.apache.spark.sql.collection.Utils
 
 /**
  * Tests various distributed index related tests.
@@ -37,8 +42,8 @@ class DistributedIndexDUnitTest(s: String) extends ClusterManagerTestBase(s) {
           io.snappydata.Property.EnableExperimentalFeatures.configEntry.defaultValueString)
         snContext.setConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key,
           SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.defaultValue.get.toString)
-        indexesToDrop.reverse.foreach(i => snContext.sql(s"DROP INDEX $i "))
-        tablesToDrop.reverse.foreach(t => snContext.sql(s"DROP TABLE $t "))
+        indexesToDrop.reverse.foreach(i => snContext.sql(s"DROP INDEX if exists $i "))
+        tablesToDrop.reverse.foreach(t => snContext.sql(s"DROP TABLE if exists $t "))
         indexesToDrop.clear()
         tablesToDrop.clear()
       }
@@ -102,6 +107,64 @@ class DistributedIndexDUnitTest(s: String) extends ClusterManagerTestBase(s) {
     executeQ(s"select * from $tableName where col1 = 111 and col3 = 'halo' ") {
       CreateIndexTest.validateIndex(Seq(indexThree))(_)
     }
+  }
+
+  private def getANetConnection(netPort: Int): Connection = {
+    val driver = "com.pivotal.gemfirexd.jdbc.ClientDriver"
+    Utils.classForName(driver).newInstance
+    val url = "jdbc:snappydata://localhost:" + netPort + "/"
+    DriverManager.getConnection(url)
+  }
+
+  def testCreateDropColumnTable(): Unit = {
+    val tableName = "tabOne"
+    val netPort1 = AvailablePortHelper.getRandomAvailableTCPPort
+    vm2.invoke(classOf[ClusterManagerTestBase], "startNetServer", netPort1)
+    val conn = getANetConnection(netPort1)
+
+    val snContext = SnappyContext(sc)
+    snContext.setConf(io.snappydata.Property.EnableExperimentalFeatures.configEntry.key, "true")
+    snContext.setConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key, "-1")
+    createBaseTable(snContext, tableName)
+    ClusterManagerTestBase.logger.info("Creating indexes")
+    val indexOne = s"${tableName}_IdxOne"
+    val indexTwo = s"${tableName}_IdxTwo"
+    val indexThree = s"${tableName}_IdxThree"
+    //    snContext.sql(s"create index $indexOne on $tableName (COL1)")
+    //    indexesToDrop += indexOne
+    val s1 = conn.createStatement()
+    s1.execute(s"create index $indexTwo on $tableName (COL2, COL3)")
+    indexesToDrop += indexTwo
+    val s2 = conn.createStatement()
+    s2.execute(s"create index $indexThree on $tableName (COL1, COL3)")
+    indexesToDrop += indexThree
+
+    val executeQ = CreateIndexTest.QueryExecutor(snContext)
+    //    executeQ(s"select * from $tableName where col1 = 111") {
+    //      CreateIndexTest.validateIndex(Seq(indexOne))(_)
+    //    }
+
+    //    executeQ(s"select * from $tableName where col2 = 'aaa' ") {
+    //      CreateIndexTest.validateIndex(Seq.empty, tableName)(_)
+    //    }
+
+    System.setProperty("LOG-NOW", "xxx")
+    getLogWriter.info("SB: About to execute queries")
+    executeQ(s"select * from $tableName where col2 = 'bbb' and col3 = 'halo' ") {
+      CreateIndexTest.validateIndex(Seq(indexTwo))(_)
+    }
+
+    executeQ(s"select * from $tableName where col1 = 111 and col3 = 'halo' ") {
+      CreateIndexTest.validateIndex(Seq(indexThree))(_)
+    }
+
+    val d1 = conn.createStatement()
+    d1.execute(s"drop index $indexTwo")
+    val d2 = conn.createStatement()
+    d2.execute(s"drop index $indexThree")
+
+    getLogWriter.info("SB: Done executing the queries")
+    System.clearProperty("LOG-NOW")
   }
 
 }
