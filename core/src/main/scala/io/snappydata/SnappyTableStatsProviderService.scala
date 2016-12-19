b3585e6c9933efdd80dd15345958343bbbf7066c
Sachin Kapse <skapse@snappydata.io>
2016-12-19 17:01:00 +0530

    Removing extra empty lines.

--- core/src/main/scala/io/snappydata/SnappyTableStatsProviderService.scala
+++ core/src/main/scala/io/snappydata/SnappyTableStatsProviderService.scala
@@ -252,17 +252,14 @@ object SnappyTableStatsProviderService extends Logging {
 
   private def getTableStatsFromAllServers: Seq[SnappyRegionStats] = {
     var result = new java.util.ArrayList[SnappyRegionStatsCollectorResult]().asScala
-
     val dataServers = GfxdMessage.getAllDataStores
     if( dataServers != null && dataServers.size() > 0 ){
-
       result = FunctionService.onMembers(dataServers)
         .withCollector(new GfxdListResultCollector())
         .execute(SnappyRegionStatsCollectorFunction.ID).getResult().
         asInstanceOf[java.util.ArrayList[SnappyRegionStatsCollectorResult]]
         .asScala
     }
-
     result.flatMap(_.getRegionStats.asScala)
   }
 
