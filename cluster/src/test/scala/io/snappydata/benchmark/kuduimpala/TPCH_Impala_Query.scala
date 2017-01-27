cfe5bcf507417c0211645820cf6b872f1405cb34
KishorBachhav <kbachhav@snappydata.io>
2017-01-27 12:26:43 +0530

    --TPCH Changes  --Added JDBC way to  load tables and execute query in snapply cluster   --Added way to load and execute query on kudu-impala cluster    --Removed unnecessary 21 call to execute method in TPCH_Spark and TPCH_Snappy_Query.scala     --Added scripts for kudu-impala execution, jdbc execution, table loading execution
        Ran:
          TPCH Perf tests

---
+++ cluster/src/test/scala/io/snappydata/benchmark/kuduimpala/TPCH_Impala_Query.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+package io.snappydata.benchmark.kuduimpala
+
+import java.io.{File, FileOutputStream, PrintStream}
+import java.sql.{DriverManager, ResultSet}
+
+import io.snappydata.benchmark.memsql.TPCH_Memsql
+import io.snappydata.benchmark.snappy.TPCH_Snappy
+
+object TPCH_Impala_Query {
+
+   def main(args: Array[String]) {
+
+     val host = args(0)
+     val port = args(1)
+     val queries: Array[String] = args(2).split(",")
+     var isResultCollection : Boolean = args(3).toBoolean
+     var warmup : Integer = args(4).toInt
+     var runsForAverage : Integer = args(5).toInt
+
+     Class.forName("com.cloudera.impala.jdbc4.Driver")
+     val dbAddress = "jdbc:impala://" + host + ":" + port + "/"
+     val conn = DriverManager.getConnection(dbAddress)
+     val stmt = conn.createStatement
+
+     stmt.execute("USE TPCH")
+
+     for(query <- queries)
+         TPCH_Impala.execute(query, isResultCollection, stmt, warmup, runsForAverage)
+     println("I am done with execution")
+     stmt.close();
+     TPCH_Impala.close()
+     println("I should exit now")
+   }
+ }
