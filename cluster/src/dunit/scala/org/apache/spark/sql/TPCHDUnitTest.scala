a3f3739aa3a9c667109affbb131ca11be70b3369
KishorBachhav <kbachhav@snappydata.io>
2017-02-02 15:38:22 +0530

    SNAP-1342: TPCHDUnitTest. testSnappy fails In one of my checkin in TPCH perf tests, to avoid code duplication, query to be executed will be specified as 1,2,3,...22 instead of Q1,Q2..Q22 But this change broke TPCHDUnitTest. Fix TPCHDUnitTest with correct string for query to be executed.
    Ran:
     TPCHDUnitTests

--- cluster/src/dunit/scala/org/apache/spark/sql/TPCHDUnitTest.scala
+++ cluster/src/dunit/scala/org/apache/spark/sql/TPCHDUnitTest.scala
@@ -32,9 +32,9 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s) {
 
   bootProps.setProperty(io.snappydata.Property.CachedBatchSize.name, "10000")
   
-  val queries = Array("q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9",
-    "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19",
-    "q20", "q21", "q22")
+  val queries = Array("1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+    "20", "21", "22")
 
   def testSnappy(): Unit = {
     val snc = SnappyContext(sc)
@@ -90,7 +90,7 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s) {
     println(s"Number of rows : $count")
 
     val expectedFile = sc.textFile(getClass.getResource(
-      s"/TPCH/RESULT/Snappy_q10.out").getPath)
+      s"/TPCH/RESULT/Snappy_10.out").getPath)
     val expectedNoOfLines = expectedFile.collect().size
     assert(count == expectedNoOfLines)
   }
@@ -134,9 +134,9 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s) {
 
 object TPCHUtils {
 
-  val queries = Array("q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9",
-    "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19",
-    "q20", "q21", "q22")
+  val queries = Array("1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+    "20", "21", "22")
 
   def createAndLoadTables(snc: SnappyContext, isSnappy: Boolean): Unit = {
     val tpchDataPath = getClass.getResource("/TPCH").getPath //"/data/wrk/w/TPCH/1GB" //
