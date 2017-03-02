961ae164a948b87b4e16808913ee3b982d78146c
Neeraj Kumar <kneeraj@snappydata.io>
2017-03-02 16:05:14 +0530

    Tests added for hints in queries, constants in projection, joins, in list, limit queries etc.

--- core/src/test/scala/org/apache/spark/sql/store/TokenizationTest.scala
+++ core/src/test/scala/org/apache/spark/sql/store/TokenizationTest.scala
@@ -16,24 +16,11 @@
  */
 package org.apache.spark.sql.store
 
-import java.sql.DriverManager
-import java.util
-
-import scala.util.{Failure, Success, Try}
-import com.gemstone.gemfire.cache.{EvictionAction, EvictionAlgorithm}
-import com.gemstone.gemfire.internal.cache.{GemFireCacheImpl, PartitionedRegion}
-import com.pivotal.gemfirexd.internal.engine.Misc
-import com.pivotal.gemfirexd.internal.impl.jdbc.EmbedConnection
-import com.pivotal.gemfirexd.internal.impl.sql.compile.ParserImpl
-import io.snappydata.{SnappyFunSuite, SnappyTableStatsProviderService}
-import io.snappydata.core.{Data, TestData, TestData2}
-import org.apache.hadoop.hive.ql.parse.ParseDriver
+import io.snappydata.SnappyFunSuite
+import io.snappydata.core.Data
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
 import org.apache.spark.Logging
 import org.apache.spark.sql.SnappySession.CachedKey
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.execution.columnar.JDBCAppendableRelation
-import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 import org.apache.spark.sql._
 
 /**
@@ -45,33 +32,23 @@ class TokenizationTest
         with BeforeAndAfter
         with BeforeAndAfterAll {
 
-  val schema = "test"
-  val table = "my_table"
+  val table  = "my_table"
+  val table2 = "my_table2"
+  val all_typetable = "my_table3"
 
   after {
-    snc.dropTable(s"$schema.$table", ifExists = true)
+    snc.dropTable(s"$table", ifExists = true)
+    snc.dropTable(s"$table2", ifExists = true)
+    snc.dropTable(s"$all_typetable", ifExists = true)
   }
 
-  test("Test tokenize") {
+  test("Test tokenize and queryHints and noTokenize if limit or projection") {
     val numRows = 10
-    val data = ((0 to numRows), (0 to numRows), (0 to numRows)).zipped.toArray
-
-    val rdd = sc.parallelize(data, data.length)
-      .map(s => Data(s._1, s._2, s._3))
-    val dataDF = snc.createDataFrame(rdd)
-
-    snc.sql(s"Drop Table if exists $schema.$table")
-    snc.sql(s"Create Table $schema.$table (a INT, b INT, c INT) " +
-      "using column options()")
+    createSimpleTableAndPoupulateData(numRows, s"$table", true)
 
     try {
-      dataDF.write.insertInto(s"$schema.$table")
-      // This sleep was necessary as it has some dependency on the region size
-      // collector thread frequency. Can't remember right now.
-      Thread.sleep(1000)
-
       val q = (0 until numRows) map { x =>
-        s"SELECT * FROM $schema.$table where a = $x"
+        s"select * from $table where a = $x"
       }
       val start = System.currentTimeMillis()
       q.zipWithIndex.map  { case (x, i) =>
@@ -83,19 +60,173 @@ class TokenizationTest
       }
       val end = System.currentTimeMillis()
 
-      // snc.sql(s"SELECT * FROM $schema.$table where a = 1200").collect()
+      // snc.sql(s"select * from $table where a = 1200").collect()
       println("Time taken = " + (end - start))
 
       val cacheMap = SnappySession.getPlanCache.asMap()
       assert( cacheMap.size() == 1)
       val x = cacheMap.keySet().toArray()(0).asInstanceOf[CachedKey].sqlText
       assert(x.equals(q(0)))
-      snc.sql(s"drop table $schema.$table")
+
+      // Now test query hints -- arbitrary hint given
+      val hintQuery = s"select * from $table /*+ XXXX( ) */ where a = 0"
+      snc.sql(hintQuery).collect()
+      assert( cacheMap.size() == 2)
+
+      // test limit
+      var query = s"select * from $table where a = 0 limit 1"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 3)
+
+      query = s"select * from $table where a = 0 limit 10"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 4)
+
+      // test constants in projection
+      query = s"SELECT a, 'x' FROM $table where a = 0"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 5)
+
+      query = s"SELECT a, 'y' FROM $table where a = 0"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 6)
+
+      // check in based queries
+      query = s"select * from $table where a in (0, 1)"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 7)
+
+      // new plan should not be generated so size should be same
+      query = s"select * from $table where a in (5, 7)"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 7)
+
+      // let us clear the plan cache
+      snc.clear()
+      assert( cacheMap.size() == 0)
+
+      createSimpleTableAndPoupulateData(numRows, s"$table2")
+      // creating table should not put anything in cache
+      assert( cacheMap.size() == 0)
+      // fire a join query
+      query = s"select * from $table t1, $table2 t2 where t1.a = 0"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 1)
+
+      query = s"select * from $table t1, $table2 t2 where t1.a = 5"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 1)
+
+      query = s"select * from $table t1, $table2 t2 where t2.a = 5"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 2)
+
+      query = s"select * from $table t1, $table2 t2 where t1.a = t2.a"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 3)
+
+      query = s"select * from $table t1, $table2 t2 where t1.a = t2.b"
+      snc.sql(query).collect()
+      assert( cacheMap.size() == 4)
+
+      snc.sql(s"drop table $table")
+
+    } finally {
+      snc.sql("set spark.sql.caseSensitive = false")
+      snc.sql("set schema = APP")
+    }
+    logInfo("Successful")
+  }
+
+  test("Test tokenize for all data types") {
+    val numRows = 10
+    createAllTypeTableAndPoupulateData(numRows, s"$all_typetable")
+
+    try {
+      val q = (0 until numRows).zipWithIndex.map { case (_, i) =>
+        s"select * from $all_typetable where s = 'abc$i'"
+      }
+      val start = System.currentTimeMillis()
+      q.zipWithIndex.map  { case (x, i) =>
+        var result = snc.sql(x).collect()
+        assert(result.length === 1)
+        result.foreach( r => {
+          assert(r.get(0) == i && r.get(4) == s"abc$i")
+        })
+      }
+      val end = System.currentTimeMillis()
+
+      // snc.sql(s"select * from $table where a = 1200").collect()
+      println("Time taken = " + (end - start))
+
+      val cacheMap = SnappySession.getPlanCache.asMap()
+      assert( cacheMap.size() == 1)
+      val x = cacheMap.keySet().toArray()(0).asInstanceOf[CachedKey].sqlText
+      assert(x.equals(q(0)))
+      snc.sql(s"drop table $all_typetable")
     } finally {
       snc.sql("set spark.sql.caseSensitive = false")
       snc.sql("set schema = APP")
     }
 
     logInfo("Successful")
+  }
+
+  test("Test tokenize for joins and sub-queries") {
+    logInfo("Successful")
+  }
+
+  test("Test tokenize for nulls") {
+    logInfo("Successful")
+  }
+
+  test("Test tokenize for cast queries") {
+    logInfo("Successful")
+  }
+
+  private def createSimpleTableAndPoupulateData(numRows: Int, name: String, dosleep: Boolean = false) = {
+    val data = ((0 to numRows), (0 to numRows), (0 to numRows)).zipped.toArray
+    val rdd = sc.parallelize(data, data.length)
+      .map(s => Data(s._1, s._2, s._3))
+    val dataDF = snc.createDataFrame(rdd)
+
+    snc.sql(s"Drop Table if exists $name")
+    snc.sql(s"Create Table $name (a INT, b INT, c INT) " +
+      "using column options()")
+    dataDF.write.insertInto(s"$name")
+    // This sleep was necessary as it has some dependency on the region size
+    // collector thread frequency. Can't remember right now.
+    if (dosleep) Thread.sleep(5000)
+  }
+
+  private def createAllTypeTableAndPoupulateData(numRows: Int, name: String, dosleep: Boolean = false) = {
+    val ints = (0 to numRows).zipWithIndex.map {case (_, i) =>
+      i
+    }
+    val longs = (0 to numRows).zipWithIndex.map    { case (_, i) => 1L*1000*i }
+    val floats = (0 to numRows).zipWithIndex.map   { case (_, i) => 0.1f*i    }
+    val decimals = (0 to numRows).zipWithIndex.map { case (_, i) => 0.1d*i    }
+    val strs = (0 to numRows).zipWithIndex.map     { case (_, i) => s"abc$i"  }
+    val dates = (0 to numRows).zipWithIndex.map    { case (_, i) => 1         }
+    val tstmps = (0 to numRows).zipWithIndex.map   { case (_, i) => s"abc$i"  }
+
+    val x =((((((ints, longs).zipped.toArray, floats).zipped.toArray,
+      decimals).zipped.toArray, strs).zipped.toArray, dates).zipped.toArray, tstmps).zipped.toArray
+    val data = x map { case ((((((i, l), f), d), s), dt), ts) =>
+      (i, l, f, d, s, dt, ts)
+    }
+
+    val rdd = sc.parallelize(data, data.length)
+      .map(s => (s._1, s._2, s._3, s._4, s._5, s._6, s._7))
+    val dataDF = snc.createDataFrame(rdd)
+
+    snc.sql(s"Drop Table if exists $name")
+    snc.sql(s"Create Table $name (a INT, b Long, c Float, d Double, s String, dt Int, ts Long) " +
+      "using column options()")
+
+    dataDF.write.insertInto(name)
+    // This sleep was necessary as it has some dependency on the region size
+    // collector thread frequency. Can't remember right now.
+    if (dosleep) Thread.sleep(5000)
   }
 }
