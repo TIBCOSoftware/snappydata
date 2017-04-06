72a185ff726c5bb3c009362c3c61203f5d0c8bc2
Vivek Bhaskar <vbhaskar@snappydata.io>
2017-04-06 17:07:26 +0530

    Updated test

--- cluster/src/dunit/scala/io/snappydata/cluster/PreparedQueryRoutingDUnitTest.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/PreparedQueryRoutingDUnitTest.scala
@@ -17,13 +17,15 @@
 
 package io.snappydata.cluster
 
-import java.sql.{Connection, DriverManager, ResultSet}
+import java.sql.{Connection, Date, DriverManager, ResultSet}
+import java.time.LocalDate
 
 import com.pivotal.gemfirexd.internal.engine.distributed.utils.GemFireXDUtils
 import io.snappydata.test.dunit.AvailablePortHelper
 
 import org.apache.spark.Logging
 import org.apache.spark.sql.SnappyContext
+import org.apache.spark.sql.types.{DataType, DateType}
 
 /**
  * Tests for query routing from JDBC client driver.
@@ -34,7 +36,6 @@ class PreparedQueryRoutingDUnitTest(val s: String)
   // set default batch size for this test
   private val default_chunk_size = GemFireXDUtils.DML_MAX_CHUNK_SIZE
   var serverHostPort = -1
-  val tableName = "order_line_col"
 
   override def tearDown2(): Unit = {
     // reset the chunk size on lead node
@@ -47,27 +48,30 @@ class PreparedQueryRoutingDUnitTest(val s: String)
   }
 
   def test1_PrepStatementRouting(): Unit = {
+    val tableName = "order_line_col_test1"
     serverHostPort = AvailablePortHelper.getRandomAvailableTCPPort
     vm2.invoke(classOf[ClusterManagerTestBase], "startNetServer", serverHostPort)
+    // scalastyle: off
     println(s"test1: network server started at $serverHostPort")
+    // scalastyle: on
 
     val snc = SnappyContext(sc)
     snc.sql(s"create table $tableName (ol_int_id  integer," +
         s" ol_int2_id  integer, ol_str_id STRING) using column " +
         "options( partition_by 'ol_int_id, ol_int2_id', buckets '2')")
 
-    insertRows_test1(1000)
+    insertRows_test1(1000, tableName)
 
     // (1 to 5).foreach(d => query())
-    query_test1()
+    query_test1(tableName)
   }
 
-  def insertRows_test1(numRows: Int): Unit = {
+  def insertRows_test1(numRows: Int, tableName: String): Unit = {
 
     val conn = DriverManager.getConnection(
       "jdbc:snappydata://localhost:" + serverHostPort)
 
-    val rows = (1 to numRows).toSeq
+    val rows = (1 to numRows)
     val stmt = conn.createStatement()
     try {
       var i = 1
@@ -80,14 +84,16 @@ class PreparedQueryRoutingDUnitTest(val s: String)
         }
       })
       stmt.executeBatch()
+      // scalastyle: off
       println(s"committed $numRows rows")
+      // scalastyle: on
     } finally {
       stmt.close()
       conn.close()
     }
   }
 
-  def verifyQuery(qryTest: String, prep_rs: ResultSet, stmt_rs: ResultSet): Unit = {
+  def verifyQuery_test1(qryTest: String, prep_rs: ResultSet, stmt_rs: ResultSet): Unit = {
     val builder = StringBuilder.newBuilder
     var index = 0
     var assertionFailed = false
@@ -146,17 +152,21 @@ class PreparedQueryRoutingDUnitTest(val s: String)
     }
 
     if (assertionFailed) {
+      // scalastyle: off
       println(builder.toString())
+      // scalastyle: on
     }
 
     assert(!assertionFailed)
   }
 
-  def query1_like_clause(limitClause: String): Unit = {
+  def query1_like_clause_test1(limitClause: String, tableName: String): Unit = {
     val conn = DriverManager.getConnection(
       "jdbc:snappydata://localhost:" + serverHostPort)
 
+    // scalastyle: off
     println(s"query1_like_clause: Connected to $serverHostPort")
+    // scalastyle: on
 
     val stmt = conn.createStatement()
     var prepStatement: java.sql.PreparedStatement = null
@@ -169,7 +179,7 @@ class PreparedQueryRoutingDUnitTest(val s: String)
           s" $limitClause" +
           s""
 
-      val prepStatement = conn.prepareStatement(qry)
+      prepStatement = conn.prepareStatement(qry)
       prepStatement.setInt(1, 500)
       prepStatement.setString(2, "%0")
       val rs = prepStatement.executeQuery
@@ -183,7 +193,7 @@ class PreparedQueryRoutingDUnitTest(val s: String)
           s""
       val rs2 = stmt.executeQuery(qry2)
 
-      verifyQuery("query1", rs, rs2)
+      verifyQuery_test1("query1", rs, rs2)
       rs.close()
       rs2.close()
 
@@ -196,11 +206,13 @@ class PreparedQueryRoutingDUnitTest(val s: String)
     }
   }
 
-  def query2_in_clause(limitClause: String): Unit = {
+  def query2_in_clause_test1(limitClause: String, tableName: String): Unit = {
     val conn = DriverManager.getConnection(
       "jdbc:snappydata://localhost:" + serverHostPort)
 
+    // scalastyle: off
     println(s"query2_in_clause: Connected to $serverHostPort")
+    // scalastyle: on
 
     val stmt = conn.createStatement()
     var prepStatement: java.sql.PreparedStatement = null
@@ -213,7 +225,8 @@ class PreparedQueryRoutingDUnitTest(val s: String)
           s" $limitClause" +
           s""
 
-      val prepStatement = conn.prepareStatement(qry)
+
+      prepStatement = conn.prepareStatement(qry)
       prepStatement.setInt(1, 500)
       prepStatement.setInt(2, 100)
       prepStatement.setInt(3, 200)
@@ -230,7 +243,7 @@ class PreparedQueryRoutingDUnitTest(val s: String)
           s""
       val rs2 = stmt.executeQuery(qry2)
 
-      verifyQuery("query3", rs, rs2)
+      verifyQuery_test1("query3", rs, rs2)
       rs.close()
       rs2.close()
 
@@ -243,10 +256,177 @@ class PreparedQueryRoutingDUnitTest(val s: String)
     }
   }
 
-  def query_test1(): Unit = {
-    query1_like_clause("limit 20")
-    query1_like_clause("")
-    query2_in_clause("limit 20")
-    query2_in_clause("")
+  def query_test1(tableName: String): Unit = {
+    query1_like_clause_test1("limit 20", tableName)
+    query1_like_clause_test1("", tableName)
+    query2_in_clause_test1("limit 20", tableName)
+    query2_in_clause_test1("", tableName)
+  }
+
+  def insertRows_test2(numRows: Int, tableName: String): Unit = {
+
+    val conn = DriverManager.getConnection(
+      "jdbc:snappydata://localhost:" + serverHostPort)
+
+    val rows = (1 to numRows)
+    val stmt = conn.createStatement()
+    try {
+      var i = 1
+      val newDate = LocalDate.parse("2017-01-01")
+      rows.foreach(d => {
+        val e = newDate.plusDays(i)
+        stmt.addBatch(s"insert into $tableName values('$e', '$e', '$e')")
+        i += 1
+        if (i % 1000 == 0) {
+          stmt.executeBatch()
+          i = 0
+        }
+      })
+      stmt.executeBatch()
+      // scalastyle: off
+      println(s"committed $numRows rows")
+      // scalastyle: on
+    } finally {
+      stmt.close()
+      conn.close()
+    }
+  }
+
+  def verifyQuery_test2(qryTest: String, prep_rs: ResultSet, stmt_rs: ResultSet): Unit = {
+    val builder = StringBuilder.newBuilder
+    var index = 0
+    var assertionFailed = false
+    while (prep_rs.next() && stmt_rs.next()) {
+      val prep_i = prep_rs.getDate(1)
+      val prep_j = prep_rs.getDate(2)
+      val prep_s = prep_rs.getString(3)
+
+      val stmt_i = stmt_rs.getDate(1)
+      val stmt_j = stmt_rs.getDate(2)
+      val stmt_s = stmt_rs.getString(3)
+
+      builder.append(s"$qryTest Prep: row($index) $prep_i $prep_j $prep_s ").append("\n")
+      builder.append(s"$qryTest Stmt: row($index) $stmt_i $stmt_j $stmt_s ").append("\n")
+
+      if (prep_i != stmt_i && !assertionFailed) {
+        builder.append(s"Assertion failed at index=$index prep=$prep_i stmt=$stmt_i").append("\n")
+        assertionFailed = true
+      }
+
+      if (prep_j != stmt_j && !assertionFailed) {
+        builder.append(s"Assertion failed at index=$index prep=$prep_j stmt=$stmt_j").append("\n")
+        assertionFailed = true
+      }
+
+      if (prep_s != stmt_s && !assertionFailed) {
+        builder.append(s"Assertion failed at index=$index prep=$prep_s stmt=$stmt_s").append("\n")
+        assertionFailed = true
+      }
+
+      index += 1
+    }
+
+    while (prep_rs.next()) {
+      if (!assertionFailed) {
+        builder.append(s"Assertion failed at index=$index").append("\n")
+        assertionFailed = true
+      }
+
+      val prep_i = prep_rs.getDate(1)
+      val prep_j = prep_rs.getDate(2)
+      val prep_s = prep_rs.getString(3)
+      builder.append(s"$qryTest Prep: row($index) $prep_i $prep_j $prep_s ").append("\n")
+    }
+
+    while (stmt_rs.next()) {
+      if (!assertionFailed) {
+        builder.append(s"Assertion failed at index=$index").append("\n")
+        assertionFailed = true
+      }
+
+      val stmt_i = stmt_rs.getDate(1)
+      val stmt_j = stmt_rs.getDate(2)
+      val stmt_s = stmt_rs.getString(3)
+      builder.append(s"$qryTest Stmt: row($index) $stmt_i $stmt_j $stmt_s ").append("\n")
+    }
+
+    if (assertionFailed) {
+      // scalastyle: off
+      println(builder.toString())
+      // scalastyle: on
+    }
+
+    assert(!assertionFailed)
+  }
+
+  def query1_test2(limitClause: String, tableName: String): Unit = {
+    val conn = DriverManager.getConnection(
+      "jdbc:snappydata://localhost:" + serverHostPort)
+
+    // scalastyle: off
+    println(s"query1_test2: Connected to $serverHostPort")
+    // scalastyle: on
+
+    val stmt = conn.createStatement()
+    var prepStatement: java.sql.PreparedStatement = null
+    val oneDate = Date.valueOf("2017-03-15")
+    val twoDate = Date.valueOf("2017-02-28")
+    try {
+      val qry = s"select ol_int_id, ol_int2_id, ol_str_id " +
+          s" from $tableName " +
+          s" where ol_int_id < ? " +
+          s" and ol_int2_id > ? " +
+          s" and ol_str_id like ? " +
+          s" $limitClause" +
+          s""
+
+      prepStatement = conn.prepareStatement(qry)
+      prepStatement.setString(1, oneDate.toString)
+      prepStatement.setDate(2, twoDate)
+      prepStatement.setString(3, "%-%")
+      val rs = prepStatement.executeQuery
+
+      val qry2 = s"select ol_int_id, ol_int2_id, ol_str_id " +
+          s" from $tableName " +
+          s" where ol_int_id < '${oneDate.toString}' " +
+          s" and ol_int2_id > '${twoDate.toString}' " +
+          s" and ol_str_id LIKE '%-%' " +
+          s" $limitClause" +
+          s""
+      val rs2 = stmt.executeQuery(qry2)
+
+      verifyQuery_test2("query3", rs, rs2)
+      rs.close()
+      rs2.close()
+
+      // Thread.sleep(1000000)
+
+    } finally {
+      stmt.close()
+      if (prepStatement != null) prepStatement.close()
+      conn.close()
+    }
+  }
+
+  def query_test2(tableName: String): Unit = {
+    query1_test2("limit 20", tableName: String)
+    query1_test2("", tableName: String)
+  }
+
+  def test2_date(): Unit = {
+    val tableName = "order_line_col_test2"
+    serverHostPort = AvailablePortHelper.getRandomAvailableTCPPort
+    vm2.invoke(classOf[ClusterManagerTestBase], "startNetServer", serverHostPort)
+    // scalastyle: off
+    println(s"test2: network server started at $serverHostPort")
+    // scalastyle: on
+
+    val snc = SnappyContext(sc)
+    snc.sql(s"create table $tableName (ol_int_id  date," +
+        s" ol_int2_id  date, ol_str_id STRING) using column " +
+        "options( partition_by 'ol_int_id, ol_int2_id', buckets '2')")
+
+    insertRows_test2(1000, tableName)
+    query_test2(tableName)
   }
 }
