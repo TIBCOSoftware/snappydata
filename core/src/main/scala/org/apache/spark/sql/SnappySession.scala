2551c870c40a8bf6cc583652a40ed0d7387568a1
Sumedh Wale <swale@snappydata.io>
2017-04-21 14:44:42 +0530

    [SNAP-1457] fix failure in ColumnTableDUnitTest.testCreateAndInsertCLOB
    Issue due to NPE in the new TaskContext/consumer as reported in
    https://jira.snappydata.io/browse/SNAP-1457
    Checks for null TaskContext for the row buffer to column store insertion path.
    
    Scalastyle cleanups in SnappySession.
    
    Reviewed by Rishi

--- core/src/main/scala/org/apache/spark/sql/SnappySession.scala
+++ core/src/main/scala/org/apache/spark/sql/SnappySession.scala
@@ -31,7 +31,7 @@ import com.gemstone.gemfire.distributed.internal.DistributionAdvisor.Profile
 import com.gemstone.gemfire.distributed.internal.ProfileListener
 import com.gemstone.gemfire.internal.cache.PartitionedRegion
 import com.gemstone.gemfire.internal.shared.{ClientResolverUtils, FinalizeHolder, FinalizeObject}
-import com.google.common.cache.{CacheBuilder, CacheLoader}
+import com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache}
 import com.google.common.util.concurrent.UncheckedExecutionException
 import io.snappydata.{Constant, SnappyTableStatsProviderService}
 
@@ -1203,7 +1203,8 @@ class SnappySession(@transient private val sc: SparkContext,
       case ThinClientConnectorMode(_, _) =>
         val isTempTable = sessionCatalog.isTemporaryTable(tableIdent)
         if (!isTempTable) {
-          sessionCatalog.asInstanceOf[ConnectorCatalog].connectorHelper.dropTable(tableIdent, ifExists)
+          sessionCatalog.asInstanceOf[ConnectorCatalog].connectorHelper
+              .dropTable(tableIdent, ifExists)
           return
         }
       case _ =>
@@ -1329,8 +1330,9 @@ class SnappySession(@transient private val sc: SparkContext,
 
     SnappyContext.getClusterMode(sc) match {
       case ThinClientConnectorMode(_, _) =>
-        return sessionCatalog.asInstanceOf[ConnectorCatalog].connectorHelper.
+        sessionCatalog.asInstanceOf[ConnectorCatalog].connectorHelper.
             createIndex(indexIdent, tableIdent, indexColumns, options)
+        return
       case _ =>
     }
 
@@ -1387,7 +1389,9 @@ class SnappySession(@transient private val sc: SparkContext,
 
     SnappyContext.getClusterMode(sc) match {
       case ThinClientConnectorMode(_, _) =>
-        return sessionCatalog.asInstanceOf[ConnectorCatalog].connectorHelper.dropIndex(indexName, ifExists)
+        sessionCatalog.asInstanceOf[ConnectorCatalog].connectorHelper
+            .dropIndex(indexName, ifExists)
+        return
       case _ =>
     }
 
@@ -1691,10 +1695,9 @@ object SnappySession extends Logging {
   private def getAllParamLiterals(queryplan: QueryPlan[_]): Array[ParamLiteral] = {
     val res = new ArrayBuffer[ParamLiteral]()
     queryplan transformAllExpressions {
-      case p: ParamLiteral => {
+      case p: ParamLiteral =>
         res += p
         p
-      }
     }
     res.toSet[ParamLiteral].toArray.sortBy(_.pos)
   }
@@ -1710,12 +1713,12 @@ object SnappySession extends Logging {
       val nocaching = session.getContextObject[Boolean](
         CachedPlanHelperExec.NOCACHING_KEY).getOrElse(false)
       if (nocaching) {
-        key.invalidatePlan
+        key.invalidatePlan()
       }
       else {
         val params1 = getAllParamLiterals(executedPlan)
-        if (!(params1.sameElements(key.pls))) {
-          key.invalidatePlan
+        if (!params1.sameElements(key.pls)) {
+          key.invalidatePlan()
         }
       }
     }
@@ -1761,10 +1764,10 @@ object SnappySession extends Logging {
     // modified tpch query no 15, It even picks those literal which we don't want.
     var allLiterals: Array[LiteralValue] = Array.empty
     if (key != null && key.valid) {
-      allLiterals = (CachedPlanHelperExec.allLiterals(
+      allLiterals = CachedPlanHelperExec.allLiterals(
         session.getContextObject[ArrayBuffer[ArrayBuffer[Any]]](
           CachedPlanHelperExec.REFERENCES_KEY).getOrElse(Seq.empty)
-      )).filter(!_.collectedForPlanCaching)
+      ).filter(!_.collectedForPlanCaching)
 
       allLiterals.foreach(_.collectedForPlanCaching = true)
     }
@@ -1800,8 +1803,8 @@ object SnappySession extends Logging {
     CacheBuilder.newBuilder().maximumSize(300).build(loader)
   }
 
-  //noinspection ScalaStyle
-  def getPlanCache = planCache
+  def getPlanCache: LoadingCache[CachedKey, (CachedDataFrame,
+      Map[String, String])] = planCache
 
   private[spark] def addBucketProfileListener(pr: PartitionedRegion): Unit = {
     val advisers = pr.getRegionAdvisor.getAllBucketAdvisorsHostedAndProxies
@@ -1827,8 +1830,8 @@ object SnappySession extends Logging {
       }
     }
 
-    var valid = true
-    def invalidatePlan = valid = false
+    private[sql] var valid = true
+    def invalidatePlan(): Unit = valid = false
   }
 
   object CachedKey {
@@ -1857,10 +1860,10 @@ object SnappySession extends Logging {
       }
 
       def transformExprID: PartialFunction[LogicalPlan, LogicalPlan] = {
-        case q: LogicalPlan => q.transformAllExpressions(normalizeExprIds)
         case f@Filter(condition, child) => f.copy(
           condition = condition.transform(normalizeExprIds),
           child = child.transformAllExpressions(normalizeExprIds))
+        case q: LogicalPlan => q.transformAllExpressions(normalizeExprIds)
       }
 
       // normalize lp so that two queries can be determined to be equal
