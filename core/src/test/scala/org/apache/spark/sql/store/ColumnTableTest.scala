cd70d66fe1ef8a024a01c61b682d8f5f6f5ef397
Sachin Janani <sjanani@snappydata.io>
2017-05-04 18:09:57 +0530

    Test for SNAP-1367 (#572)
    * Test for SNAP-1367
    
    * Incorporated review comments and modified tests to cover few more scenarios
    
    * Incorporated review comments and also added assertions in test for schema validation
    
    * Linking store

--- core/src/test/scala/org/apache/spark/sql/store/ColumnTableTest.scala
+++ core/src/test/scala/org/apache/spark/sql/store/ColumnTableTest.scala
@@ -1013,4 +1013,27 @@ class ColumnTableTest
     assert(df1.schema == df3.schema)
 
   }
+
+  test("Test create table from CSV without header") {
+    snc.sql(s"create table test1 using com.databricks.spark.csv options(path '${(getClass.getResource
+    ("/northwind/orders" +
+      ".csv").getPath)}', header 'false', inferschema 'true')")
+    snc.sql("create table test2 using column options() as (select * from test1)")
+    val df2 = snc.sql("select * from test2")
+    df2.show()
+
+    snc.sql("drop table test2")
+    snc.sql("create table test2(_col1 integer,__col2 integer) using column options()")
+    snc.sql("insert into test2 values(1,2)")
+    snc.sql("insert into test2 values(2,3)")
+    val df3 = snc.sql("select _col1,__col2 from test2")
+    df3.show()
+    val struct = (new StructType())
+      .add(StructField("_COL1", IntegerType, true))
+      .add(StructField("__COL2", IntegerType, true))
+
+    df3.printSchema()
+    assert(struct == df3.schema)
+
+  }
 }
