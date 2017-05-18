f794d0cce81dce3dc0b0e3c62d7bd5685101dc6b
swati <ssawant@snappydata.io>
2017-05-18 14:54:14 +0530

    - Modified the test code for creating connection once per thread and then using same connection for executing number of queries - Reduced number of locators and leads in the test

--- dtests/src/test/java/io/snappydata/hydra/cluster/SnappyConcurrencyTest.java
+++ dtests/src/test/java/io/snappydata/hydra/cluster/SnappyConcurrencyTest.java
@@ -19,6 +19,8 @@ package io.snappydata.hydra.cluster;
 import com.gemstone.gemfire.cache.query.Struct;
 import com.gemstone.gemfire.cache.query.internal.types.StructTypeImpl;
 import hydra.Log;
+import hydra.RemoteTestModule;
+import hydra.TestConfig;
 import sql.sqlutil.ResultSetHelper;
 import util.TestException;
 
@@ -29,28 +31,38 @@ import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 
+import static hydra.Prms.totalTaskTimeSec;
+
 public class SnappyConcurrencyTest extends SnappyTest {
 
   public static void runPointLookUpQueries() throws SQLException {
     Vector<String> queryVect = SnappyPrms.getPointLookUpQueryList();
+    long totalTaskTime = TestConfig.tab().longAt(totalTaskTimeSec);
+    Log.getLogWriter().info("SS - totalTaskTime : " + totalTaskTime);
     String query = null;
-    int queryNum = new Random().nextInt(queryVect.size());
-    query = (String) queryVect.elementAt(queryNum);
     Connection conn = getLocatorConnection();
     ResultSet rs;
-    try {
-      Log.getLogWriter().info("SS - Executing query : " + query);
-      rs = conn.createStatement().executeQuery(query);
-      SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numQueriesExecuted);
-      int numQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
-      Log.getLogWriter().info("SS - numQueriesExecuted : " +  numQueries);
-      SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numPointLookUpQueriesExecuted);
-      int numPointLookUpQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB
-          .numPointLookUpQueriesExecuted);
-      Log.getLogWriter().info("SS - numPointLookUpQueriesExecuted : " +  numPointLookUpQueries);
-    } catch (SQLException se) {
-      throw new TestException("Got exception while executing pointLookUp query:" + query, se);
-    }
+    long startTime;
+    long endTime = System.currentTimeMillis() + totalTaskTime;
+    do {
+      try {
+        int queryNum = new Random().nextInt(queryVect.size());
+        query = queryVect.elementAt(queryNum);
+        Log.getLogWriter().info("SS - Executing query : " + query);
+        rs = conn.createStatement().executeQuery(query);
+        SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numQueriesExecuted);
+        int numQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
+        Log.getLogWriter().info("SS - numQueriesExecuted : " + numQueries);
+        SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numPointLookUpQueriesExecuted);
+        int numPointLookUpQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB
+            .numPointLookUpQueriesExecuted);
+        Log.getLogWriter().info("SS - numPointLookUpQueriesExecuted : " + numPointLookUpQueries);
+        startTime = System.currentTimeMillis();
+      } catch (SQLException se) {
+        throw new TestException("Got exception while executing pointLookUp query:" + query, se);
+      }
+    } while (startTime < endTime);
+
     /*StructTypeImpl sti = ResultSetHelper.getStructType(rs);
     List<Struct> queryResult = ResultSetHelper.asList(rs, sti, false);
     Log.getLogWriter().info("SS - Result for query : " + query + "\n" + queryResult.toString());*/
@@ -60,22 +72,29 @@ public class SnappyConcurrencyTest extends SnappyTest {
   public static void runAnalyticalQueries() throws SQLException {
     Connection conn = getLocatorConnection();
     Vector<String> queryVect = SnappyPrms.getAnalyticalQueryList();
+    long totalTaskTime = TestConfig.tab().longAt(totalTaskTimeSec);
+    Log.getLogWriter().info("SS - totalTaskTime : " + totalTaskTime);
     String query = null;
     int queryNum = new Random().nextInt(queryVect.size());
     query = (String) queryVect.elementAt(queryNum);
     ResultSet rs;
-    try {
-      Log.getLogWriter().info("SS - Executing query : " + query);
-      rs = conn.createStatement().executeQuery(query);
-      SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numQueriesExecuted);
-      int numQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
-      Log.getLogWriter().info("SS - numQueriesExecuted : " + numQueries);
-      SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numAggregationQueriesExecuted);
-      int numAggregationQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
-      Log.getLogWriter().info("SS - numAggregationQueriesExecuted : " + numAggregationQueries);
-    } catch (SQLException se) {
-      throw new TestException("Got exception while executing Analytical query:" + query, se);
-    }
+    long startTime;
+    long endTime = System.currentTimeMillis() + totalTaskTime;
+    do {
+      try {
+        Log.getLogWriter().info("SS - Executing query : " + query);
+        rs = conn.createStatement().executeQuery(query);
+        SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numQueriesExecuted);
+        int numQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
+        Log.getLogWriter().info("SS - numQueriesExecuted : " + numQueries);
+        SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numAggregationQueriesExecuted);
+        int numAggregationQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
+        Log.getLogWriter().info("SS - numAggregationQueriesExecuted : " + numAggregationQueries);
+        startTime = System.currentTimeMillis();
+      } catch (SQLException se) {
+        throw new TestException("Got exception while executing Analytical query:" + query, se);
+      }
+    } while (startTime < endTime);
     /*StructTypeImpl sti = ResultSetHelper.getStructType(rs);
     List<Struct> queryResult = ResultSetHelper.asList(rs, sti, false);
     Log.getLogWriter().info("SS - Result for query : " + query + "\n" + queryResult.toString());*/
