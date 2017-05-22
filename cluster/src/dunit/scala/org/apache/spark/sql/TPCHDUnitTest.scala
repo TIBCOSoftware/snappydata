33124754ebe77bbb6d6c3e0c304f9378ac781ba0
Shirish Deshmukh <sdeshmukh@snappydata.io>
2017-05-22 18:31:08 +0530

    Changing the test to use thin client based connector

--- cluster/src/dunit/scala/org/apache/spark/sql/TPCHDUnitTest.scala
+++ cluster/src/dunit/scala/org/apache/spark/sql/TPCHDUnitTest.scala
@@ -34,20 +34,16 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s)
   val queries = Array("1", "2", "3", "4", "5", "6", "7", "8", "9",
     "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
     "20", "21", "22")
-  override val stopNetServersInTearDown = false
 
   protected val productDir =
     SmartConnectorFunctions.getEnvironmentVariable("SNAPPY_HOME")
 
   override def beforeClass(): Unit = {
     super.beforeClass()
-    startNetworkServersOnAllVMs()
     vm3.invoke(classOf[ClusterManagerTestBase], "startSparkCluster", productDir)
   }
 
   override def afterClass(): Unit = {
-    Array(vm3, vm2, vm1, vm0).foreach(_.invoke(getClass, "stopNetworkServers"))
-    ClusterManagerTestBase.stopNetworkServers()
     super.afterClass()
     vm3.invoke(classOf[ClusterManagerTestBase], "stopSparkCluster", productDir)
   }
@@ -55,13 +51,15 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s)
   def testSnappy(): Unit = {
     startNetworkServersOnAllVMs()
     val snc = SnappyContext(sc)
+    val clientPort = locatorNetPort
 
     // create table randomly either using smart connector or
     // from embedded mode
     if ((System.currentTimeMillis() % 2) == 0) {
       logInfo("CREATING TABLE USING SMART CONNECTOR")
+
       vm3.invoke(classOf[SmartConnectorFunctions],
-        "createTablesUsingConnector", locatorNetPort)
+        "createTablesUsingConnector", clientPort)
     } else {
       logInfo("CREATING TABLE IN EMBEDDED MODE")
       TPCHUtils.createAndLoadTables(snc, isSnappy = true)
@@ -70,7 +68,7 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s)
     TPCHUtils.validateResult(snc, isSnappy = true)
 
     vm3.invoke(classOf[SmartConnectorFunctions],
-      "queryValidationOnConnector", locatorNetPort)
+      "queryValidationOnConnector", clientPort)
   }
 
   /*
@@ -82,13 +80,14 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s)
   def _testSnappy_Tokenization(): Unit = {
     startNetworkServersOnAllVMs()
     val snc = SnappyContext(sc)
+    val clientPort = locatorNetPort
 
     // create table randomly either using smart connector or
     // from embedded mode
     if ((System.currentTimeMillis() % 2) == 0) {
       logInfo("CREATING TABLE USING SMART CONNECTOR")
       vm3.invoke(classOf[SmartConnectorFunctions],
-        "createTablesUsingConnector", ClusterManagerTestBase.locPort)
+        "createTablesUsingConnector", clientPort)
     } else {
       logInfo("CREATING TABLE IN EMBEDDED MODE")
       TPCHUtils.createAndLoadTables(snc, isSnappy = true)
@@ -99,7 +98,7 @@ class TPCHDUnitTest(s: String) extends ClusterManagerTestBase(s)
     TPCHUtils.validateResult(snc, isSnappy = true, isTokenization = true )
 
     vm3.invoke(classOf[SmartConnectorFunctions],
-      "queryValidationOnConnector", ClusterManagerTestBase.locPort)
+      "queryValidationOnConnector", clientPort)
   }
 
   def _testSpark(): Unit = {
