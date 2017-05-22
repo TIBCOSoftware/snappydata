f619cf60e49c96bc3d9ebefe32642985644fbb15
swati <ssawant@snappydata.io>
2017-05-22 11:15:15 +0530

    - Removed log statements added for debugging purpose

--- dtests/src/test/java/io/snappydata/hydra/cluster/SnappyConcurrencyTest.java
+++ dtests/src/test/java/io/snappydata/hydra/cluster/SnappyConcurrencyTest.java
@@ -19,7 +19,6 @@ package io.snappydata.hydra.cluster;
 import com.gemstone.gemfire.cache.query.Struct;
 import com.gemstone.gemfire.cache.query.internal.types.StructTypeImpl;
 import hydra.Log;
-import hydra.RemoteTestModule;
 import hydra.TestConfig;
 import sql.sqlutil.ResultSetHelper;
 import util.TestException;
@@ -27,7 +26,6 @@ import util.TestException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 
@@ -38,25 +36,18 @@ public class SnappyConcurrencyTest extends SnappyTest {
   public static void runPointLookUpQueries() throws SQLException {
     Vector<String> queryVect = SnappyPrms.getPointLookUpQueryList();
     long totalTaskTime = TestConfig.tab().longAt(totalTaskTimeSec);
-    Log.getLogWriter().info("SS - totalTaskTime : " + totalTaskTime);
     String query = null;
     Connection conn = getLocatorConnection();
     ResultSet rs;
     long startTime = System.currentTimeMillis();
-    long endTime = startTime + totalTaskTime*1000;
+    long endTime = startTime + totalTaskTime * 1000;
     while (endTime > System.currentTimeMillis()) {
       try {
         int queryNum = new Random().nextInt(queryVect.size());
         query = queryVect.elementAt(queryNum);
-        Log.getLogWriter().info("SS - Executing query : " + query);
         rs = conn.createStatement().executeQuery(query);
         SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numQueriesExecuted);
-        int numQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
-        Log.getLogWriter().info("SS - numQueriesExecuted : " + numQueries);
         SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numPointLookUpQueriesExecuted);
-        int numPointLookUpQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB
-            .numPointLookUpQueriesExecuted);
-        Log.getLogWriter().info("SS - numPointLookUpQueriesExecuted : " + numPointLookUpQueries);
       } catch (SQLException se) {
         throw new TestException("Got exception while executing pointLookUp query:" + query, se);
       }
@@ -75,19 +66,14 @@ public class SnappyConcurrencyTest extends SnappyTest {
     String query = null;
     ResultSet rs;
     long startTime = System.currentTimeMillis();
-    long endTime = startTime + totalTaskTime*1000;
+    long endTime = startTime + totalTaskTime * 1000;
     while (endTime > System.currentTimeMillis()) {
       try {
         int queryNum = new Random().nextInt(queryVect.size());
         query = (String) queryVect.elementAt(queryNum);
-        Log.getLogWriter().info("SS - Executing query : " + query);
         rs = conn.createStatement().executeQuery(query);
         SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numQueriesExecuted);
-        int numQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
-        Log.getLogWriter().info("SS - numQueriesExecuted : " + numQueries);
         SnappyBB.getBB().getSharedCounters().increment(SnappyBB.numAggregationQueriesExecuted);
-        int numAggregationQueries = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numQueriesExecuted);
-        Log.getLogWriter().info("SS - numAggregationQueriesExecuted : " + numAggregationQueries);
       } catch (SQLException se) {
         throw new TestException("Got exception while executing Analytical query:" + query, se);
       }
@@ -103,10 +89,10 @@ public class SnappyConcurrencyTest extends SnappyTest {
     int numpointLookUpQueriesExecuted = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB
         .numPointLookUpQueriesExecuted);
     int numAggregationQueriesExecuted = (int) SnappyBB.getBB().getSharedCounters().read(SnappyBB.numAggregationQueriesExecuted);
-    Log.getLogWriter().info("SS - Total number of queries executed : " + numQueriesExecuted);
-    Log.getLogWriter().info("SS - Total number of pointLookUp queries executed : " +
+    Log.getLogWriter().info("Total number of queries executed : " + numQueriesExecuted);
+    Log.getLogWriter().info("Total number of pointLookUp queries executed : " +
         numpointLookUpQueriesExecuted);
-    Log.getLogWriter().info("SS - Total number of analytical queries executed : " + numAggregationQueriesExecuted);
+    Log.getLogWriter().info("Total number of analytical queries executed : " + numAggregationQueriesExecuted);
   }
 
 }
