ccf5dbb234aee61a70c74256171cabfd249ef80c
Sumedh Wale <swale@snappydata.io>
2017-05-30 18:51:38 +0530

    [branch-jj] Changes made for working with Tableau (#621)
    Use hive metadata for column table metadata too for consistency.
    
    Link to store with [branch-jj] merged.

--- core/src/main/scala/org/apache/spark/sql/store/StoreUtils.scala
+++ core/src/main/scala/org/apache/spark/sql/store/StoreUtils.scala
@@ -97,7 +97,7 @@ object StoreUtils {
   val EMPTY_STRING = ""
   val NONE = "NONE"
 
-  val SHADOW_COLUMN_NAME = "rowid"
+  val SHADOW_COLUMN_NAME = "snappydata_internal_rowid"
 
   val SHADOW_COLUMN = s"$SHADOW_COLUMN_NAME bigint generated always as identity"
 
