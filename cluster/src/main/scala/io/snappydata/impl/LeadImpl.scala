98e347b698a66380a154dd9dee8417dd3e1a2503
ahshahid <ashahid@snappydata.io>
2017-06-02 17:14:28 -0700

    Multigrid master (#628)
    * allowing passing of gemfire-grid property to GemFireStore.boot method
    
    * allowing spark & snappydata prefixed properties to be passed to boot function of GemFireStore
    
    * removing dead code

--- cluster/src/main/scala/io/snappydata/impl/LeadImpl.scala
+++ cluster/src/main/scala/io/snappydata/impl/LeadImpl.scala
@@ -27,7 +27,7 @@ import scala.collection.JavaConverters._
 import akka.actor.ActorSystem
 import com.gemstone.gemfire.distributed.internal.DistributionConfig
 import com.gemstone.gemfire.distributed.internal.locks.{DLockService, DistributedMemberLock}
-import com.gemstone.gemfire.internal.cache.GemFireCacheImpl
+import com.gemstone.gemfire.internal.cache.{GemFireCacheImpl}
 import com.pivotal.gemfirexd.FabricService.State
 import com.pivotal.gemfirexd.internal.engine.store.ServerGroupUtils
 import com.pivotal.gemfirexd.{FabricService, NetworkInterface}
@@ -126,7 +126,7 @@ class LeadImpl extends ServerImpl with Lead
           } else {
             Constant.STORE_PROPERTY_PREFIX + k
           }
-        }
+        } 
         else {
           k
         }
