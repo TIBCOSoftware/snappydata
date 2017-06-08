837ecd27d00dc0f4876ff09d2aa2bb22fc00513c
Soubhik Chakraborty <schakraborty@snappydata.io>
2017-06-08 19:45:31 +0530

    Jdbc cdc streaming (#622)
    * First cut of cdc.
    
    * Enabling offset persistence
    
    * Adding partitionBy and partitioningQuery parameter
    
    - above parameters will be used to parallelize jdbc query on sqlserver.
    per table configuration is possible apart from generic property.
    
    - re-organised the code to not flood the sql monitoring tab with offset
    queries.
    
    - fixed few corner cases of offset calculation with limit enforced.
    
    ToDo:
    - test on wider cluster (esp. streaming cluster hosts completely
        separate from snappy cluster. Untested scenario in our test suite)
    - prefix with 'jdbc.' for all the user accepted properties relevant for
    jdbcStream
    
    * Improving the properties and now supports
    
    - partitiionByQuery that can provide custom query generating preordered
    or preranged or just a set of distinct values that can be used for range
    determination instead of the default logic of the product.
    
    * DataSource register moved into snappy-cluster
    
    Moving the JdbcStream and Sink classes registration into the cluster jar
    because the base classes using them is in cluster itself. Otherwise
    ClassNotFound exception occurs while using readStream.format("jdbcStream")
    from the user code when used from a separate maven project.
    This doesn't happen for dunits/junits etc. but shows up only when used
    from a separate project like users will do.
    
    * Statistics fix and inclusion of snappy-connector
    
    + Added insert/upsert/delete row count in the metrics as it wasn't
    showing up until now.
    
    + Accomodating snappy-connector/jdbc-stream-connector project into the
    compilation if cloned inside snappydata parent project much like aqp.
    If needed can add it as a submodule so that it gets automatically
    checkedout and the jdbc-stream-connector.jar is published as part of the
    SnappyData product distribution. Otherwise user would have to explicitly
    add this in the libs folder of the distribution during every upgrade.
    
    For now this would suffice.
    
    * Cleaning up boilder plate example code from product
    
    - IndexTest is cleaned up.
    
    * Fixing targetProject due to which snappy-cluster wasn't getting
    published and hence distribution packaging was getting wrong.
    
    * getting rid of password
    
    * Taking care of review comments
    
    * test added
    
    + added a test to verify deleteFrom with escaping of quotes. PR #648 is
    merged, so utilized escapeQuote variable.
    
    + modified the error messages to be more precise.

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/JDBCSourceAsColumnarStore.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/JDBCSourceAsColumnarStore.scala
@@ -42,7 +42,7 @@ import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.ParamLiteral
 import org.apache.spark.sql.collection._
 import org.apache.spark.sql.execution.columnar._
-import org.apache.spark.sql.execution.row.{ResultSetTraversal, RowFormatScanRDD, RowInsertExec}
+import org.apache.spark.sql.execution.row.{ResultSetTraversal, RowFormatScanRDD, RowDMLExec}
 import org.apache.spark.sql.execution.{BufferedRowIterator, ConnectionPool, RDDKryo, WholeStageCodegenExec}
 import org.apache.spark.sql.hive.ConnectorCatalog
 import org.apache.spark.sql.sources.{ConnectionProperties, Filter, JdbcExtendedUtils}
@@ -318,7 +318,7 @@ class JDBCSourceAsColumnarStore(override val connProperties: ConnectionPropertie
               otherRDDs = Seq.empty, numBuckets = -1,
               partitionColumns = Seq.empty, partitionColumnAliases = Seq.empty,
               baseRelation = null, schema, allFilters = Seq.empty, schemaAttrs)
-            val insertPlan = RowInsertExec(tableScan, upsert = true,
+            val insertPlan = RowDMLExec(tableScan, putInto = true, delete = false,
               Seq.empty, Seq.empty, -1, schema, None, onExecutor = true,
               resolvedName = null, connProperties)
             // now generate the code with the help of WholeStageCodegenExec
@@ -338,7 +338,7 @@ class JDBCSourceAsColumnarStore(override val connProperties: ConnectionPropertie
         val resolvedName = ExternalStoreUtils.lookupName(tableName,
           connection.getSchema)
         val putSQL = JdbcExtendedUtils.getInsertOrPutString(resolvedName,
-          schema, upsert = true)
+          schema, putInto = true)
         val stmt = connection.prepareStatement(putSQL)
         refs(statementRef) = stmt
         // no harm in passing a references array with extra element at end
