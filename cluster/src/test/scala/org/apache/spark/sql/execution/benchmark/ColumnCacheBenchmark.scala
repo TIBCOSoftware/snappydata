74d4c7579292de2388c9c84fa896de07d23fb6b5
Rishitesh Mishra <rmishra@snappydata.io>
2017-06-08 17:20:48 +0530

    [SNAP-1703] ArrayIndexOutOfBound when query has wide schema and a fil… (#647)
    * [SNAP-1703] ArrayIndexOutOfBound when query has wide schema and a filter clause
    
    Cursor variable was not getting advanced in the case of wide schema if a filter is applied and filter value is null for a row. Decoder cursor should get moved irrespective of whether the row is processed or not.

--- cluster/src/test/scala/org/apache/spark/sql/execution/benchmark/ColumnCacheBenchmark.scala
+++ cluster/src/test/scala/org/apache/spark/sql/execution/benchmark/ColumnCacheBenchmark.scala
@@ -204,6 +204,7 @@ class ColumnCacheBenchmark extends SnappyFunSuite {
       Column(sparkSession.sessionState.sqlParser.parseExpression(expr))
     }: _*)
 
+
     testDF.collect()
     val sql = (1 to num_col).map(i => s"C$i STRING").mkString(",")
     snappySession.sql(s"create table wide_table($sql) using column")
@@ -219,6 +220,9 @@ class ColumnCacheBenchmark extends SnappyFunSuite {
     val avgProjections = (1 to num_col).map(i => s"AVG(C$i)").mkString(",")
     val df1 = snappySession.sql(s"select $avgProjections from wide_table")
     df1.collect()
+
+    val df2 = snappySession.sql(s"select $avgProjections from wide_table where C1 = '1' ")
+    df2.collect()
   }
 }
 
