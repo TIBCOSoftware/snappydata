9d97d1f4a3b44ce56c356b253dd2408c7e5a4d11
swati <ssawant@snappydata.io>
2017-06-29 16:56:34 +0530

    - Added fix for SNAP-1751

--- dtests/src/test/java/io/snappydata/hydra/cluster/SnappyTest.java
+++ dtests/src/test/java/io/snappydata/hydra/cluster/SnappyTest.java
@@ -2223,7 +2223,6 @@ public class SnappyTest implements Serializable {
       String leadHost = productConfDirPath + sep + "leadHost";
       String leadPort = productConfDirPath + sep + "leadPort";
       String masterHost = productConfDirPath + sep + "masterHost";
-      String masterPort = productConfDirPath + sep + "masterPort";
       String primaryLocatorHost = productConfDirPath + sep + "primaryLocatorHost";
       String primaryLocatorPort = productConfDirPath + sep + "primaryLocatorPort";
       Files.delete(Paths.get(locatorList));
@@ -2231,7 +2230,6 @@ public class SnappyTest implements Serializable {
       Files.delete(Paths.get(leadHost));
       Files.delete(Paths.get(leadPort));
       Files.delete(Paths.get(masterHost));
-      Files.delete(Paths.get(masterPort));
       Files.delete(Paths.get(primaryLocatorHost));
       Files.delete(Paths.get(primaryLocatorPort));
       Log.getLogWriter().info("Long Running Test artifacts deleted.");
