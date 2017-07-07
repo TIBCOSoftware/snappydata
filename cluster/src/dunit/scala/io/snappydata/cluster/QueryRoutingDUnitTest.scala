b86c38f009a760bf9d0accac4447da826b56e551
vivekwiz <vbhaskar@snappydata.io>
2017-07-07 17:59:51 +0530

    [SNAP-1780] Disabling QueryRoutingDUnitTest.testQueryRouting

--- cluster/src/dunit/scala/io/snappydata/cluster/QueryRoutingDUnitTest.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/QueryRoutingDUnitTest.scala
@@ -53,7 +53,7 @@ class QueryRoutingDUnitTest(val s: String)
     super.tearDown2()
   }
 
-  def testQueryRouting(): Unit = {
+  def _testQueryRouting(): Unit = {
     val netPort1 = AvailablePortHelper.getRandomAvailableTCPPort
     vm2.invoke(classOf[ClusterManagerTestBase], "startNetServer", netPort1)
 
