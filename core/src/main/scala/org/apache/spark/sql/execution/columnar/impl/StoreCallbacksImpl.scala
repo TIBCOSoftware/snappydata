de66ac42168c60bd54c0643d84efcff663f7d36b
Amogh Shetkar <ashetkar@snappydata.io>
2017-08-20 23:54:41 +0530

    * Put additional checks before loading some classes.

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/StoreCallbacksImpl.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/StoreCallbacksImpl.scala
@@ -320,6 +320,9 @@ object StoreCallbacksImpl extends StoreCallbacks with Logging with Serializable 
 
   override def shouldStopRecovery(): Boolean =
     MemoryManagerCallback.memoryManager.shouldStopRecovery()
+
+  override def isEnterpriseEdition: Boolean =
+    SnappySession.isEnterpriseEdition
 }
 
 trait StoreCallback extends Serializable {
