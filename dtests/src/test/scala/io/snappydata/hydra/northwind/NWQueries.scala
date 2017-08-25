4bacdcf09084266fd498375aadaa7d82a442c29c
Swati Sawant <ssawant@snappydata.io>
2017-08-25 20:19:03 +0530

    Add a hydra test for stress testing with eviction (#807)
    - Added a new eviction test which is evicting lot of data while the queries are getting executed
    - Changes in existing tests to work as per the new property required for CreateAndLoadNWTables spark app and snappy job
    - Modified task for adding new server node with rebalance option to use servers.sh script for startup instead of snappy shell way

--- dtests/src/test/scala/io/snappydata/hydra/northwind/NWQueries.scala
+++ dtests/src/test/scala/io/snappydata/hydra/northwind/NWQueries.scala
@@ -514,10 +514,10 @@ object NWQueries {
       .load(s"${snc.getConf("dataFilesLocation")}/categories.csv")
 
   val categories_table = "create table categories (" +
-    "CategoryID int, " +
-    "CategoryName string, " +
-    "Description string, " +
-    "Picture blob)"
+      "CategoryID int, " +
+      "CategoryName string, " +
+      "Description string, " +
+      "Picture blob)"
 
   def shippers(sqlContext: SQLContext): DataFrame = sqlContext.read.format("com.databricks.spark" +
       ".csv")
@@ -541,27 +541,27 @@ object NWQueries {
       .load(s"${snc.getConf("dataFilesLocation")}/employees.csv")
 
   val employees_table = "create table employees(" +
-    //    "EmployeeID int not null , " +
-    //    "LastName string not null, " +
-    //    "FirstName string not null, " +
-    "EmployeeID int, " +
-    "LastName string,  " +
-    "FirstName string, " +
-    "Title string, " +
-    "TitleOfCourtesy string, " +
-    "BirthDate timestamp, " +
-    "HireDate timestamp, " +
-    "Address string, " +
-    "City string, " +
-    "Region string, " +
-    "PostalCode string, " +
-    "Country string, " +
-    "HomePhone string, " +
-    "Extension string, " +
-    "Photo blob, " +
-    "Notes string, " +
-    "ReportsTo int, " +
-    "PhotoPath string)"
+      //    "EmployeeID int not null , " +
+      //    "LastName string not null, " +
+      //    "FirstName string not null, " +
+      "EmployeeID int, " +
+      "LastName string,  " +
+      "FirstName string, " +
+      "Title string, " +
+      "TitleOfCourtesy string, " +
+      "BirthDate timestamp, " +
+      "HireDate timestamp, " +
+      "Address string, " +
+      "City string, " +
+      "Region string, " +
+      "PostalCode string, " +
+      "Country string, " +
+      "HomePhone string, " +
+      "Extension string, " +
+      "Photo blob, " +
+      "Notes string, " +
+      "ReportsTo int, " +
+      "PhotoPath string)"
 
   def customers(sqlContext: SQLContext): DataFrame = sqlContext.read.format("com.databricks" +
       ".spark.csv")
@@ -594,21 +594,39 @@ object NWQueries {
       .load(s"${snc.getConf("dataFilesLocation")}/orders.csv")
 
   val orders_table = "create table orders (" +
-    // "OrderID int not null, " +
-    "OrderID int, " +
-    "CustomerID string, " +
-    "EmployeeID int, " +
-    "OrderDate timestamp, " +
-    "RequiredDate timestamp, " +
-    "ShippedDate timestamp, " +
-    "ShipVia int, " +
-    "Freight double, " +
-    "ShipName string, " +
-    "ShipAddress string, " +
-    "ShipCity string, " +
-    "ShipRegion string, " +
-    "ShipPostalCode string, " +
-    "ShipCountry string)"
+      // "OrderID int not null, " +
+      "OrderID int, " +
+      "CustomerID string, " +
+      "EmployeeID int, " +
+      "OrderDate timestamp, " +
+      "RequiredDate timestamp, " +
+      "ShippedDate timestamp, " +
+      "ShipVia int, " +
+      "Freight double, " +
+      "ShipName string, " +
+      "ShipAddress string, " +
+      "ShipCity string, " +
+      "ShipRegion string, " +
+      "ShipPostalCode string, " +
+      "ShipCountry string)"
+
+  val large_orders_table = "create table orders (" +
+      // "OrderID int not null, " +
+      "OrderID int, " +
+      "CustomerID string, " +
+      "EmployeeID int, " +
+      "OrderDate timestamp, " +
+      "RequiredDate timestamp, " +
+      "ShippedDate timestamp, " +
+      "ShipVia int, " +
+      "Freight double, " +
+      "ShipName string, " +
+      "ShipAddress string, " +
+      "ShipCity string, " +
+      "ShipRegion string, " +
+      "ShipPostalCode string, " +
+      "ShipCountry string," +
+      "bigComment string)"
 
   def order_details(sqlContext: SQLContext): DataFrame = sqlContext.read.format("com.databricks" +
       ".spark.csv")
@@ -620,16 +638,29 @@ object NWQueries {
 
 
   val order_details_table = "create table order_details (" +
-    //    "OrderID int not null, " +
-    //    "ProductID int not null, " +
-    //    "UnitPrice double not null, " +
-    //    "Quantity smallint not null, " +
-    //    "Discount double not null)"
-    "OrderID int, " +
-    "ProductID int, " +
-    "UnitPrice double, " +
-    "Quantity smallint, " +
-    "Discount double)"
+      //    "OrderID int not null, " +
+      //    "ProductID int not null, " +
+      //    "UnitPrice double not null, " +
+      //    "Quantity smallint not null, " +
+      //    "Discount double not null)"
+      "OrderID int, " +
+      "ProductID int, " +
+      "UnitPrice double, " +
+      "Quantity smallint, " +
+      "Discount double)"
+
+  val large_order_details_table = "create table order_details (" +
+      //    "OrderID int not null, " +
+      //    "ProductID int not null, " +
+      //    "UnitPrice double not null, " +
+      //    "Quantity smallint not null, " +
+      //    "Discount double not null)"
+      "OrderID int, " +
+      "ProductID int, " +
+      "UnitPrice double, " +
+      "Quantity smallint, " +
+      "Discount double," +
+      "bigComment string)"
 
   def products(sqlContext: SQLContext): DataFrame = sqlContext.read.format("com.databricks.spark" +
       ".csv")
@@ -640,19 +671,19 @@ object NWQueries {
       .load(s"${snc.getConf("dataFilesLocation")}/products.csv")
 
   val products_table = "create table products(" +
-    // "ProductID int not null, " +
-    "ProductID int, " +
-    "ProductName string, " +
-    //    "SupplierID int not null, " +
-    //    "CategoryID int not null," +
-    "SupplierID int, " +
-    "CategoryID int," +
-    "QuantityPerUnit string, " +
-    "UnitPrice double, " +
-    "UnitsInStock smallint, " +
-    "UnitsOnOrder smallint," +
-    "ReorderLevel smallint, " +
-    "Discontinued smallint) "
+      // "ProductID int not null, " +
+      "ProductID int, " +
+      "ProductName string, " +
+      //    "SupplierID int not null, " +
+      //    "CategoryID int not null," +
+      "SupplierID int, " +
+      "CategoryID int," +
+      "QuantityPerUnit string, " +
+      "UnitPrice double, " +
+      "UnitsInStock smallint, " +
+      "UnitsOnOrder smallint," +
+      "ReorderLevel smallint, " +
+      "Discontinued smallint) "
 
   def suppliers(sqlContext: SQLContext): DataFrame = sqlContext.read.format("com.databricks" +
       ".spark.csv")
@@ -663,20 +694,20 @@ object NWQueries {
       .load(s"${snc.getConf("dataFilesLocation")}/suppliers.csv")
 
   val suppliers_table = "create table suppliers(" +
-    //    "SupplierID int not null, " +
-    //    "CompanyName string not null, " +
-    "SupplierID int, " +
-    "CompanyName string, " +
-    "ContactName string, " +
-    "ContactTitle string, " +
-    "Address string, " +
-    "City string, " +
-    "Region string, " +
-    "PostalCode string, " +
-    "Country string, " +
-    "Phone string, " +
-    "Fax string, " +
-    "HomePage string) "
+      //    "SupplierID int not null, " +
+      //    "CompanyName string not null, " +
+      "SupplierID int, " +
+      "CompanyName string, " +
+      "ContactName string, " +
+      "ContactTitle string, " +
+      "Address string, " +
+      "City string, " +
+      "Region string, " +
+      "PostalCode string, " +
+      "Country string, " +
+      "Phone string, " +
+      "Fax string, " +
+      "HomePage string) "
 
   def territories(sqlContext: SQLContext): DataFrame = sqlContext.read.format("com.databricks" +
       ".spark.csv")
@@ -687,12 +718,12 @@ object NWQueries {
       .load(s"${snc.getConf("dataFilesLocation")}/territories.csv")
 
   val territories_table = "create table territories(" +
-    //    "TerritoryID string not null, " +
-    //    "TerritoryDescription string not null, " +
-    //    "RegionID string not null)"
-    "TerritoryID string, " +
-    "TerritoryDescription string, " +
-    "RegionID string)"
+      //    "TerritoryID string not null, " +
+      //    "TerritoryDescription string not null, " +
+      //    "RegionID string not null)"
+      "TerritoryID string, " +
+      "TerritoryDescription string, " +
+      "RegionID string)"
 
   def employee_territories(sqlContext: SQLContext): DataFrame = sqlContext.read.format("com" +
       ".databricks.spark.csv")
