e03f9fe7299267052e80dbdaf7f60a7d71fd3819
Shirish Deshmukh <sdeshmukh@snappydata.io>
2017-08-29 07:51:19 +0530

    Test changes

--- cluster/src/dunit/scala/io/snappydata/cluster/SetIsolationDUnitTest.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/SetIsolationDUnitTest.scala
@@ -58,8 +58,22 @@ class SetIsolationDUnitTest (val s: String)
       stmt.execute(query)
       assert(false, "query should have failed as tx on column table is not allowed")
     } catch {
-      case se: SQLException if SQLState.SNAPPY_OP_DISALLOWED_ON_COLUMN_TABLES.
-          startsWith(se.getSQLState) => // expected
+      case sq: SQLException if SQLState.SNAPPY_OP_DISALLOWED_ON_COLUMN_TABLES.
+          startsWith(sq.getSQLState) => // expected
+
+//      case e: Throwable =>
+//        var cause = e
+//        var expectedException = false
+//        while(cause != null && !expectedException) {
+//          cause match {
+//            case sq: SQLException if SQLState.SNAPPY_OP_DISALLOWED_ON_COLUMN_TABLES.
+//                startsWith(sq.getSQLState) =>
+//              expectedException = true
+//            case _ =>
+//              cause = cause.getCause
+//          }
+//        }
+//        if (!expectedException) throw e
     }
   }
 
@@ -81,6 +95,7 @@ class SetIsolationDUnitTest (val s: String)
     rs1 = stmt1.executeQuery(s"select count(*) from $tableName")
     assert(rs1.next())
     assert(rs1.getInt(1) == 100, "result mismatch")
+    stmt1.close()
   }
 
   def testSetIsolationLevel(): Unit = {
@@ -88,29 +103,33 @@ class SetIsolationDUnitTest (val s: String)
     vm2.invoke(classOf[ClusterManagerTestBase], "startNetServer", netPort1)
     val conn = getANetConnection(netPort1)
 
+    logInfo("Creating tables for the test")
     createTables(conn)
 
     // with autocommit true transactions on row and column table are allowed
+    logInfo("setting autocommit true")
     conn.setAutoCommit(true)
     conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED)
     validateTableData(conn)
-    performOperationsOnTable(conn, "rowtable")
+//    performOperationsOnTable(conn, "rowtable")
     performOperationsOnTable(conn, "coltable")
 
     // with autocommit false transactions allowed on row tables only
+    logInfo("setting autocommit false")
     conn.setAutoCommit(false)
     conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED)
-    validateTableData(conn)
     performOperationsOnTable(conn, "rowtable")
-//    conn.commit()
 
     val stmt1 = conn.createStatement()
+    logInfo("checking unsupported queries on column tables")
     checkTxQueryOnColumnTable(stmt1, "select count(*) from coltable")
 //    checkTxQueryOnColumnTable(stmt1, "insert into coltable values(101, 101, 101)")
-//    checkTxQueryOnColumnTable(stmt1, "delete from coltable where col1 = 101")
-//    checkTxQueryOnColumnTable(stmt1, "update coltable set col1 = 101")
-//    checkTxQueryOnColumnTable(stmt1, "select rowtable.col1 as rc1, coltable.col1 as cc1" +
-//        " from rowtable, coltable where rowtable.col1 = coltable.col1")
+    checkTxQueryOnColumnTable(stmt1, "delete from coltable where col1 = 101")
+    checkTxQueryOnColumnTable(stmt1, "update coltable set col2 = 101")
+    checkTxQueryOnColumnTable(stmt1, "select rowtable.col1 as rc1, coltable.col1 as cc1" +
+        " from rowtable, coltable where rowtable.col1 = coltable.col1")
+    stmt1.close()
+    conn.close()
 
   }
 
