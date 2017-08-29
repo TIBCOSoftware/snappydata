6a55fa5f380c02d315122419c021455ab778813e
Shirish Deshmukh <sdeshmukh@snappydata.io>
2017-08-29 17:49:56 +0530

    Remove debug statements

--- cluster/src/dunit/scala/io/snappydata/cluster/ClusterManagerTestBase.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/ClusterManagerTestBase.scala
@@ -48,10 +48,10 @@ abstract class ClusterManagerTestBase(s: String)
 
   val bootProps: Properties = new Properties()
   bootProps.setProperty("log-file", "snappyStore.log")
-  bootProps.setProperty("log-level", "fine")
+  bootProps.setProperty("log-level", "config")
   // Easier to switch ON traces. thats why added this.
-   bootProps.setProperty("gemfirexd.debug.true",
-     "QueryDistribution,TraceExecution,TraceActivation,TraceTran")
+//   bootProps.setProperty("gemfirexd.debug.true",
+//     "QueryDistribution,TraceExecution,TraceActivation,TraceTran")
   bootProps.setProperty("statistic-archive-file", "snappyStore.gfs")
   bootProps.setProperty("spark.executor.cores",
     TestUtils.defaultCores.toString)
