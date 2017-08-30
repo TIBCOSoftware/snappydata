c9c2a800a5bd1eba4fb59cfd0ff2264deb1e7412
Shirish Deshmukh <sdeshmukh@snappydata.io>
2017-08-30 10:49:15 +0530

    Addressing scalatest failure

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/ExternalStoreUtils.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/ExternalStoreUtils.scala
@@ -198,7 +198,7 @@ object ExternalStoreUtils {
           case ExternalEmbeddedMode(_, url) =>
             Constant.DEFAULT_EMBEDDED_URL + ";host-data=false;" + url
           case LocalMode(_, url) =>
-            Constant.DEFAULT_EMBEDDED_URL + ";" + url
+            Constant.DEFAULT_EMBEDDED_URL + ";" + url + ";internal-connection=true"
           case ExternalClusterMode(_, url) =>
             throw new AnalysisException("Option 'url' not specified for cluster " +
                 url)
