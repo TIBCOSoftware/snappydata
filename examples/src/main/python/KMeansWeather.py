0ff1a37316abaaa37db63d1b2962d524c485ccca
rmishra <rmishra@pivotal.io>
2017-09-07 13:08:43 +0530

    Added more comments

--- examples/src/main/python/KMeansWeather.py
+++ examples/src/main/python/KMeansWeather.py
@@ -1,3 +1,24 @@
+#
+# Copyright (c) 2017 SnappyData, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You
+# may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License. See accompanying
+# LICENSE file.
+#
+
+# This example uses some random ID and random temperature to
+# prepare the data. After preparing the model, it applies the original data set to
+# predict the temperature
+
 from __future__ import print_function
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
@@ -7,7 +28,7 @@ from pyspark.ml.clustering import KMeans, KMeansModel
 import random
 import os
 
-
+# Create SnappyData Tables and insert synthetic data
 def createPartitionedTableUsingSQL(snappy):
     snappy.sql("DROP TABLE IF EXISTS WEATHER")
     snappy.sql("CREATE TABLE WEATHER(" +
@@ -28,7 +49,7 @@ def createPartitionedTableUsingSQL(snappy):
 
     print("DONE")
 
-
+#
 def applyKMeans(snappy):
     # Selects and parses the data from the table created earlier
     data = snappy.sql("SELECT id, WeatherDegrees FROM WEATHER")
@@ -49,7 +70,7 @@ def applyKMeans(snappy):
 
     a.close()
 
-    # Trains the data in order to pass it to the Kmeans Clustering Function
+    # Trains the data in order to pass it to the KMeans Clustering Function
     dataset = snappy.read.format("libsvm").load("weatherdata/parsedData.txt")
     print("dataset is " + str(dataset))
     kmeans = KMeans().setK(4).setSeed(2)
