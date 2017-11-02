74cbe1597982cef67d2377cd6bd155126cc70699
KishorBachhav <kbachhav@snappydata.io>
2017-11-02 14:40:05 +0530

    --Incorporated review changes

--- tests/benchmark/snappy/dynamic/PerfComparison.sh
+++ tests/benchmark/snappy/dynamic/PerfComparison.sh
@@ -1,3 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# compare the performance of some number of runs
+#
 
-java -ea -cp "/home/kishor/SNAPPY/CHECKOUT/snappydata/cluster/build-artifacts/scala-2.11/libs/snappydata-cluster_2.11-1.0.0-tests.jar" io.snappydata.benchmark.snappy.TPCHPerfComparer /home/kishor/SNAPPY/OUTPUT/HYDRA/smokePerf/
+trap 'exit 1' 2 #traps Ctrl-C (signal 2)
+
+USAGE="Usage: $0 dir"
+
+if [ "$#" == "0" ]; then
+  echo "$USAGE"
+  exit 1
+fi
+
+java -ea -cp $Snappydata/cluster/build-artifacts/scala-2.11/libs/snappydata-cluster_2.11-1.0.0-tests.jar io.snappydata.benchmark.snappy.TPCHPerfComparer $1
