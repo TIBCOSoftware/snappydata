7f8b135dff745639b56359eb60cd13d4e53f1405
vivekwiz <vbhaskar@snappydata.io>
2017-11-24 20:50:30 +0530

    Temporary fix to handle negative ordinal value which allowed scanning of sorted-inserted rows while sorted insert. TODO: Need to fix for negative vs positive value = 0

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/encoding/UpdatedColumnDecoder.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/encoding/UpdatedColumnDecoder.scala
@@ -221,6 +221,9 @@ abstract class UpdatedColumnDecoderBase(decoder: ColumnDecoder, field: StructFie
     if (isCaseOfUpdate) {
       if (nextUpdatedPosition == Int.MinValue) {
         nextUpdatedPosition = moveToNextUpdatedPosition(-1)
+        if (nextUpdatedPosition <= 0) {
+          nextUpdatedPosition = Int.MaxValue
+        }
       }
       // Original
       if (nextUpdatedPosition > ordinal) true
