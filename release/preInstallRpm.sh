9eaca3f71903a4d02719e1952ae90d942d713f2e
Amogh Shetkar <ashetkar@snappydata.io>
2017-12-12 12:30:55 +0530

    * Bundle aws-java-sdk 1.7.4 (instead of 1.11.211) jar which is compatible with hadoop-aws 2.7.3 jar * Minor updates to pre-install scripts on snappydata installers. * Changes by @sumwale

--- release/preInstallRpm.sh
+++ release/preInstallRpm.sh
@@ -11,6 +11,3 @@ if ! getent passwd $SNAPPY_USER > /dev/null; then
   useradd -r -M -d $SNAPPY_HOME -s /bin/bash -N -g $SNAPPY_GROUP \
       -c "SnappyData cluster owner" $SNAPPY_USER
 fi
-
-# remove old system profiles if present
-rm -f /opt/$SNAPPY_HOME/profile.d/snappydata.*sh
