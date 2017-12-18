468747461241959dafb92a2cd12075e31500e231
Sumedh Wale <swale@snappydata.io>
2017-12-19 05:12:17 +0530

    publish initial table stats at the end of hive metastore initialization

--- core/src/main/java/io/snappydata/impl/SnappyHiveCatalog.java
+++ core/src/main/java/io/snappydata/impl/SnappyHiveCatalog.java
@@ -512,7 +512,7 @@ public class SnappyHiveCatalog implements ExternalCatalog {
             getTable(hmc, "APP", "DUMMY");
           } catch (SQLException ignored) {
           }
-          return;
+          break;
         } catch (Exception ex) {
           Throwable t = ex;
           boolean noDataStoreFound = false;
@@ -545,6 +545,9 @@ public class SnappyHiveCatalog implements ExternalCatalog {
           }
         }
       }
+      // publish the column table stats at this point because that
+      // requires the hive metastore
+      Misc.getMemStoreBooting().getDatabase().publishColumnStats();
     }
 
     private Table getTable(Hive hmc, String dbName, String tableName) throws SQLException {
