6e11ccb295400b2dd3236ff64f011179b21e391e
kneeraj <kneeraj@snappydata.io>
2018-01-15 19:18:51 +0530

    Snap 2114 (#922)
    * Try plan caching for only snappy tables. Fixes SNAP-2114.

--- core/src/main/scala/org/apache/spark/sql/SnappySession.scala
+++ core/src/main/scala/org/apache/spark/sql/SnappySession.scala
@@ -2061,8 +2061,13 @@ object SnappySession extends Logging {
       localCollect, allLiterals, queryHints, executionTime, executionId)
 
     // if this has in-memory caching then don't cache since plan can change
-    // dynamically after caching due to unpersist etc
-    if (executedPlan.find(_.isInstanceOf[InMemoryTableScanExec]).isDefined) {
+    // dynamically after caching due to unpersist etc. Also do not cache
+    // if snappy tables are no there
+    if (executedPlan.find(t => t match {
+      case imse : InMemoryTableScanExec => true
+      case dsc: DataSourceScanExec => !dsc.relation.isInstanceOf[DependentRelation]
+      case _ => false
+    }).isDefined) {
       throw new EntryExistsException("uncached plan", cdf)
     }
     cdf
