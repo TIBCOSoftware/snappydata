92f38acddc344a657c40749c65fc73535d2bfdc8
Sumedh Wale <swale@snappydata.io>
2018-01-23 16:31:42 +0530

    cleaning up handling of update positions in UpdatedColumnDecoder
    also fixes issue noted in SNAP-2118 for updated columns where the same
    ordinal can be queried multiple times

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/encoding/UpdatedColumnDecoder.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/encoding/UpdatedColumnDecoder.scala
@@ -31,10 +31,8 @@ import org.apache.spark.sql.types._
  * a nullable or non-nullable version as appropriate.
  */
 final class UpdatedColumnDecoder(decoder: ColumnDecoder, field: StructField,
-    delta1Position: Int, delta1: ColumnDeltaDecoder,
-    delta2Position: Int, delta2: ColumnDeltaDecoder)
-    extends UpdatedColumnDecoderBase(decoder, field, delta1Position, delta1,
-      delta2Position, delta2) {
+    delta1: ColumnDeltaDecoder, delta2: ColumnDeltaDecoder)
+    extends UpdatedColumnDecoderBase(decoder, field, delta1, delta2) {
 
   protected def nullable: Boolean = false
 
@@ -45,10 +43,8 @@ final class UpdatedColumnDecoder(decoder: ColumnDecoder, field: StructField,
  * Nullable version of [[UpdatedColumnDecoder]].
  */
 final class UpdatedColumnDecoderNullable(decoder: ColumnDecoder, field: StructField,
-    delta1Position: Int, delta1: ColumnDeltaDecoder,
-    delta2Position: Int, delta2: ColumnDeltaDecoder)
-    extends UpdatedColumnDecoderBase(decoder, field, delta1Position, delta1,
-      delta2Position, delta2) {
+    delta1: ColumnDeltaDecoder, delta2: ColumnDeltaDecoder)
+    extends UpdatedColumnDecoderBase(decoder, field, delta1, delta2) {
 
   protected def nullable: Boolean = true
 
@@ -59,101 +55,70 @@ object UpdatedColumnDecoder {
   def apply(decoder: ColumnDecoder, field: StructField,
       delta1Buffer: ByteBuffer, delta2Buffer: ByteBuffer): UpdatedColumnDecoderBase = {
 
-    // positions are initialized at max so that they always are greater
-    // than a valid index
-
-    var delta1Position = Int.MaxValue
-    val delta1 = if (delta1Buffer ne null) {
-      val d = new ColumnDeltaDecoder(delta1Buffer, field)
-      delta1Position = d.moveToNextPosition()
-      d
-    } else null
-
-    var delta2Position = Int.MaxValue
-    val delta2 = if (delta2Buffer ne null) {
-      val d = new ColumnDeltaDecoder(delta2Buffer, field)
-      delta2Position = d.moveToNextPosition()
-      d
-    } else null
-
+    val delta1 = if (delta1Buffer ne null) new ColumnDeltaDecoder(delta1Buffer, field) else null
+    val delta2 = if (delta2Buffer ne null) new ColumnDeltaDecoder(delta2Buffer, field) else null
     // check for nullable column (don't use actual number of nulls to avoid changing
     //   type of decoder that can cause trouble with JVM inlining)
     if (field.nullable) {
-      new UpdatedColumnDecoderNullable(decoder, field, delta1Position, delta1,
-        delta2Position, delta2)
+      new UpdatedColumnDecoderNullable(decoder, field, delta1, delta2)
     } else {
-      new UpdatedColumnDecoder(decoder, field, delta1Position, delta1,
-        delta2Position, delta2)
+      new UpdatedColumnDecoder(decoder, field, delta1, delta2)
     }
   }
 }
 
 abstract class UpdatedColumnDecoderBase(decoder: ColumnDecoder, field: StructField,
-    private final var delta1Position: Int, delta1: ColumnDeltaDecoder,
-    private final var delta2Position: Int, delta2: ColumnDeltaDecoder) {
+    delta1: ColumnDeltaDecoder, delta2: ColumnDeltaDecoder) {
 
   protected def nullable: Boolean
 
-  protected final var nextDeltaBuffer: ColumnDeltaDecoder = _
+  private final var delta1UpdatedPosition: Int =
+    if (delta1 ne null) delta1.readUpdatedPosition() else Int.MaxValue
+  private final var delta2UpdatedPosition: Int =
+    if (delta2 ne null) delta2.readUpdatedPosition() else Int.MaxValue
+
   protected final var currentDeltaBuffer: ColumnDeltaDecoder = _
-  protected final var nextUpdatedPosition: Int = moveToNextUpdatedPosition(-1)
+  protected final var nextUpdatedPosition: Int = moveToNextUpdatedPosition()
 
   final def getCurrentDeltaBuffer: ColumnDeltaDecoder = currentDeltaBuffer
 
-  @inline protected final def skipUpdatedPosition(delta: ColumnDeltaDecoder): Unit = {
-    if (!nullable || delta.readNotNull) delta.nextNonNullPosition()
-    delta.nextPosition()
-  }
-
-  protected final def moveToNextUpdatedPosition(ordinal: Int): Int = {
+  protected final def moveToNextUpdatedPosition(): Int = {
     var next = Int.MaxValue
-    var movedIndex = -1
-
+    var firstDelta = false
     // first delta is the lowest in hierarchy and overrides others
-    if (delta1Position < next) {
-      next = delta1Position
-      movedIndex = 0
+    if (delta1UpdatedPosition != Int.MaxValue) {
+      next = delta1UpdatedPosition
+      firstDelta = true
     }
-    // next delta in hierarchy
-    if (delta2Position < next) {
-      // skip on equality (result should be returned by the previous call)
-      if (delta2Position <= ordinal) {
-        skipUpdatedPosition(delta2)
-        delta2Position = delta2.moveToNextPosition()
-        if (delta2Position < next) {
-          next = delta2Position
-          movedIndex = 1
+    // check next delta in hierarchy
+    if (delta2 ne null) {
+      if (delta2UpdatedPosition <= next) {
+        if (delta2UpdatedPosition < next) {
+          next = delta2UpdatedPosition
+          currentDeltaBuffer = delta2
+          firstDelta = false
         }
-      } else {
-        next = delta2Position
-        movedIndex = 1
+        // skip on equality in any case (result should be returned by delta1)
+        delta2.moveUpdatePositionCursor()
+        delta2UpdatedPosition = delta2.readUpdatedPosition()
       }
     }
-    movedIndex match {
-      case 0 =>
-        delta1Position = delta1.moveToNextPosition()
-        nextDeltaBuffer = delta1
-      case 1 =>
-        delta2Position = delta2.moveToNextPosition()
-        nextDeltaBuffer = delta2
-      case _ =>
+    if (firstDelta) {
+      currentDeltaBuffer = delta1
+      delta1.moveUpdatePositionCursor()
+      delta1UpdatedPosition = delta1.readUpdatedPosition()
     }
     next
   }
 
   private def skipUntil(ordinal: Int): Boolean = {
-    var nextUpdated = nextUpdatedPosition
-    do {
-      // skip the position in current delta
-      skipUpdatedPosition(nextDeltaBuffer)
+    while (true) {
       // update the cursor and keep on till ordinal is not reached
-      nextUpdated = moveToNextUpdatedPosition(nextUpdated)
-    } while (nextUpdated < ordinal)
-    nextUpdatedPosition = nextUpdated
-    if (nextUpdated > ordinal) return true
-    currentDeltaBuffer = nextDeltaBuffer
-    nextUpdatedPosition = moveToNextUpdatedPosition(ordinal)
-    false
+      nextUpdatedPosition = moveToNextUpdatedPosition()
+      if (nextUpdatedPosition > ordinal) return true
+      if (nextUpdatedPosition == ordinal) return false
+    }
+    false // never reached
   }
 
   final def unchanged(ordinal: Int): Boolean = {
