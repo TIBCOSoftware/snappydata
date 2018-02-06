9dbce8e6159096a3f82da121f267c42749bcca69
Sumedh Wale <swale@snappydata.io>
2018-02-06 12:03:15 +0530

    enable linkPartitionsToBuckets when preferPrimaries is on
    also disabled partition pruner in smart connector for now due to SNAP-2194

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/JDBCSourceAsColumnarStore.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/JDBCSourceAsColumnarStore.scala
@@ -547,7 +547,8 @@ class JDBCSourceAsColumnarStore(private var _connProperties: ConnectionPropertie
             connProperties.driver, connProperties.dialect, poolProps,
             connProperties.connProps, connProperties.executorConnProps,
             connProperties.hikariCP),
-          schema, this, parts, prunePartitions, embdClusterRelDestroyVersion, delayRollover)
+          // TODO: correct to proper pruner as part of SNAP-2194
+          schema, this, parts, -1, embdClusterRelDestroyVersion, delayRollover)
     }
   }
 
