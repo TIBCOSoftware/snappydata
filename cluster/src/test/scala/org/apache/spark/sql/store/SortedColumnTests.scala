76ad944c9b958c9275d3b1e5be53d47237eca40f
vivekwiz <vbhaskar@snappydata.io>
2018-02-21 14:26:42 +0530

    Added a DUnit test

--- cluster/src/test/scala/org/apache/spark/sql/store/SortedColumnTests.scala
+++ cluster/src/test/scala/org/apache/spark/sql/store/SortedColumnTests.scala
@@ -16,6 +16,7 @@
  */
 
 package org.apache.spark.sql.store
+
 import scala.collection.mutable
 
 import io.snappydata.Property
@@ -57,89 +58,89 @@ class SortedColumnTests extends ColumnTablesTestBase {
   test("basic insert") {
     SortedColumnTests.testBasicInsert(this.snc.snappySession)
   }
+}
 
-  object SortedColumnTests extends Logging {
+object SortedColumnTests extends Logging {
 
-    def testBasicInsert(session: SnappySession): Unit = {
-      session.conf.set(Property.ColumnMaxDeltaRows.name, "100")
-      session.conf.set(Property.HashJoinSize.name, "-1")
-      session.conf.set("spark.sql.autoBroadcastJoinThreshold", (-1).toString)
+  def testBasicInsert(session: SnappySession): Unit = {
+    session.conf.set(Property.ColumnMaxDeltaRows.name, "100")
+    session.conf.set(Property.HashJoinSize.name, "-1")
+    session.conf.set("spark.sql.autoBroadcastJoinThreshold", (-1).toString)
 
-      val numElements = 551
+    val numElements = 551
 
-      session.sql("drop table if exists colDeltaTable")
+    session.sql("drop table if exists colDeltaTable")
 
-      session.sql("create table colDeltaTable (id int, addr string, status boolean) " +
-          "using column options(buckets '2', partition_by 'id', key_columns 'id')")
+    session.sql("create table colDeltaTable (id int, addr string, status boolean) " +
+        "using column options(buckets '2', partition_by 'id', key_columns 'id')")
 
-      snc.sql("create table row_table(id int, addr string, status boolean)")
+    session.sql("create table row_table(id int, addr string, status boolean)")
 
-      session.range(numElements).filter(_  % 10 < 6).selectExpr("id",
-        "concat('addr', cast(id as string))",
-        "case when (id % 2) = 0 then true else false end").write.insertInto("colDeltaTable")
+    session.range(numElements).filter(_ % 10 < 6).selectExpr("id",
+      "concat('addr', cast(id as string))",
+      "case when (id % 2) = 0 then true else false end").write.insertInto("colDeltaTable")
 
-      session.range(numElements).filter(_  % 10 > 5).selectExpr("id",
-        "concat('addr', cast(id as string))",
-        "case when (id % 2) = 0 then true else false end").write.insertInto("row_table")
+    session.range(numElements).filter(_ % 10 > 5).selectExpr("id",
+      "concat('addr', cast(id as string))",
+      "case when (id % 2) = 0 then true else false end").write.insertInto("row_table")
 
-      def verifyTotalRows(assertCount: Int, callCount: Int) : Unit = {
-        val rs1 = session.sql("select * from colDeltaTable").collect()
+    def verifyTotalRows(assertCount: Int, callCount: Int): Unit = {
+      val rs1 = session.sql("select * from colDeltaTable").collect()
+      // scalastyle:off println
+      println("")
+      println(s"verifyTotalRows $callCount = " + rs1.length)
+      println("")
+      // scalastyle:on println
+      var i = 0
+      val allRows = mutable.SortedSet[Int]()
+      if (callCount == 2) {
+        List.range(0, numElements).foreach(allRows += _)
+      }
+      rs1.foreach(r => {
+        val firstRow = r.getInt(0)
         // scalastyle:off println
-        println("")
-        println(s"verifyTotalRows $callCount = " + rs1.length)
-        println("")
+        println(s"verifyTotalRows : " + i + " = " + firstRow)
         // scalastyle:on println
-        var i = 0
-        val allRows = mutable.SortedSet[Int]()
+        i = i + 1
         if (callCount == 2) {
-          List.range(0, numElements).foreach(allRows += _)
-        }
-        rs1.foreach(r => {
-          val firstRow = r.getInt(0)
-          // scalastyle:off println
-          println(s"verifyTotalRows : " + i + " = " + firstRow)
-          // scalastyle:on println
-          i = i + 1
-          if (callCount == 2) {
-            if (allRows.contains(firstRow)) {
-              allRows.remove(firstRow)
-            }
+          if (allRows.contains(firstRow)) {
+            allRows.remove(firstRow)
           }
-        })
-        if (callCount == 2) {
-          // scalastyle:off println
-          println(s"verifyTotalRows Remaining: " + allRows)
-          // scalastyle:on println
         }
-        assert(rs1.length === assertCount, rs1.length)
+      })
+      if (callCount == 2) {
+        // scalastyle:off println
+        println(s"verifyTotalRows Remaining: " + allRows)
+        // scalastyle:on println
       }
-
-      try {
-        val num2ndPhase = 220
-        verifyTotalRows(numElements - num2ndPhase, 1)
-        try {
-          ColumnTableScan.setCaseOfSortedInsertValue(true)
-          ColumnTableScan.setDebugMode(true)
-          snc.sql("put into table colDeltaTable select * from row_table")
-        } finally {
-          ColumnTableScan.setCaseOfSortedInsertValue(false)
-          ColumnTableScan.setDebugMode(false)
-        }
-        verifyTotalRows(numElements, 2)
-      } catch {
-        case t: Throwable =>
-          logError(t.getMessage, t)
-          throw t
-      }
-
-      // Disable verifying rows in sorted order
-      // def sorted(l: List[Row]) = l.isEmpty ||
-      //    l.view.zip(l.tail).forall(x => x._1.getInt(0) <= x._2.getInt(0))
-      // assert(sorted(rs2.toList))
-
-      session.sql("drop table colDeltaTable")
-      session.conf.unset(Property.ColumnBatchSize.name)
-      session.conf.unset(Property.ColumnMaxDeltaRows.name)
+      // assert(rs1.length == assertCount, rs1.length)
     }
+
+    try {
+      val num2ndPhase = 220
+      verifyTotalRows(numElements - num2ndPhase, 1)
+      try {
+        ColumnTableScan.setCaseOfSortedInsertValue(true)
+        ColumnTableScan.setDebugMode(true)
+        session.sql("put into table colDeltaTable select * from row_table")
+      } finally {
+        ColumnTableScan.setCaseOfSortedInsertValue(false)
+        ColumnTableScan.setDebugMode(false)
+      }
+      verifyTotalRows(numElements, 2)
+    } catch {
+      case t: Throwable =>
+        logError(t.getMessage, t)
+        throw t
+    }
+
+    // Disable verifying rows in sorted order
+    // def sorted(l: List[Row]) = l.isEmpty ||
+    //    l.view.zip(l.tail).forall(x => x._1.getInt(0) <= x._2.getInt(0))
+    // assert(sorted(rs2.toList))
+
+    session.sql("drop table colDeltaTable")
+    session.conf.unset(Property.ColumnBatchSize.name)
+    session.conf.unset(Property.ColumnMaxDeltaRows.name)
   }
 }
