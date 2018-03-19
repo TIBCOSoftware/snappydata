a021f6a69794b1f49c22f44cca5ed8c88be5f3f8
Rishitesh Mishra <rmishra@snappydata.io>
2018-03-19 11:23:02 +0530

    [SNAP-2225] Different results of nearly identical queries , due to join order (#971)
    Removed OrderlessHashpartition class.
    a) Handled bucket linking in TableExec and table scan. Removed the tableBucket param from partitioning class. If delink is enabled just keep numPartition as numBuckets.
    
    b) Also removed custom changes in HashPartition. We won't store bucket information in HashPartitioning. Instead based on the flag "linkPartitionToBucket" we can determine the number of partitions to be either numBuckets or num cores assigned to the executor.
    This will help in enabling delinkPartition in Smart connector mode as well. This will be tracked with a separate ticket.
    
    c) Fixed couple of issues with partition pruning.
    d) Fixed one issue with deleteFrom from IndextTest of the test suite.
    e) Removed ClusterSnappyJoinSuite, as both in core & cluster join order optimization should be applied.

---
+++ core/src/main/scala/org/apache/spark/sql/internal/JoinQueryPlanning.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2017 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+
+package org.apache.spark.sql.internal
+
+import scala.annotation.tailrec
+
+import org.apache.spark.sql.catalyst.expressions.{Alias, Expression, NamedExpression}
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+
+trait JoinQueryPlanning {
+
+  @tailrec final def unAlias(e: Expression): Expression = e match {
+    case a: Alias => unAlias(a.child)
+    case _ => e
+  }
+
+  def getKeyOrder(plan: LogicalPlan, joinKeys: Seq[Expression],
+      partitioning: Seq[NamedExpression]): (Seq[Int], Boolean) = {
+    val part = partitioning.map(unAlias)
+    lazy val planExpressions = plan.expressions
+    val keyOrder = joinKeys.map { k =>
+      val key = unAlias(k)
+      val i = part.indexWhere(_.semanticEquals(key))
+      if (i < 0) {
+        // search for any view aliases (SNAP-2204)
+        key match {
+          case ke: NamedExpression =>
+            planExpressions.collectFirst {
+              case a: Alias if ke.exprId == a.exprId => unAlias(a.child)
+              case e: NamedExpression if (ke ne e) && ke.exprId == e.exprId => e
+            } match {
+              case Some(e) =>
+                val j = part.indexWhere(_.semanticEquals(e))
+                if (j < 0) {
+                  Int.MaxValue
+                } else j
+              case None => Int.MaxValue
+            }
+          case _ => Int.MaxValue
+        }
+      } else i
+    }
+    (keyOrder, joinKeySubsetOfPart(keyOrder, partitioning))
+  }
+
+  private def joinKeySubsetOfPart(keyOrder: Seq[Int],
+      partitioning: Seq[NamedExpression]): Boolean = {
+    keyOrder.filterNot(i => i == Int.MaxValue).nonEmpty
+  }
+}
