2c5fe9d2f6e8c52270e458c1256f4b42c4c2d54e
supriya <spillai@snappydata.io>
2018-03-23 12:39:32 +0530

    - Added stop task for streaming app.

--- dtests/src/test/java/io/snappydata/hydra/cdcConnector/SnappyCDCPrms.java
+++ dtests/src/test/java/io/snappydata/hydra/cdcConnector/SnappyCDCPrms.java
@@ -4,17 +4,28 @@ import io.snappydata.hydra.cluster.SnappyPrms;
 
 public class SnappyCDCPrms extends SnappyPrms {
 
+  /**startRange is integer value used for giving
+   *  the starting range from where the ingestion
+   *  app should start ingestion
+   */
   public static Long startRange;
 
+  /**endRange is integer value used for giving
+   *  the end range at which the ingestion app will stop its ingestion
+   */
   public static Long endRange;
 
   public static Long dataLocation;
 
   public static Long isScanQuery;
 
+  public static Long isCDCStream;
+
+  public static Long isCDC;
+
   public static Long threadCnt;
 
-  public static Long queryFilePath;
+  public static Long appName;
 
   public static String getDataLocation(){
     String dataLoc = tasktab().stringAt(dataLocation, tab().stringAt
@@ -23,9 +34,26 @@ public class SnappyCDCPrms extends SnappyPrms {
     return dataLoc;
   }
 
+  public static String getAppName(){
+    String name = tasktab().stringAt(appName, tab().stringAt
+        (appName, null));
+    if (name == null) return "";
+    return name;
+  }
+
   public static boolean getIsScanQuery() {
     Long key = isScanQuery;
     return tasktab().booleanAt(key, tab().booleanAt(key, false));
+  }
+
+  public static boolean getIsCDC() {
+    Long key = isCDC;
+    return tasktab().booleanAt(key, tab().booleanAt(key, false));
+  }
+
+  public static boolean getIsCDCStream() {
+    Long key = isCDCStream;
+    return tasktab().booleanAt(key, false);
   }
 
   public static int getStartRange() {
