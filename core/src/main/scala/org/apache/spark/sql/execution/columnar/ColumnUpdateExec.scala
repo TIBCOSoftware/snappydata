f724b23502007c4a624e8603ae4c5c91f1b79b30
vivekwiz <vbhaskar@snappydata.io>
2018-03-28 13:03:32 +0530

    Temporary fix for insertingrows in sorted cahed batches which have no preceding rows in order in main table.

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/ColumnUpdateExec.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/ColumnUpdateExec.scala
@@ -238,7 +238,11 @@ case class ColumnUpdateExec(child: SparkPlan, columnTable: String,
            |  if ($ordinalIdVar < 0) {
            |    // +ordinal is to adjust all inserts in delta so far
            |    // +1 since ordinalIdVar is of the last position
-           |    updatedOrdinalIdVar = ~(~$ordinalIdVar + $ordinal + 1);
+           |    if (~$ordinalIdVar > 0) {
+           |      updatedOrdinalIdVar = ~(~$ordinalIdVar + $ordinal + 1);
+           |    } else {
+           |      updatedOrdinalIdVar = ~(~$ordinalIdVar + $ordinal);
+           |    }
            |  } else {
            |    updatedOrdinalIdVar = $ordinalIdVar;
            |  }
