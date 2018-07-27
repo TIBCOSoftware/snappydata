8040d4030b4dc0742e0abaac7c81f2687719e2fb
swati <ssawant@snappydata.io>
2018-07-27 15:59:43 +0530

    - Added a column with unique ids in order to perform updates on table - Added an empty column to be used for update - Removed commented queries from the list

--- dtests/src/resources/scripts/concurrency/airline/create_and_load_airline_table.sql
+++ dtests/src/resources/scripts/concurrency/airline/create_and_load_airline_table.sql
@@ -5,10 +5,11 @@ DROP TABLE IF EXISTS AIRLINE ;
 ----- CREATE TEMPORARY STAGING TABLE TO LOAD PARQUET FORMATTED DATA -----
 CREATE EXTERNAL TABLE STAGING_AIRLINE
     USING parquet OPTIONS(path 's3a://AKIAILHSQ3FINHV473RQ:Tnn6GOHhjaayIRtVNApYQgNLU3FxXkw9albr9hVJ@zeppelindemo/airlinedata_cleaned');
+    -- USING parquet OPTIONS(path '/home/swati/snappy-commons/examples/quickstart/data/airlineParquetData');
 
 ----- CREATE COLUMN TABLE -----
 CREATE TABLE AIRLINE USING column OPTIONS(PERSISTENT 'SYNCHRONOUS', BUCKETS '128', EVICTION_BY 'LRUHEAPPERCENT', overflow 'true')  AS (
-  SELECT Year_, Month_ , DayOfMonth, DepTime, CRSDepTime, ArrTime, UniqueCarrier, ArrDelay, DepDelay, Origin,
+  SELECT monotonically_increasing_id() as id, '' as dummy,  Year AS Year_, Month AS Month_ , DayOfMonth, DepTime, CRSDepTime, ArrTime, UniqueCarrier, ArrDelay, DepDelay, Origin,
     Dest, Distance, TaxiIn, TaxiOut, Cancelled, CarrierDelay, WeatherDelay
     FROM STAGING_AIRLINE);
 
@@ -19,6 +20,7 @@ DROP TABLE IF EXISTS AIRLINEREF;
 ----- CREATE TEMPORARY STAGING TABLE TO LOAD PARQUET FORMATTED DATA -----
 CREATE EXTERNAL TABLE STAGING_AIRLINEREF
     USING parquet OPTIONS(path 's3a://AKIAILHSQ3FINHV473RQ:Tnn6GOHhjaayIRtVNApYQgNLU3FxXkw9albr9hVJ@zeppelindemo/airportdata');
+    --USING parquet OPTIONS(path '/home/swati/snappy-commons/examples/quickstart/data/airportcodeParquetData');
 
 ----- CREATE ROW TABLE -----
 
