c370c3333a9e861b4fca5bc7645f348f2abb066e
ahshahid <ashahid@snappydata.io>
2018-07-27 17:59:57 -0700

    added test

--- cluster/src/test/scala/org/apache/spark/sql/policy/PolicyJdbcClientTest.scala
+++ cluster/src/test/scala/org/apache/spark/sql/policy/PolicyJdbcClientTest.scala
@@ -127,6 +127,70 @@ class PolicyJdbcClientTest extends SnappyFunSuite
     this.testMultiplePolicyCreationWithQuery(rowTableName)
   }
 
+  test("test policy recreation on column table ENT:38") {
+    this.testPolicyRecreation(colTableName)
+  }
+
+  test("test policy recreation on row table ENT:38") {
+    this.testPolicyRecreation(colTableName)
+  }
+
+  def testPolicyRecreation(tableName: String): Unit = {
+    val conn = getConnection(Some(tableOwner))
+    val stmt = conn.createStatement()
+    val conn1 = getConnection(Some("UserX"))
+    val stmt1 = conn1.createStatement()
+    try {
+
+      var rs = stmt.executeQuery(s"select * from $tableName where id > 25 or id < 10 ")
+      var rsSize = 0
+      while (rs.next()) rsSize += 1
+      assertEquals(numElements - 1 - 25 + 10, rsSize)
+
+      rsSize = 0
+      rs = stmt1.executeQuery(s"select * from $tableName where id > 25 or id < 10 ")
+      while (rs.next()) rsSize += 1
+      assertEquals(numElements - 1 - 25 + 10, rsSize)
+
+
+      stmt.execute(s"create policy testPolicy1 on  " +
+          s"$tableName for select to current_user using id > 10")
+
+      rs = stmt.executeQuery(s"select * from $tableName where id > 25 or id < 10 ")
+      rsSize = 0
+      while (rs.next()) rsSize += 1
+      assertEquals(numElements - 1 - 25 + 10, rsSize)
+      rsSize = 0
+
+      rs = stmt1.executeQuery(s"select * from $tableName where id > 25 or id < 10 ")
+      while (rs.next()) rsSize += 1
+      assertEquals(numElements - 1 - 25, rsSize)
+
+      stmt.execute(s"alter table $tableName disable row level security")
+      stmt.execute("drop policy testPolicy1")
+      rsSize = 0
+      rs = stmt1.executeQuery(s"select * from $tableName where id > 25 or id < 10 ")
+      while (rs.next()) rsSize += 1
+      assertEquals(numElements - 1 - 25 + 10, rsSize)
+
+      stmt.execute(s"create policy testPolicy1 on  " +
+          s"$tableName for select to current_user using id > 10")
+
+      stmt.execute(s"alter table $tableName enable row level security")
+      rsSize = 0
+      rs = stmt1.executeQuery(s"select * from $tableName where id > 25 or id < 10 ")
+      while (rs.next()) rsSize += 1
+      assertEquals(numElements - 1 - 25, rsSize)
+
+      stmt.execute("drop policy testPolicy1")
+
+    } finally {
+      conn.close()
+      conn1.close()
+    }
+  }
+
+
   def testMultiplePolicyCreationWithQuery(tableName: String): Unit = {
     val conn = getConnection(Some(tableOwner))
     val stmt = conn.createStatement()
