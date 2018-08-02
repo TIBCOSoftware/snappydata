25e75b068dca99b60275282bd734a1340558de7b
swati <ssawant@snappydata.io>
2018-08-02 11:27:17 +0530

    - Changes in script to read data from s3 and load into extenal table

--- dtests/src/resources/scripts/concurrency/airline/create_and_load_airline_table.sql
+++ dtests/src/resources/scripts/concurrency/airline/create_and_load_airline_table.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS AIRLINE ;
 
 ----- CREATE TEMPORARY STAGING TABLE TO LOAD PARQUET FORMATTED DATA -----
 CREATE EXTERNAL TABLE STAGING_AIRLINE
-    USING parquet OPTIONS(path '/export/shared/QA_DATA/airlinedata_cleaned');
-    -- USING parquet OPTIONS(path 's3a://AKIAILHSQ3FINHV473RQ:Tnn6GOHhjaayIRtVNApYQgNLU3FxXkw9albr9hVJ@zeppelindemo/airlinedata_cleaned');
+     USING parquet OPTIONS(path 's3a://AKIAILHSQ3FINHV473RQ:Tnn6GOHhjaayIRtVNApYQgNLU3FxXkw9albr9hVJ@zeppelindemo/airlinedata_cleaned');
+     -- USING parquet OPTIONS(path '/export/shared/QA_DATA/airlinedata_cleaned');
 
 ----- CREATE COLUMN TABLE -----
-CREATE TABLE AIRLINE USING column OPTIONS(partition_by 'UniqueCarrier,Year_', PERSISTENT 'SYNCHRONOUS', BUCKETS '128', EVICTION_BY 'LRUHEAPPERCENT', overflow 'true', key_columns 'id')  AS (
+CREATE TABLE AIRLINE USING column OPTIONS(partition_by 'UniqueCarrier,Year_', PERSISTENT 'SYNCHRONOUS', BUCKETS '384', EVICTION_BY 'LRUHEAPPERCENT', overflow 'true', key_columns 'id')  AS (
   SELECT cast(monotonically_increasing_id() as string) as id, '' as dummy, Year_, Month_ , DayOfMonth, DepTime, CRSDepTime, ArrTime, UniqueCarrier, ArrDelay, DepDelay, Origin,
     Dest, Distance, TaxiIn, TaxiOut, Cancelled, CarrierDelay, WeatherDelay
     FROM STAGING_AIRLINE);
@@ -19,8 +19,8 @@ DROP TABLE IF EXISTS AIRLINEREF;
 
 ----- CREATE TEMPORARY STAGING TABLE TO LOAD PARQUET FORMATTED DATA -----
 CREATE EXTERNAL TABLE STAGING_AIRLINEREF
-    USING parquet OPTIONS(path '/export/shared/QA_DATA/airportdata');
-    -- USING parquet OPTIONS(path 's3a://AKIAILHSQ3FINHV473RQ:Tnn6GOHhjaayIRtVNApYQgNLU3FxXkw9albr9hVJ@zeppelindemo/airportdata');
+     USING parquet OPTIONS(path 's3a://AKIAILHSQ3FINHV473RQ:Tnn6GOHhjaayIRtVNApYQgNLU3FxXkw9albr9hVJ@zeppelindemo/airportdata');
+     -- USING parquet OPTIONS(path '/export/shared/QA_DATA/airportdata');
 
 ----- CREATE ROW TABLE -----
 
