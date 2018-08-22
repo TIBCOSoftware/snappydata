4c0a65940846d148d1ca8d33f44502dab9ffbc08
rmishra <rmishra@snappydata.io>
2018-08-22 16:44:07 +0530

    [SNAP-2483]Wrapped replciated joins in DelegateRDD

--- core/src/main/scala/org/apache/spark/sql/execution/joins/HashJoinExec.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/joins/HashJoinExec.scala
@@ -208,7 +208,8 @@ case class HashJoinExec(leftKeys: Seq[Expression],
           streamRDDs.tail.map(rdd => new DelegateRDD[InternalRow](
             rdd.sparkContext, rdd, buildRDDs, preferredLocations))
       } else {
-        streamRDDs
+        Seq(new DelegateRDD[InternalRow](streamRDD.sparkContext, streamRDD, buildRDDs,
+          preferredLocations, Seq.empty[Dependency[_]]))
       }
       (streamPlanRDDs, buildRDDs)
     } else {
