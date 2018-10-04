9bad11e2d3a680a065806475fce297be8ca5abe8
Trilok Khairnar <tkhairnar@snappydata.io>
2018-10-04 16:55:30 +0530

    SNAP-2596: Automate data loading into MemSQL tables on table creation for TPC-H

--- cluster/src/test/scala/io/snappydata/benchmark/memsql/TPCH_Memsql_Tables.scala
+++ cluster/src/test/scala/io/snappydata/benchmark/memsql/TPCH_Memsql_Tables.scala
@@ -26,14 +26,20 @@ object TPCH_Memsql_Tables {
 
      val host = args(0)
      val port = args(1)
+     val dataDirectory = args(2)
+     val numberOfDataLoadingStages : String = args(3)
+
      val dbName = "TPCH"
      val user = "root"
      val password = ""
 
-     Class.forName("com.mysql.jdbc.Driver")
+     //Class.forName("com.mysql.jdbc.Driver")
+     Class.forName("com.mysql.cj.jdbc.Driver")
      val dbAddress = "jdbc:mysql://" + host + ":" + port + "/"
      val conn = DriverManager.getConnection(dbAddress, user, password)
      val stmt = conn.createStatement
+
+     // Create TPC-H database and tables
 
      stmt.execute("DROP DATABASE IF EXISTS " + dbName)
      stmt.execute("CREATE DATABASE IF NOT EXISTS " + dbName)
@@ -56,13 +62,30 @@ object TPCH_Memsql_Tables {
      TPCHColumnPartitionedTable.createLineItemTable_Memsql(stmt)
 
      var rs = stmt.executeQuery("SHOW TABLES")
-     println("Tables" + rs)
+     println(System.lineSeparator() + "List of tables in TPC-H database:")
      while (rs.next()) {
        System.out.println(rs.getString(1));
      }
 
+     // Load data into TPC-H tables
+
+     val tableNames = List("REGION", "NATION", "SUPPLIER", "PART", "PARTSUPP", "CUSTOMER", "ORDERS", "LINEITEM")
+     for(table <- tableNames){
+       val stages : Int = numberOfDataLoadingStages.toInt
+
+       if(stages == 1){
+         println(s"Loading data from '${dataDirectory}/${table.toLowerCase}.tbl' into table ${table}");
+         stmt.execute(s"LOAD DATA INFILE '${dataDirectory}/${table.toLowerCase}.tbl' INTO TABLE ${table} COLUMNS TERMINATED BY '|' LINES TERMINATED BY '|\n' ");
+       }
+       else{
+         for(stage <- 1 to numberOfDataLoadingStages.toInt){
+           println(s"Loading data from '${dataDirectory}/${table.toLowerCase}.tbl' into table ${table}");
+           stmt.execute(s"LOAD DATA INFILE '${dataDirectory}/${table.toLowerCase}.tbl.${stage}' INTO TABLE ${table} COLUMNS TERMINATED BY '|' LINES TERMINATED BY '|\n' ");
+         }
+       }
+       println(s"Finished loading data in ${table}")
+     }
+
      stmt.close();
-
-
    }
  }
