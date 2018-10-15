e515ad5c3b0b62819592de0a5c942ffac903c6d4
suranjan kumar <skumar@snappydata.io>
2018-10-15 18:43:05 +0530

    Make condition for replicatedTableJoin stricter
    - HashJoinExec has a boolean replicatedTableJoin which hints
    it to do join using single partition as it would be local join
    and build side will be reding from a single partition.
    To set this variable, we used to check if all children are
    non partitioned recursively
    There could be cases where result of NLJ was partitioned and this
    caused only single partition of that NLJ result to be consumed by
    parent HashJoin.

--- core/src/main/scala/org/apache/spark/sql/execution/joins/HashJoinExec.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/joins/HashJoinExec.scala
@@ -110,8 +110,7 @@ case class HashJoinExec(leftKeys: Seq[Expression],
 
   override def requiredChildDistribution: Seq[Distribution] =
     if (replicatedTableJoin) {
-      AllTuples :: AllTuples :: Nil	
-      //UnspecifiedDistribution :: UnspecifiedDistribution :: Nil
+      UnspecifiedDistribution :: UnspecifiedDistribution :: Nil
     } else {
       // if left or right side is already distributed on a subset of keys
       // then use the same partitioning (for the larger side to reduce exchange)
