b732e5d79387ba7b62d8c6d572cb8f35297840b3
Sumedh Wale <swale@snappydata.io>
2018-12-22 14:37:46 +0530

    [SNAP-2790] check valid key_columns in table create
    also fail if key_columns is specified for a row table

--- core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/DefaultSource.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/columnar/impl/DefaultSource.scala
@@ -116,7 +116,6 @@ final class DefaultSource extends ExternalSchemaRelationProvider with SchemaRela
 
     val partitions = ExternalStoreUtils.getAndSetTotalPartitions(session, parameters,
       forManagedTable = true)
-    val partitioningColumns = StoreUtils.getAndSetPartitioningAndKeyColumns(session, parameters)
 
     val tableOptions = new CaseInsensitiveMap(parameters.toMap)
     val parametersForShadowTable = new CaseInsensitiveMutableHashMap(parameters)
@@ -134,6 +133,8 @@ final class DefaultSource extends ExternalSchemaRelationProvider with SchemaRela
         } else field
       })
     }
+    val partitioningColumns = StoreUtils.getAndSetPartitioningAndKeyColumns(session,
+      schema, parameters)
 
     val ddlExtension = StoreUtils.ddlExtensionString(parameters,
       isRowTable = false, isShadowTable = false)
