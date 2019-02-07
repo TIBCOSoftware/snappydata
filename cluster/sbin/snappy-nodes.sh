65985786f95b1a3e669838611fe2cfea67ea443b
Neeraj Kumar <kneeraj@snappydata.io>
2019-02-07 16:59:44 +0530

    Removed an echo statement

--- cluster/sbin/snappy-nodes.sh
+++ cluster/sbin/snappy-nodes.sh
@@ -264,7 +264,6 @@ function execute() {
       fi
     fi
     launchcommand="${@// /\\ } ${args} ${postArgs} < /dev/null 2>&1"
-    echo "LAUNCH_COMMAND = $launchcommand"
     eval $launchcommand &
     LAST_PID="$!"
   fi
