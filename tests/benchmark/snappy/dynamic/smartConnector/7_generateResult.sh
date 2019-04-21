85aa8615d0222480c3772a719cc29ff727b31e3e
Trilok Khairnar <tkhairnar@snappydata.io>
2019-04-21 22:53:19 +0530

    SNAP-2986 adding result generation script for smart connector mode

---
+++ tests/benchmark/snappy/dynamic/smartConnector/7_generateResult.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+source PerfRun.conf
+
+directory=$outputLocation/GeneratedResults/$(date "+%Y.%m.%d-%H.%M.%S")_$dataSize
+mkdir -p $directory
+
+cp PerfRun.conf $directory/
+cp $SPARK_HOME/conf/spark-env.sh $SPARK_HOME/conf/spark-default.conf $directory/
+mv *.csv *.out $directory/
+
+
+latestProp=$directory/latestProp.props
+
+cd $SNAPPY_HOME
+cd ../../../..
+echo snappyData = $(git rev-parse HEAD)_$(git log -1 --format=%cd) > $latestProp
+cd spark
+echo snappy-spark = $(git rev-parse HEAD)_$(git log -1 --format=%cd) >> $latestProp
+
+
+echo "******************Performance Result Generated*****************"
+
