c3ee3b0262c89808f5fe43f28a2e712137c1a490
Trilok Khairnar <tkhairnar@snappydata.io>
2019-04-22 11:29:22 +0000

    SNAP-2986: Minor path corrections in results generation for smart connector

--- tests/benchmark/snappy/dynamic/smartConnector/7_generateResult.sh
+++ tests/benchmark/snappy/dynamic/smartConnector/7_generateResult.sh
@@ -5,14 +5,14 @@ directory=$outputLocation/GeneratedResults/$(date "+%Y.%m.%d-%H.%M.%S")_$dataSiz
 mkdir -p $directory
 
 cp PerfRun.conf $directory/
-cp $SPARK_HOME/conf/spark-env.sh $SPARK_HOME/conf/spark-default.conf $directory/
+cp $SPARK_HOME/conf/spark-env.sh $SPARK_HOME/conf/spark-defaults.conf $directory/
 mv *.csv *.out $directory/
 
 
 latestProp=$directory/latestProp.props
 
 cd $SNAPPY_HOME
-cd ../../../..
+cd ../../../
 echo snappyData = $(git rev-parse HEAD)_$(git log -1 --format=%cd) > $latestProp
 cd spark
 echo snappy-spark = $(git rev-parse HEAD)_$(git log -1 --format=%cd) >> $latestProp
