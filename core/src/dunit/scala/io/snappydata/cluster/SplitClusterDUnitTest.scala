d7ff0d07a53582a6ef6228a65c3d0d559bfc6d3b
Sumedh Wale <swale@snappydata.io>
2019-04-24 03:15:55 +0530

    fix jdbc jar name in tests and docs

--- core/src/dunit/scala/io/snappydata/cluster/SplitClusterDUnitTest.scala
+++ core/src/dunit/scala/io/snappydata/cluster/SplitClusterDUnitTest.scala
@@ -739,7 +739,7 @@ object SplitClusterDUnitTest extends SplitClusterDUnitTestObject {
     try {
       // perform some operations through spark-shell using JDBC pool driver API on current Spark
       val jars = Files.newDirectoryStream(Paths.get(s"$productDir/../distributions/"),
-        "snappydata-jdbc_*.jar")
+        "TIB_compute-jdbc_*.jar")
       var securityConf = ""
       if (props.containsKey(Attribute.USERNAME_ATTR)) {
         securityConf = s" --conf spark.snappydata.user=" +
