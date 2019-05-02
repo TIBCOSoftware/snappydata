ccefec76aba88d83a9b69ed0d06659ca959cb239
Trilok Khairnar <tkhairnar@snappydata.io>
2019-05-03 00:40:52 +0530

    SNAP-2986 : echo the new additional args too in smart connector test

--- tests/benchmark/snappy/dynamic/smartConnector/5_createAndLoadTable.sh
+++ tests/benchmark/snappy/dynamic/smartConnector/5_createAndLoadTable.sh
@@ -5,9 +5,9 @@ source PerfRun.conf
 echo "bash $SPARK_HOME/bin/spark-submit --master spark://$master:7077 \
 --conf spark.snappydata.connection=$locator:1527 \
 $sparkProperties --class io.snappydata.benchmark.snappy.tpch.TableCreationSmartConnector \
-$TPCHJar $dataDir $NumberOfLoadStages $Parquet \
+$TPCHJar $dataDir $NumberOfLoadStages $Parquet $createParquet \
 $buckets_Order_Lineitem $buckets_Cust_Part_PartSupp $IsSupplierColumnTable \
-$buckets_Supplier $Redundancy $Persistence $Persistence_Type $threadNumber"
+$buckets_Supplier $Redundancy $Persistence $Persistence_Type $traceEvents $threadNumber"
 
 #Execute Spark App
 bash $SPARK_HOME/bin/spark-submit --master spark://$master:7077 \
