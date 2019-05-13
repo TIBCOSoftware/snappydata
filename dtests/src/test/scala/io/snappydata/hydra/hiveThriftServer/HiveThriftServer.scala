179112b708fc3853ceb77dbd37731b4c9a17800f
Chandresh <cbhatt@snappydata.io>
2019-05-13 19:21:17 +0530

    Added the test case for commands show databases,show schemas,show tables,drop table,create table for Hive Thrift Server

--- dtests/src/test/scala/io/snappydata/hydra/hiveThriftServer/HiveThriftServer.scala
+++ dtests/src/test/scala/io/snappydata/hydra/hiveThriftServer/HiveThriftServer.scala
@@ -19,30 +19,83 @@ package io.snappydata.hydra.hiveThriftServer
 import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
 import com.typesafe.config.Config
-import org.apache.spark.sql.{SnappyJobValid, SnappyJobValidation, SnappySQLJob, SnappySession}
+import org.apache.spark.sql._
 
 class HiveThriftServer extends SnappySQLJob {
+
+  var connection : Connection = null
+  var result : String = ""
+  var rs : ResultSet = null
+  var stmt : Statement = null
+
   override def isValidJob(sc: SnappySession, config: Config): SnappyJobValidation = SnappyJobValid()
 
-  override def runSnappyJob(sc: SnappySession, jobConfig: Config): Any = {
+  override def runSnappyJob(snappySession: SnappySession, jobConfig: Config): Any = {
 
     // scalastyle:off println
+    val snc : SnappyContext = snappySession.sqlContext
     println("Starting the Hive Thrift Server testing job.....")
-    val connection : Connection = DriverManager.getConnection(
-      "jdbc:hive2://localhost:10000", "app", "app")
-    val stmt : Statement = connection.createStatement()
-    val rs : ResultSet = stmt.executeQuery("show databases")
-    var result : String = ""
-    while(rs.next) {
-      result = result + rs.getString(1) + "\n"
-      result = result + rs.getString(2) + "\n"
-      result = result + rs.getString(3)
+    val hiveThriftServer = new HiveThriftServer
+    connectToBeeline(hiveThriftServer)
+    executeShowSchemasAndValidate(hiveThriftServer, "ShoW DaTAbasEs")
+    executeShowSchemasAndValidate(hiveThriftServer, "sHOw SCHemas")
+    executeCreateTable(hiveThriftServer, "create TablE if not exists " +
+      "Emp(id int, name String)")
+    executeShowTables(hiveThriftServer, "show TabLES in default")
+    executeDropTables(hiveThriftServer, "drop table if exists Emp")
+    executeShowTables(hiveThriftServer, "ShoW tAbLES iN DEFAULT")
+    disconnectToBeeline(hiveThriftServer)
+    println("Finished the Hive Thrift Server testing.....")
+  }
+
+  def connectToBeeline(hts : HiveThriftServer) : Unit = {
+    hts.connection = DriverManager
+      .getConnection("jdbc:hive2://localhost:10000", "app", "app")
+  }
+
+  def disconnectToBeeline(hts : HiveThriftServer) : Unit = {
+      hts.connection.close()
+      if(hts.connection.isClosed()) {
+      println("Connection with Beeline client closed....")
+    }
+  }
+
+  def executeShowSchemasAndValidate(hts : HiveThriftServer, command : String) : Unit = {
+    result = ""
+    hts.stmt = hts.connection.createStatement()
+    hts.rs = hts.stmt.executeQuery(command)
+    while(hts.rs.next) {
+      result = result + hts.rs.getString("databaseName") + "\n"
     }
 
-    println("show databases : " + result)
+    if(command == "ShoW DaTAbasEs") {
+      println("show databases :" + "\n" + result)
+    }
 
+    if(command == "sHOw SCHemas") {
+      println("show schemas :" + "\n" + result)
+    }
+  }
 
-    println("Finished the Hive Thrift Server testing.....")
+  def executeCreateTable(hts : HiveThriftServer, command : String) : Unit = {
+    hts.stmt = hts.connection.createStatement()
+    hts.stmt.executeQuery(command)
+  }
 
+  def executeShowTables(hts : HiveThriftServer, command : String) : Unit = {
+    result = ""
+    println("Command :" + command)
+    hts.stmt = hts.connection.createStatement()
+    hts.rs = hts.stmt.executeQuery(command)
+    while(hts.rs.next()) {
+        result = hts.rs.getString("schemaName") + " " +
+        hts.rs.getString("tableName") + " "  + hts.rs.getString("isTemporary") + "\n"
+    }
+    println("show tables : " + result)
+  }
+
+  def executeDropTables(hts : HiveThriftServer, command : String) : Unit = {
+    hts.stmt = hts.connection.createStatement()
+    hts.stmt.executeQuery(command)
   }
 }
