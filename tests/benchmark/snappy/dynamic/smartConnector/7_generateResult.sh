9901b64c508a41f9fefb45cea3781e1dd84a8202
Trilok Khairnar <tkhairnar@snappydata.io>
2019-05-13 11:51:05 +0000

    SNAP-2986: include SnappyData conf files in results generation

--- tests/benchmark/snappy/dynamic/smartConnector/7_generateResult.sh
+++ tests/benchmark/snappy/dynamic/smartConnector/7_generateResult.sh
@@ -3,11 +3,14 @@ source PerfRun.conf
 
 directory=$outputLocation/GeneratedResults/$(date "+%Y.%m.%d-%H.%M.%S")_$dataSize
 mkdir -p $directory
+mkdir -p $directory/conf/
 
 cp PerfRun.conf $directory/
 cp $SPARK_HOME/conf/spark-env.sh $SPARK_HOME/conf/spark-defaults.conf $directory/
 mv *.csv *.out $directory/
 
+cd $SnappyData/conf
+cp leads locators servers *.properties $directory/conf/
 
 latestProp=$directory/latestProp.props
 
