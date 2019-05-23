a46c0178dfc1a8dff35f20b81db24d7c4aa12d4e
spillai <spillai@snappydata.io>
2019-05-23 00:09:02 -0700

    - Merged RLS changes in this branch.

--- dtests/src/test/scala/io/snappydata/hydra/security/SecurityTestUtil.scala
+++ dtests/src/test/scala/io/snappydata/hydra/security/SecurityTestUtil.scala
@@ -19,7 +19,11 @@ package io.snappydata.hydra.security
 
 import java.io.{PrintWriter}
 
+import scala.collection.immutable.HashMap
+
 import io.snappydata.hydra.northwind.NWQueries
+
+import org.apache.spark.sql.{DataFrame, SnappyContext}
 
 import org.apache.spark.sql.SnappyContext
 
@@ -27,6 +31,10 @@ object SecurityTestUtil {
   // scalastyle:off println
   var expectedExpCnt = 0;
   var unExpectedExpCnt = 0;
+
+  var policyUserMap: HashMap[String, String] = HashMap()
+  var policySelectQueryMap: HashMap[Map[String, DataFrame], String] = HashMap()
+  var policyFullSelectQueryMap: HashMap[Map[String, DataFrame], String] = HashMap()
 
   def createColRowTables(snc: SnappyContext) : Unit = {
     println("Inside createColAndRow tables")
@@ -61,6 +69,157 @@ object SecurityTestUtil {
         " USING column options (PARTITION_BY 'SupplierID', buckets '123', redundancy '1')")
     NWQueries.suppliers(snc).write.insertInto("suppliers")
     println("Finished  createColAndRow tables")
+  }
+
+  def createPolicy(snc: SnappyContext, userSchema: Array[String],
+      userName: Array[String]): Unit = {
+    println("Inside createPolicy() ")
+    var filterCond, orderBy = ""
+    var cnt = 1
+    try {
+      for (i <- 0 to userName.length - 1) {
+        val policyUser = userName(i)
+        for (s <- 0 to userSchema.length - 1) {
+          if (userSchema(s).contains("employees")) {
+            filterCond = "EMPLOYEEID = 1 AND COUNTRY = 'USA'"
+            orderBy = " ORDER BY EMPLOYEEID asc "
+          }
+          else {
+            filterCond = "CATEGORYID = 4"
+            orderBy = " ORDER BY CATEGORYID asc "
+          }
+
+          val grantSql = " GRANT SELECT on " + userSchema(s) + " TO " + userName(i)
+          println("GRANT sql statement is " + grantSql)
+          snc.sql(grantSql)
+
+          var policyName = "p" + 0;
+          while (policyUserMap.contains(policyName)) {
+            println("checking for policy name " + policyName);
+            policyName = "p" + cnt
+            println("The next policy name is " + policyName)
+            cnt = cnt + 1
+          }
+          var queryMap1: Map[String, DataFrame] = Map()
+          var queryMap2: Map[String, DataFrame] = Map()
+          val policyStr = "CREATE POLICY " + policyName + " ON " + userSchema(s) +
+              " FOR SELECT TO " + policyUser + " USING " + filterCond
+          println("The create policy string is " + policyStr)
+          val selectQryWF = "SELECT * FROM " + userSchema(s) + " WHERE " + filterCond;
+          println("Select Query with filter is " + selectQryWF)
+          val selectQry = "SELECT * FROM " + userSchema(s) + orderBy;
+          println("Select Query is " + selectQry)
+          println("Policy created for " + policyUser + " on table " +
+              userSchema(s) + " is " + policyStr)
+          snc.sql(policyStr)
+          val result1 = snc.sql(selectQry)
+          val result2 = snc.sql(selectQryWF)
+
+          queryMap1 += (selectQry -> result1)
+          queryMap2 += (selectQryWF -> result2)
+          policySelectQueryMap += (queryMap2 -> policyUser)
+          policyFullSelectQueryMap += (queryMap1 -> policyUser)
+          policyUserMap += (policyName -> userSchema(s))
+        }
+      }
+    }
+    catch {
+      case ex: Exception => {
+        println("Caught exception in createPolicy " + ex.printStackTrace())
+      }
+    }
+    println("Successfully completed createPolicy task")
+  }
+
+  def dropPolicy(snc: SnappyContext): Unit = {
+    println("Inside dropPolicy()")
+    try {
+      val policyMap = policyUserMap
+      for ((k, v) <- policyMap) {
+        val policyNm = k
+        val schemaOwner = v
+        val dropPolicyStr = " DROP POLICY " + policyNm
+        println("Dropping policy p " + policyNm + " with schemaOwner " + schemaOwner)
+        snc.sql(dropPolicyStr)
+      }
+    }
+    catch {
+      case ex: Exception => {
+        println("Caught exception in dropPolicy() " + ex.printStackTrace())
+      }
+    }
+  }
+
+  def enableRLS(snc: SnappyContext, userSchema: Array[String]): Unit = {
+    println("Inside enableRLS()")
+    val isAltrTableRLS = true
+    var alterTabSql = ""
+    try {
+      for (s <- 0 to userSchema.length - 1) {
+        if (isAltrTableRLS) {
+          alterTabSql = "ALTER TABLE " + userSchema(s) + " ENABLE ROW LEVEL SECURITY";
+          println("The alter table sql is " + alterTabSql);
+        } else {
+          alterTabSql = "ALTER TABLE " + userSchema(s) + " DISABLE ROW LEVEL SECURITY";
+          println("The alter table sql is " + alterTabSql);
+        }
+        snc.sql(alterTabSql)
+      }
+    }
+    catch {
+      case ex: Exception => {
+        println("Caught exception in enableRLS " + ex.printStackTrace())
+      }
+    }
+    println("Successfully completed enableRLS task")
+  }
+
+  def validateRLS(snc: SnappyContext, isDropPolicy: Boolean, isAltrTableRLS:
+  Boolean = true): Unit = {
+    println("Inside validateRLS")
+    var queryUserMap: HashMap[Map[String, DataFrame], String] = HashMap()
+    if (isDropPolicy || !isAltrTableRLS) {
+      queryUserMap = policyFullSelectQueryMap; // this map contains rs fro select * from table query
+      println("Using policyFullSelectQueryMap");
+    } else {
+      queryUserMap = policySelectQueryMap; // this map contains rs fro
+      // select * from table with condition query
+      println("Using policySelectQueryMap");
+    }
+    try
+        for ((k, v) <- queryUserMap) {
+          val queryMap = k
+          val policyUser = v
+          println("The user is " + policyUser);
+          for ((k1, v1) <- queryMap) {
+            val selectQry = k1
+            val finalSelectQ: Array[String] = selectQry.split("WHERE")
+            val prevDF = v1
+            println("The select Query is " + selectQry);
+            println("The Final select Query tobe executed by policy User is  " + finalSelectQ(0));
+            val currDF = snc.sql(finalSelectQ(0))
+            val currFiltrDF = snc.sql(selectQry)
+            val currDFSize = currDF.count()
+            val prevDFSize = prevDF.count()
+            val currFiltrDFSize = currFiltrDF.count()
+            println("Current DF size = " + currDFSize + " Previous DF size = " + prevDFSize +
+                " Current DF with filter size = " + currFiltrDFSize)
+            if ((currDFSize == prevDFSize) && (currFiltrDFSize == prevDFSize)) {
+              println(" The number of rows returned are equal ")
+            }
+            else {
+              println(" The number of rows returned donot match Current DF size = " + currDFSize
+                  + " Previous DF size = " + prevDFSize + " and CurrFiltrDF Size = " +
+                  currFiltrDFSize)
+            }
+          }
+        }
+
+    catch {
+      case ex: Exception => {
+        println("Caught exception in validateRLS " + ex.printStackTrace())
+      }
+    }
   }
 
   def runQueries(snc: SnappyContext, queryArray: Array[String], expectExcpCnt: Integer,
