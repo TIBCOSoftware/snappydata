951d151a088d7307ddbb223d74f7dd8c0725dab6
Vatsal Mevada <vmevada@snappydata.io>
2019-06-26 16:09:44 +0530

    Considering jobserver class loader as a key for generated code cache - For each submission of a snappy-job, a new URI class loader is used. The first run of a snappy-job may generate some code and it will cached. The subsequent run of the snappy job will end up using the generated code which was cached by the first run of the job. This can lead to issues as the class loader used for the cached code is the one from the first job submission and subsequent submissions will be using a different class loader. This change is done to avoid such failures.

--- cluster/src/main/scala/org/apache/spark/sql/SnappySessionFactory.scala
+++ cluster/src/main/scala/org/apache/spark/sql/SnappySessionFactory.scala
@@ -24,6 +24,8 @@ import io.snappydata.impl.LeadImpl
 import spark.jobserver.context.SparkContextFactory
 import spark.jobserver.util.ContextURLClassLoader
 import spark.jobserver.{ContextLike, SparkJobBase, SparkJobInvalid, SparkJobValid, SparkJobValidation}
+
+import org.apache.spark.sql.catalyst.expressions.codegen.CodeGenerator
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.util.SnappyUtils
 
@@ -101,6 +103,7 @@ object SnappySessionFactory {
             cl.addURL(u)
           })
         }
+        CodeGenerator.jobClassLoader.set(cl)
         cl
       }
     }
