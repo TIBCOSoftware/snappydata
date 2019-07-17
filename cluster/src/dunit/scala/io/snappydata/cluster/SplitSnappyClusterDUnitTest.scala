4eabe7cada58972b39d42b749c88dfdfa4d74405
vatsal mevada <vmevada@tibco.com>
2019-07-17 11:04:52 +0000

    [SNAP-3023] Handle CatalogStaleException with retries in snappy sink (#1354)
    Retrying streaming batch processing for the snappy sink when
    `CatalogStaleException` is encountered.  
    Currently, the number of attempts is set to 10 with backoff interval of
    `100 * attempt_number` millis.  
    Also exposed an internal property (`internal___attempts`) to overwrite this for
    tests.

--- cluster/src/dunit/scala/io/snappydata/cluster/SplitSnappyClusterDUnitTest.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/SplitSnappyClusterDUnitTest.scala
@@ -16,15 +16,19 @@
  */
 package io.snappydata.cluster
 
+import java.io.{File, PrintWriter}
 import java.net.InetAddress
+import java.nio.file.{Files, Paths}
 import java.util.Properties
 
+import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
+import scala.reflect.io.Path
+import scala.util.Try
 
 import com.gemstone.gemfire.internal.cache.PartitionedRegion
 import com.pivotal.gemfirexd.internal.engine.Misc
-import io.snappydata.cluster.SplitSnappyClusterDUnitTest.sc
 import io.snappydata.core.{TestData, TestData2}
 import io.snappydata.test.dunit.{AvailablePortHelper, SerializableRunnable}
 import io.snappydata.util.TestUtils
@@ -33,12 +37,23 @@ import org.junit.Assert
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.execution.CatalogStaleException
 import org.apache.spark.sql.execution.columnar.impl.ColumnFormatRelation
+import org.apache.spark.sql.kafka010.KafkaTestUtils
 import org.apache.spark.sql.store.{SnappyJoinSuite, StoreUtils}
-import org.apache.spark.sql.types.{DateType, StringType, StructField, StructType}
+import org.apache.spark.sql.streaming.{ProcessingTime, SnappyStoreSinkProvider}
 import org.apache.spark.sql.udf.UserDefinedFunctionsDUnitTest
 import org.apache.spark.{Logging, SparkConf, SparkContext}
+import io.snappydata.cluster.SplitSnappyClusterDUnitTest.sc
+import io.snappydata.test.dunit.DistributedTestBase.WaitCriterion
+import org.apache.commons.io.FileUtils
+
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.kafka010.KafkaTestUtils
+import org.apache.spark.sql.streaming.ProcessingTime
+import org.apache.spark.sql.streaming.SnappySinkProviderDUnitTest.{TestSinkCallback, checkpointDirectory, kafkaTestUtils, ldapGroup, snc, tableName, waitTillTheBatchIsPickedForProcessing}
+import org.apache.spark.sql.types.{DateType, IntegerType, LongType, StringType, StructField, StructType}
 
 /**
  * Basic tests for non-embedded mode connections to an embedded cluster.
@@ -361,6 +376,35 @@ class SplitSnappyClusterDUnitTest(s: String)
     }
   }
 
+  def testStaleCatalogRetryForStreamingSink(): Unit = {
+    val snc = SnappyContext(sc)
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val testTempDirectory = "/tmp/SplitSnappyClusterDUnitTest"
+
+    val future = Future {
+      vm3.invoke(getClass, "doTestStaleCatalogRetryForStreamingSink",
+        startArgs :+ Int.box(locatorClientPort) :+ testTempDirectory)
+    }
+    try {
+      var attempts = 0
+      while (!Files.exists(Paths.get(testTempDirectory, "file0")) && attempts < 15) {
+        Thread.sleep(4000)
+        attempts += 1
+      }
+      assert(attempts < 14, "No data ingested by streaming application.")
+
+      // perform DDL leading to stale catalog in smart connector application
+      snc.sql(s"CREATE TABLE SYNC_TABLE(COL1 STRING) " + s"USING column")
+
+      new PrintWriter(s"$testTempDirectory/file1") {
+        write("dummydata"); close()
+      }
+      Await.result(future, Duration(2, "min"))
+    } finally {
+      snc.sql("drop table if exists T6")
+      snc.sql("drop table if exists T5")
+    }
+  }
 
   def testSNAP3024(): Unit = {
     val snc = SnappyContext(sc)
@@ -1237,6 +1281,97 @@ object SplitSnappyClusterDUnitTest
         retryOperation(5) {
           snc.sql("update t7 set col2 = '22' where col1 = '2'")
         }
+    }
+  }
+
+  def doTestStaleCatalogRetryForStreamingSink(locatorPort: Int,
+      prop: Properties, locatorClientPort: Int, testTempDir: String): Unit = {
+    val tableName = "users"
+    val kafkaTestUtils = new KafkaTestUtils
+    kafkaTestUtils.setup()
+    kafkaTestUtils.createTopic(tableName, partitions = 3)
+    try {
+      val snc: SnappyContext = getSnappyContextForConnector(locatorClientPort)
+      snc.sql(s"drop table if exists $tableName")
+      snc.sql(
+        s"""create table $tableName (id long , name varchar(40), age int)
+           | using column options(key_columns 'id')""".stripMargin)
+
+      val streamingDF = snc
+          .readStream
+          .format("kafka")
+          .option("kafka.bootstrap.servers", kafkaTestUtils.brokerAddress)
+          .option("subscribe", tableName)
+          .option("startingOffsets", "earliest")
+          .load()
+
+      implicit val encoder = RowEncoder(snc.table(tableName).schema)
+      val session = snc.sparkSession
+      import session.implicits._
+      val streamingQuery = streamingDF.selectExpr("CAST(value AS STRING)")
+          .as[String]
+          .map(_.split(","))
+          .map(r => Row(r(0).toLong, r(1), r(2).toInt))
+          .writeStream
+          .format("snappysink")
+          .queryName(tableName)
+          .trigger(ProcessingTime("1 seconds"))
+          .option("tableName", tableName)
+          .option("checkpointLocation", s"$testTempDir/checkpoint")
+          .start()
+
+      // produce first batch of data
+      val dataBatch1 = Seq(Seq(1, "name1", 20), Seq(2, "name2", 20))
+      kafkaTestUtils.sendMessages(tableName, dataBatch1.map(r => r.mkString(",")).toArray)
+
+      waitTillTheBatchIsPickedForProcessing(snc, 0, tableName)
+
+      new PrintWriter(s"$testTempDir/file0") {
+        write("dummyData")
+        close()
+      }
+
+      // wait till DDL is fired on snappy cluster which will lead to stale smart-connector catalog
+      var attempts = 0
+      while (!Files.exists(Paths.get(testTempDir, "file1")) && attempts < 15) {
+        Thread.sleep(4000)
+        attempts += 1
+      }
+
+      assert(attempts < 14, "Waiting for stale catalog timed out")
+
+      // produce second batch of data
+      val dataBatch2 = Seq(Seq(3, "name3", 20))
+      kafkaTestUtils.sendMessages(tableName, dataBatch2.map(r => r.mkString(",")).toArray)
+
+      streamingQuery.processAllAvailable()
+
+      assertData(Array(Row(1, "name1", 20), Row(2, "name2", 20), Row(3, "name3", 20)))
+
+      def assertData(expectedData: Array[Row]): Unit = {
+        val actualData = snc.sql(s"select * from $tableName" +
+            s" order by id, name, age")
+            .collect()
+
+        assert(expectedData sameElements actualData, "actual data:" +
+            actualData.map(a => a.toString()).mkString(","))
+      }
+    } finally {
+      kafkaTestUtils.teardown()
+    }
+  }
+
+  private def waitTillTheBatchIsPickedForProcessing(snc: SnappyContext, batchId: Int,
+      queryName: String, retries: Int = 15): Unit = {
+    if (retries == 0) {
+      throw new RuntimeException(s"Batch id $batchId not found in sink status table")
+    }
+    val sql = s"select batch_id from snappysys_internal____sink_state_table " +
+        s"where stream_query_id = '$queryName'"
+    val batchIdFromTable = snc.sql(sql).collect()
+    if (batchIdFromTable.isEmpty || batchIdFromTable(0)(0) != batchId) {
+      Thread.sleep(1000)
+      waitTillTheBatchIsPickedForProcessing(snc, batchId, queryName, retries - 1)
     }
   }
 }
