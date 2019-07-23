c7b1fd08fa2ea2596a6725d7a50d486ecb5f0ca8
Chandresh <cbhatt@snappydata.io>
2019-07-23 18:45:37 +0530

    Add the test case for concurrent dml operations with Hive Thrift Server

--- dtests/src/test/java/io/snappydata/hydra/hiveThriftServer/HiveThrifServerUtils.java
+++ dtests/src/test/java/io/snappydata/hydra/hiveThriftServer/HiveThrifServerUtils.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class HiveThrifServerUtils extends SnappyTest {
 
@@ -33,16 +34,16 @@ public class HiveThrifServerUtils extends SnappyTest {
 
     public static String[] snappyQuery = {"update snappyDB.Student set marks = marks + 100",
             "update snappyDB.Student set subject = 'Multithreading and Concurrency' where subject='Maths-2'",
-            "update snappy.Student set marks = marks - 200",
-            "update snappy.Student set subject = 'Maths-2' where subject='Multithreading and Concurrency'",
-            "delete from snappy.Student where subject = 'Graphics'"};
+            "update snappyDB.Student set marks = marks - 200",
+            "update snappyDB.Student set subject = 'Maths-2' where subject='Multithreading and Concurrency'",
+            "delete from snappyDB.Student where subject = 'Graphics'"};
 
     private static Connection connectToBeeline() {
         Connection jdbcConnection = null;
         try {
             jdbcConnection = DriverManager.getConnection("jdbc:hive2://localhost:10000", "app", "app");
         } catch(SQLException se) {
-            Log.getLogWriter().info("Error in connecting to Beeline , " + se.getMessage());
+            throw new TestException("Error when connecting  to Beeline", se);
         }
         return  jdbcConnection;
     }
@@ -50,6 +51,8 @@ public class HiveThrifServerUtils extends SnappyTest {
     public static void HydraTask_CreateTableFromBeeline() {
         Connection jdbcConnection = connectToBeeline();
         try {
+            Vector filePath = SnappyPrms.getDataLocationList();
+            Log.getLogWriter().info("Hive Thrift Server data path : " + filePath.get(0));
             jdbcConnection.createStatement().execute("create schema beelineDB");
             //Below line needs to be added when external hive meta store merged into the master.
             //htsUtils.jdbcConnection.createStatement().execute("create table if not exists beelineDB.Student(id int, name String, subject String, marks int, tid int) row format delimited fields terminated by ','");
@@ -63,8 +66,8 @@ public class HiveThrifServerUtils extends SnappyTest {
                 Log.getLogWriter().info(rs.getString(1) + "," + rs.getString(2) + "," + rs.getString(3) + "," + rs.getString(4) + "," + rs.getString(5) );
             }
             } catch(SQLException se) {
-            Log.getLogWriter().info("Error in creating table from beeline , " + se.getMessage());
-         }
+            throw new TestException("Error in creating table from beeline ,", se);
+        }
          closeConnection(jdbcConnection);
     }
 
@@ -85,7 +88,7 @@ public class HiveThrifServerUtils extends SnappyTest {
 
             Log.getLogWriter().info("Table snappyDB.Student created successfully in Snappy");
         } catch(SQLException se) {
-            Log.getLogWriter().info("Error in creating table from snappy , " + se.getMessage());
+            throw new TestException("Error in creating table from snappy ", se);
         }
         closeConnection(snappyJDBCConnection);
     }
@@ -116,11 +119,11 @@ public class HiveThrifServerUtils extends SnappyTest {
             Log.getLogWriter().info("SnappyConnection : " + snappyJDBCConnection.isClosed());
             Log.getLogWriter().info("JDBCConnection : " + jdbcConnection.isClosed());
         } catch (SQLException se) {
-            Log.getLogWriter().info("Error while establishing the connection, " + se.getMessage());
+            throw new TestException("Error while establishing the connection : " , se);
         }
 
         try {
-        beelineRs = jdbcConnection.createStatement().executeQuery("select * from beelineDB.Student");
+        beelineRs = jdbcConnection.createStatement().executeQuery("select * from beelineDB.Student ");
         snappyRs = snappyJDBCConnection.createStatement().executeQuery("select * from snappyDB.Student");
 
         StructTypeImpl beelineSti = ResultSetHelper.getStructType(beelineRs);
@@ -145,7 +148,6 @@ public class HiveThrifServerUtils extends SnappyTest {
     }
     }
 
-
     public static void HydraTask_populateTableData() {
         htsUtils = new HiveThrifServerUtils();
         int tid = htsUtils.getMyTid();
@@ -157,7 +159,6 @@ public class HiveThrifServerUtils extends SnappyTest {
         String logFile = "snappyJobResult_thr_" + tid + "_" + System.currentTimeMillis() + ".log";
         htsUtils.executeSnappyJob(SnappyPrms.getSnappyJobClassNames(), logFile, SnappyPrms.getUserAppJar(),
                 jarPath, SnappyPrms.getUserAppName());
-
     }
 
     public static void HydraTask_performDMLOps() {
@@ -184,9 +185,10 @@ public class HiveThrifServerUtils extends SnappyTest {
             Log.getLogWriter().info("SnappyConnection : " + snappyJDBCConnection.isClosed());
             Log.getLogWriter().info("JDBCConnection : " + jdbcConnection.isClosed());
         } catch (SQLException se) {
-            Log.getLogWriter().info("Error while establishing the connection, " + se.getMessage());
+           throw new TestException("Error while establishing the connection :  " , se);
         }
         for (int index = 0; index < snappyQuery.length; index++) {
+
             String beelineStmt = beelineQuery[index];
             Log.getLogWriter().info("tid = " + tid);
             if (beelineStmt.toUpperCase().contains("WHERE"))
@@ -203,13 +205,11 @@ public class HiveThrifServerUtils extends SnappyTest {
             Log.getLogWriter().info("snappyQuery :  " + snappyStmt);
 
             try {
-
                 jdbcConnection.createStatement().executeUpdate(beelineStmt);
-                beelineRs = jdbcConnection.createStatement().executeQuery("select * from beelineDB.Student where tid = " + tid);
-
                 snappyJDBCConnection.createStatement().executeUpdate(snappyStmt);
-                snappyRs = snappyJDBCConnection.createStatement().executeQuery("select * from snappyDB.Student where tid = " + tid);
 
+                beelineRs = jdbcConnection.createStatement().executeQuery("select * from beelineDB.Student where tid = " + tid);
+                snappyRs = snappyJDBCConnection.createStatement().executeQuery("select * from snappyDB.Student where tid = " + tid);
 
                 StructTypeImpl beelineSti = ResultSetHelper.getStructType(beelineRs);
                 StructTypeImpl snappySti = ResultSetHelper.getStructType(snappyRs);
@@ -218,25 +218,34 @@ public class HiveThrifServerUtils extends SnappyTest {
                 beelineRs.close();
                 snappyRs.close();
 
-                beelineFile = logFile + File.separator + "beelineQuery_" + index  + "_" + tid +  ".out";
-                snappyFile = logFile + File.separator + "snappyQuery_" + index + "_" + tid +  ".out";
+                beelineFile = logFile + File.separator + "beelineQuery_" + index + "_" + tid + ".out";
+                snappyFile = logFile + File.separator + "snappyQuery_" + index + "_" + tid + ".out";
 
                 testInstance.listToFile(snappyList, snappyFile);
-                testInstance.listToFile(beelineList,beelineFile);
+                testInstance.listToFile(beelineList, beelineFile);
 
                 String msg = testInstance.compareFiles(logFile, beelineFile, snappyFile, false, "query_" + index);
-                if(msg.length() > 0 ) {
+                if (msg.length() > 0) {
                     throw new TestException("Validation failed : " + msg);
                 }
-            } catch (SQLException se) {
-                Log.getLogWriter().info("Error while closing the connection, " + se.getMessage());
+            } catch(SQLException se) {
+                throw new TestException("Got Exception while executing stmt", se);
             }
         }
+
+        try {
+            jdbcConnection.close();
+            snappyJDBCConnection.close();
+        } catch (SQLException se) {
+            Log.getLogWriter().info("Error while closing the connection, " + se.getMessage());
+        }
+
     }
 
     public static void HydraTask_DropTableFromBeeline() {
         Connection jdbcConnection = connectToBeeline();
         try {
+            jdbcConnection.createStatement().execute("drop table if exists beelineDB.stage_Student");
             jdbcConnection.createStatement().execute("drop table if exists beelineDB.Student");
             jdbcConnection.createStatement().execute("drop schema beelineDB restrict");
             Log.getLogWriter().info("Table beelineDB.Student dropped from beeline");
@@ -250,6 +259,7 @@ public class HiveThrifServerUtils extends SnappyTest {
         Connection snappyJDBCConnection = null;
         try {
             snappyJDBCConnection = SnappyTest.getLocatorConnection();
+            snappyJDBCConnection.createStatement().execute("drop table if exists snappyDB.stage_Student");
             snappyJDBCConnection.createStatement().execute("drop table if exists snappyDB.Student");
             snappyJDBCConnection.createStatement().execute("drop schema snappyDB restrict");
             Log.getLogWriter().info("Table snappyDB.Student dropped from snappy");
@@ -257,5 +267,6 @@ public class HiveThrifServerUtils extends SnappyTest {
             Log.getLogWriter().info("Error in droping table from snappy , " + se.getMessage());
         }
         closeConnection(snappyJDBCConnection);
+        Log.getLogWriter().info("HiveThrift Server Concurrent validation test successful.");
     }
 }
