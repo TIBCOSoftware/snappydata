015ecdb0d948a02d5f17f643c64863a3bf551635
Sumedh Wale <swale@snappydata.io>
2019-07-24 17:11:57 +0530

    initial code for porting hive suite

--- dtests/src/test/java/io/snappydata/hydra/cluster/SnappyTest.java
+++ dtests/src/test/java/io/snappydata/hydra/cluster/SnappyTest.java
@@ -2174,11 +2174,12 @@ public class SnappyTest implements Serializable {
   }
 
   protected String getPrimaryLeadHost() {
+    leadHost = (String) SnappyBB.getBB().getSharedMap().get("primaryLeadHost");
     if (leadHost == null) {
       retrievePrimaryLeadHost();
       leadHost = (String) SnappyBB.getBB().getSharedMap().get("primaryLeadHost");
       Log.getLogWriter().info("primaryLead Host is: " + leadHost);
-    }  else leadHost = (String) SnappyBB.getBB().getSharedMap().get("primaryLeadHost");
+    }
     return leadHost;
   }
 
