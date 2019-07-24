04e5f83e245adb086fd56f78f2e7702c2af7e704
Piyush Bisen <pbisen@snappydata.io>
2019-07-24 14:18:54 +0530

    Added one more test for Failover RETRY

--- core/src/test/scala/io/snappydata/util/ODBCDriverTestSuite.scala
+++ core/src/test/scala/io/snappydata/util/ODBCDriverTestSuite.scala
@@ -8,27 +8,42 @@ import scala.sys.process._
 
 class ODBCDriverTestSuite extends SnappyTestRunner {
   val snappyProductDir = System.getenv("SNAPPY_HOME")
-  test("ODBC_FailOverTest"){
+  test("ODBC_FailOverTest_NEWSERVER"){
     try {
       // cluster has been started already
-      var consoleOutput = (snappyProductDir +
-          "/sbin/snappy-server.sh start -locators=localhost:10334").!!
-      assert(consoleOutput.contains("ERROR"),
-        s"Option -dir not specified: $consoleOutput")
+//      var consoleOutput = (snappyProductDir +
+//          "/sbin/snappy-server.sh start -locators=localhost:10334").!!
+//      assert(consoleOutput.contains("ERROR"),
+//        s"Option -dir not specified: $consoleOutput")
       var scriptPath = snappyProductDir +
           "/../../../store/native/tests/unitTest.sh "
       var scriptDirPath = snappyProductDir +
           "/../../../store/native/tests/"
-//      var scriptPath = "./unitTest.sh "
-      consoleOutput = (scriptPath
+      var consoleOutput = (scriptPath
           + snappyProductDir + " " + scriptDirPath).!!
       assert(consoleOutput.contains("after"),
         "Failed")
     } finally {
-      (snappyProductDir +
-          "/sbin/snappy-stop-all.sh").!!
+
+    }
+  }
+  test("ODBC_FailOverTest_RETRY"){
+    try {
+      // cluster has been started already
+//      var consoleOutput = (snappyProductDir +
+//          "/sbin/snappy-server.sh start -locators=localhost:10334").!!
+//      assert(consoleOutput.contains("ERROR"),
+//        s"Option -dir not specified: $consoleOutput")
+      var scriptPath = snappyProductDir +
+          "/../../../store/native/tests/unitTest_2.sh "
       var scriptDirPath = snappyProductDir +
           "/../../../store/native/tests/"
+      var consoleOutput = (scriptPath
+          + snappyProductDir + " " + scriptDirPath).!!
+      assert(consoleOutput.contains("Exception"),
+        "Failed to connect to same server")
+    } finally {
+
     }
   }
 }
