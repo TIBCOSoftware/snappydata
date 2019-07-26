f1a6517bfa014ec6b67045fb8f4edff18bdc6dae
Piyush Bisen <pbisen@snappydata.io>
2019-07-26 18:26:41 +0530

    Recfactored the unit test code.

--- core/src/test/scala/io/snappydata/util/ODBCDriverTestSuite.scala
+++ core/src/test/scala/io/snappydata/util/ODBCDriverTestSuite.scala
@@ -8,30 +8,25 @@ import scala.sys.process._
 
 class ODBCDriverTestSuite extends SnappyTestRunner {
   val snappyProductDir = System.getenv("SNAPPY_HOME")
+  val snappyNativeTestDir = s"$snappyProductDir/../../../store/native/tests"
+
   test("ODBC_FailOverTest_NEWSERVER"){
     try {
-      var scriptPath = snappyProductDir +
-          "/../../../store/native/tests/unitTest.sh "
-      var scriptDirPath = snappyProductDir +
-          "/../../../store/native/tests/"
-      var consoleOutput = (scriptPath
-          + snappyProductDir + " " + scriptDirPath).!!
-      assert(consoleOutput.contains("after"),
-        "Failed")
+      var scriptPath = s"$snappyNativeTestDir/failoverTest_NewServer.sh"
+      var consoleOutput = s"$scriptPath $snappyProductDir $snappyNativeTestDir".!!
+      assert(consoleOutput.contains("Test executed successfully"),
+        s"FailOver failed $consoleOutput")
     } finally {
 
     }
   }
+
   test("ODBC_FailOverTest_NONE"){
     try {
-      var scriptPath = snappyProductDir +
-          "/../../../store/native/tests/unitTest_2.sh "
-      var scriptDirPath = snappyProductDir +
-          "/../../../store/native/tests/"
-      var consoleOutput = (scriptPath
-          + snappyProductDir + " " + scriptDirPath).!!
-      assert(consoleOutput.contains("Exception"),
-        "Failed to connect to same server")
+      var scriptPath = s"$snappyNativeTestDir/failoverTest_None.sh"
+      var consoleOutput = s"$scriptPath $snappyProductDir $snappyNativeTestDir".!!
+      assert(consoleOutput.contains("Test executed successfully, no failover tried"),
+        s"There failover tried but failed $consoleOutput")
     } finally {
 
     }
