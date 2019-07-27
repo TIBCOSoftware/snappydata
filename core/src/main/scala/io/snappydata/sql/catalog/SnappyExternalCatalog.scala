a2cf8568d1a3001199f4a197d1dc6801ff62e194
paresh-p11 <43569032+paresh-p11@users.noreply.github.com>
2019-07-27 10:09:30 +0530

    Fixing [SNAP-2653] (#1368)
    * Mask credentials (in case of s3 URI) in Describe extended/formatted output.
    * Mask credentials in case of s3 on UI for external tables.
    * Disallow access non-admin user to the tables in SNAPPY_HIVE_METASTORE.

--- core/src/main/scala/io/snappydata/sql/catalog/SnappyExternalCatalog.scala
+++ core/src/main/scala/io/snappydata/sql/catalog/SnappyExternalCatalog.scala
@@ -289,7 +289,7 @@ object SnappyExternalCatalog {
   val INDEXED_TABLE_LOWER: String = Utils.toLowerCase("INDEXED_TABLE")
 
   val EMPTY_SCHEMA: StructType = StructType(Nil)
-  private[sql] val PASSWORD_MATCH = "(?i)(password|passwd).*".r
+  private[sql] val PASSWORD_MATCH = "(?i)(password|passwd|secret).*".r
 
   val currentFunctionIdentifier = new ThreadLocal[FunctionIdentifier]
 
