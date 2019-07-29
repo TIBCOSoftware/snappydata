8336e572e1d16070674a7f5d36c99039f9c5b143
Vatsal Mevada <vmevada@snappydata.io>
2019-07-29 16:20:46 +0530

    incorporating review comments

--- cluster/src/main/scala/org/apache/spark/util/LocalDirectoryCleanupUtil.scala
+++ cluster/src/main/scala/org/apache/spark/util/LocalDirectoryCleanupUtil.scala
@@ -1,21 +1,5 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * Copyright (c) 2018 SnappyData, Inc. All rights reserved.
+ * Copyright (c) 2017-2019 TIBCO Software Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You
@@ -35,12 +19,11 @@ package org.apache.spark.util
 
 import java.io.File
 import java.nio.file.{Files, Path, Paths}
-import java.util.Collections
-import java.util.function.Consumer
 
 import scala.collection.JavaConverters._
 import scala.io.Source
 
+import com.gemstone.gemfire.internal.shared.ClientSharedUtils
 import org.apache.commons.io.FileUtils
 
 import org.apache.spark.SparkEnv
@@ -59,7 +42,10 @@ object LocalDirectoryCleanupUtil extends Logging {
    * these directories if left orphan due to abrupt JVM failure.
    */
   def save(): Unit = synchronized {
-    FileUtils.writeLines(new File(listFile), "UTF-8", getLocalDirs.toList.asJava, true)
+    if (SparkEnv.get != null) {
+      val localDirs = SparkEnv.get.blockManager.diskBlockManager.localDirs.toList.asJava
+      FileUtils.writeLines(new File(listFile), "UTF-8", localDirs, true)
+    }
   }
 
   /**
@@ -86,19 +72,9 @@ object LocalDirectoryCleanupUtil extends Logging {
 
   private def delete(path: Path): Unit = {
     if (Files.exists(path)) {
-      Files.walk(path).sorted(Collections.reverseOrder()).forEach(new Consumer[Path] {
-        override def accept(p: Path): Unit = {
-          try {
-            Files.delete(p)
-          } catch {
-            case e: Exception => logError(s"Failure while deleting file or directory: $p.", e)
-          }
-        }
-      })
+      ClientSharedUtils.deletePath(path, false, false)
     } else {
       logInfo(s"File or directory does not exists : $path")
     }
   }
-
-  private def getLocalDirs: Array[File] = SparkEnv.get.blockManager.diskBlockManager.localDirs
 }
