df25e508d5824fcd04e928625928cb44f1de0002
ahshahid <ashahid@snappydata.io>
2019-07-29 11:42:37 -0700

    checkin in the missing file

--- core/src/main/scala/org/apache/spark/sql/internal/session.scala
+++ core/src/main/scala/org/apache/spark/sql/internal/session.scala
@@ -97,7 +97,11 @@ class SnappyConf(@transient val session: SnappySession)
     case SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key |
          Property.HashJoinSize.name |
          Property.HashAggregateSize.name |
-         Property.ForceLinkPartitionsToBuckets.name =>
+         Property.ForceLinkPartitionsToBuckets.name |
+         Property.ForceLinkPartitionsToBuckets.name |
+         Property.TestDisableByteBufferMapInSHA.name |
+         Property.TestUseBBMapInSHAFor1StringGrouBy.name |
+         Property.TestExplodeComplexDataTypeInSHA.name =>
       session.clearPlanCache()
       key
     case SQLConf.SHUFFLE_PARTITIONS.key =>
