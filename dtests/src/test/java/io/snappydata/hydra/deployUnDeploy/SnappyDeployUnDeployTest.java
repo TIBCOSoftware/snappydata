a4f934c8c2cbf1d29cb4fdf3210e98879c854189
supriya <spillai@snappydata.io>
2019-08-02 10:37:15 +0530

    -Added test for deploy and undeploy of packages.

--- dtests/src/test/java/io/snappydata/hydra/deployUnDeploy/SnappyDeployUnDeployTest.java
+++ dtests/src/test/java/io/snappydata/hydra/deployUnDeploy/SnappyDeployUnDeployTest.java
@@ -1,22 +1,124 @@
 package io.snappydata.hydra.deployUnDeploy;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Vector;
 
 import hydra.Log;
 import hydra.TestConfig;
+import io.snappydata.hydra.cdcConnector.SnappyCDCPrms;
 import io.snappydata.hydra.cluster.SnappyPrms;
 import io.snappydata.hydra.cluster.SnappyTest;
 import io.snappydata.test.util.TestException;
-import scala.tools.nsc.transform.patmat.Logic;
-
 
 public class SnappyDeployUnDeployTest extends SnappyTest {
 
   public static SnappyDeployUnDeployTest snappyDeployUnDeployTest;
+  public String jarAlias = "cassandraJar";
 
   public SnappyDeployUnDeployTest() {
+  }
+
+  public static void HydraTask_closeCassandraCluster() {
+    if (snappyDeployUnDeployTest == null) {
+      snappyDeployUnDeployTest = new SnappyDeployUnDeployTest();
+    }
+    snappyDeployUnDeployTest.closeCassandraCluster();
+  }
+
+  public void closeCassandraCluster() {
+    try {
+      String cmd = "pkill -f CassandraDaemon";
+      String dest = getCurrentDirPath() + File.separator + "cassandraClusterStop.log";
+      File logFile = new File(dest);
+      ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", cmd);
+      snappyTest.executeProcess(pb, logFile);
+    } catch (Exception ex) {
+      throw new TestException("Exception while closing Cassandra Cluster " + ex.getMessage());
+    }
+  }
+
+  public static void HydraTask_startCassandraCluster() {
+    if (snappyDeployUnDeployTest == null) {
+      snappyDeployUnDeployTest = new SnappyDeployUnDeployTest();
+    }
+    snappyDeployUnDeployTest.startCassandraCluster();
+  }
+
+  public void startCassandraCluster() {
+    String cassPath = SnappyCDCPrms.getSnappyFileLoc();
+    String scripPath = SnappyPrms.getDataLocationList().get(0).toString();
+    String dest = getCurrentDirPath() + File.separator + "cassandraClusterStart.log";
+    File logFile = new File(dest);
+    try {
+      ProcessBuilder pbClustStart = new ProcessBuilder(cassPath + "/bin/cassandra");
+      Long startTime1 = System.currentTimeMillis();
+      snappyTest.executeProcess(pbClustStart, logFile);
+      Long totalTime1 = (System.currentTimeMillis() - startTime1);
+      Log.getLogWriter().info("The cassandra cluster took " + totalTime1 + " ms to start up");
+
+      ProcessBuilder pbScript = new ProcessBuilder(cassPath + "/bin/cqlsh", "-f", scripPath);
+      snappyTest.executeProcess(pbScript, logFile);
+      Log.getLogWriter().info("The cassandra script execution completed");
+    } catch (Exception ex) {
+      throw new TestException("Exception while start Cassandra Cluster" + ex.getMessage());
+    }
+  }
+
+  public static void HydraTask_deployPkg() {
+    if (snappyDeployUnDeployTest == null) {
+      snappyDeployUnDeployTest = new SnappyDeployUnDeployTest();
+    }
+    snappyDeployUnDeployTest.deployPkg();
+  }
+
+  public void deployPkg() {
+
+    String pkgName = "com.datastax.spark:spark-cassandra-connector_2.11:2.0.7";
+    String pkgPath = SnappyPrms.getDataLocationList().get(0).toString();
+    try {
+      Connection conn = getLocatorConnection();
+      String deployStr = "deploy package " + jarAlias + " '" + pkgName + "' path '" + pkgPath + "' ";
+      Log.getLogWriter().info("The deployStr is " + deployStr);
+      conn.createStatement().execute(deployStr);
+    } catch (Exception ex) {
+      throw new TestException("Exception while deploying jar " + ex.getMessage());
+    }
+  }
+
+  public void deployJar() {
+    String jarName = "com.datastax.spark_spark-cassandra-connector_2.11-2.0.7.jar";
+    String jarPath = SnappyPrms.getDataLocationList().get(0).toString() + "/" + jarName;
+    try {
+      //deploy package kafkaSource 'org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.1' path '/home/smahajan/Downloads';
+      Connection conn = getLocatorConnection();
+      String deployStr = "deploy jar " + jarAlias + " '" + jarPath + "'";
+      Log.getLogWriter().info("The deployStr is " + deployStr);
+      conn.createStatement().execute(deployStr);
+    } catch (Exception ex) {
+      throw new TestException("Exception while deploying jar " + ex.getMessage());
+    }
+  }
+
+  public static void HydraTask_unDeployPkg() {
+    if (snappyDeployUnDeployTest == null) {
+      snappyDeployUnDeployTest = new SnappyDeployUnDeployTest();
+    }
+    snappyDeployUnDeployTest.unDeployPkg();
+  }
+
+  public void unDeployPkg() {
+    try {
+      //undeploy packageAlias;
+      Connection conn = getLocatorConnection();
+      String unDeployStr = "undeploy " + jarAlias;
+      Log.getLogWriter().info("The deployStr is " + unDeployStr);
+      conn.createStatement().execute(unDeployStr);
+    } catch (Exception ex) {
+      throw new TestException("Exception while undeploying jar " + ex.getMessage());
+    }
   }
 
   public static void HydraTask_createFunction() {
@@ -161,4 +263,16 @@ public class SnappyDeployUnDeployTest extends SnappyTest {
       throw new TestException("Exception while dropping function" + ex.getMessage());
     }
   }
+
+  public void listUDFS(Connection conn) {
+    try {
+      String cmd = " list jars";
+      ResultSet rs = conn.createStatement().executeQuery(cmd);
+
+    } catch (Exception ex) {
+
+    }
+  }
+
+
 }
