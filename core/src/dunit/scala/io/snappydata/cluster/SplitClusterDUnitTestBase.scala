e7d3f0967a5d2f4781d5a84719201cc76a26e498
suranjan kumar <skumar@snappydata.io>
2019-08-02 13:33:38 +0530

    Fixed an issue with lock release in case of deleteFrom  (#1407)
    * Refactor and fixed a case where delete was not releasing lock
    * Added more tests

--- core/src/dunit/scala/io/snappydata/cluster/SplitClusterDUnitTestBase.scala
+++ core/src/dunit/scala/io/snappydata/cluster/SplitClusterDUnitTestBase.scala
@@ -26,16 +26,14 @@ import scala.collection.mutable.ArrayBuffer
 import scala.language.postfixOps
 import scala.util.Random
 import scala.util.control.NonFatal
-
 import com.pivotal.gemfirexd.Attribute
 import com.pivotal.gemfirexd.internal.engine.Misc
 import io.snappydata.Property.PlanCaching
 import io.snappydata.test.dunit.{SerializableRunnable, VM}
 import io.snappydata.test.util.TestException
 import io.snappydata.util.TestUtils
-import io.snappydata.{ColumnUpdateDeleteTests, Constant, SnappyFunSuite}
+import io.snappydata.{ColumnUpdateDeleteTests, ConcurrentOpsTests, Constant, SnappyFunSuite}
 import org.junit.Assert
-
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.collection.{Utils, WrappedInternalRow}
 import org.apache.spark.sql.store.{MetadataTest, StoreUtils}
@@ -198,6 +196,31 @@ trait SplitClusterDUnitTestBase extends Logging {
   def testTableFormChanges(): Unit = {
     doTestTableFormChanges(skewNetworkServers)
   }
+
+  def testConcurrentOpsOnColumnTables(): Unit = {
+    val testObject = this.testObject
+    val netPort = this.locatorClientPort
+    // check update/delete in the connector mode
+    vm3.invoke(new SerializableRunnable() {
+      override def run(): Unit = {
+        val snc = testObject.getSnappyContextForConnector(netPort)
+        val session = snc.snappySession
+        ConcurrentOpsTests.testSimpleLockInsert(session)
+        ConcurrentOpsTests.testSimpleLockUpdate(session)
+        ConcurrentOpsTests.testSimpleLockDeleteFrom(session)
+        ConcurrentOpsTests.testSimpleLockPutInto(session)
+
+        ConcurrentOpsTests.testConcurrentUpdate(session)
+        ConcurrentOpsTests.testConcurrentPutInto(session)
+        ConcurrentOpsTests.testConcurrentDelete(session)
+        ConcurrentOpsTests.testConcurrentPutIntoUpdate(session)
+        ConcurrentOpsTests.testAllOpsConcurrent(session)
+        ConcurrentOpsTests.testConcurrentDeleteFromMultipleTables(session)
+        ConcurrentOpsTests.testConcurrentPutIntoMultipleTables(session)
+      }
+    })
+  }
+
 
   def testUpdateDeleteOnColumnTables(): Unit = {
     val testObject = this.testObject
