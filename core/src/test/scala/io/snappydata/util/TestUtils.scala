7d4a559d0f9df822b2910b0c60ce857244b91252
Sumedh Wale <swale@tibco.com>
2019-08-04 03:06:55 +0530

    added dunit test for spark.task.cpus
    renamed ColumnBatchScanDUnitTest to ColumnTableDUnitTest and added unit test to
    check for various cases with spark.task.cpus settings

--- core/src/test/scala/io/snappydata/util/TestUtils.scala
+++ core/src/test/scala/io/snappydata/util/TestUtils.scala
@@ -24,7 +24,8 @@ import org.apache.spark.sql.{Row, SnappyContext, SnappySession}
 
 object TestUtils extends Logging {
 
-  def defaultCores: Int = math.min(8, Runtime.getRuntime.availableProcessors())
+  // factor of 2 like in product by default
+  val defaultCores: Int = math.min(8, Runtime.getRuntime.availableProcessors()) * 2
 
   def dropAllSchemas(session: SnappySession): Unit = {
     val sc = SnappyContext.globalSparkContext
