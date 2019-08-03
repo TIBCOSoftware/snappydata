455274ec792c31e5ab568c210024e40362d596d7
Sumedh Wale <swale@tibco.com>
2019-08-04 03:26:52 +0530

    fix test "fix" in d203948d6eb66ac11627aa2babbfbdc75c3ac798 that broke smoke tests

--- dtests/src/test/java/io/snappydata/hydra/cluster/SnappyTest.java
+++ dtests/src/test/java/io/snappydata/hydra/cluster/SnappyTest.java
@@ -3245,8 +3245,12 @@ public class SnappyTest implements Serializable {
     boolean checkHang = new File(currDir, "hang.txt").exists();
     { // record SparkSubmit processes if left
       Vector<String> hostNamesList = BasePrms.tab().vecAt(HostPrms.hostNames, null);
-      if (hostNamesList == null || hostNamesList.isEmpty())
+      if (hostNamesList == null) {
+        hostNamesList = new Vector<>(1);
+      }
+      if (hostNamesList.isEmpty()) {
         hostNamesList.add("localhost");
+      }
       for (int i = 0; i < hostNamesList.size(); i++)
         recordSnappyProcessIDinNukeRun("SparkSubmit", hostNamesList.get(i));
 
