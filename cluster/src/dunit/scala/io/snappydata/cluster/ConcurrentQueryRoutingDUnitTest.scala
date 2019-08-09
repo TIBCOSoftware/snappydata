78e2e0fe2211d758fd80e5356ba635d24b1f1a69
ahshahid <ashahid@snappydata.io>
2019-08-08 20:48:48 -0700

    Changing tests to use CodegenFallback

--- cluster/src/dunit/scala/io/snappydata/cluster/ConcurrentQueryRoutingDUnitTest.scala
+++ cluster/src/dunit/scala/io/snappydata/cluster/ConcurrentQueryRoutingDUnitTest.scala
@@ -26,7 +26,10 @@ import org.apache.spark.sql.collection.Utils
 
 class ConcurrentQueryRoutingDUnitTest(val s: String)
     extends ClusterManagerTestBase(s) with Logging {
-
+  sysProps.remove("spark.testing")
+  System.clearProperty("spark.testing")
+  sysProps.remove("SPARK_TESTING")
+  System.clearProperty("SPARK_TESTING")
   def columnTableRouting(thr: Int, iter: Int, serverHostPort: Int): Int = {
     val tableName = s"order_line_col_${thr}_$iter"
 
