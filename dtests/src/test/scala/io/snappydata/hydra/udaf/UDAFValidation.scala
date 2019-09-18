eaf77a88802d1e7d0932f17cf87291a0d2529b71
Chandresh <cbhatt@snappydata.io>
2019-09-18 20:56:35 +0530

    Add negative test cases,wrong jar path, wrong qualified class name and add log messages

--- dtests/src/test/scala/io/snappydata/hydra/udaf/UDAFValidation.scala
+++ dtests/src/test/scala/io/snappydata/hydra/udaf/UDAFValidation.scala
@@ -16,6 +16,8 @@
  */
 package io.snappydata.hydra.udaf
 
+import java.io.{File, FileOutputStream, PrintWriter}
+
 import com.typesafe.config.Config
 import org.apache.spark.sql._
 
@@ -26,21 +28,42 @@ class UDAFValidation extends SnappySQLJob {
     // scalastyle:off println
     println("UDAF validation started....")
     val snc : SnappyContext = snappySession.sqlContext
+
     val createUDAF = "create function mean_udaf as io.snappydata.hydra.MeanMarks " +
       "returns double " +
       "using jar '/home/cbhatt/TestWork/out/artifacts/TestWork_jar/TestWork.jar'"
+
+    val createUDAFWrongJarPath = "create function mean_udaf as io.snappydata.hydra.MeanMarks " +
+      "returns double " +
+      "using jar '/home/cbhatt/TestWork/TestWork.jar'"
+
+    val createUDAFWrongQualifiedClass = "create function mean_udaf " +
+      "as io.snappydata.udaftest.AvgMarks " +
+      "returns double " +
+      "using jar '/home/cbhatt/TestWork/out/artifacts/TestWork_jar/TestWork.jar'"
+
     val dropUDAF = "drop function if exists mean_udaf"
+
     val createStagingStudent = "create external table if not exists staging_student" +
       "(studentid int,name string,class int," +
       "maths double,english double,physics double,socialst double,year int,total double) " +
       "using csv options(path '/home/cbhatt/UDAF_Data_1')"
+
     val dropStagingStudent = "drop table if exists stagingStudent"
+
     val createStudent = "create table if not exists " +
       "student using column as select * from staging_student"
+
     val dropStudent = "drop table if exists Student"
+
+    val outputFile = "ValidateUDAF_" +  System.currentTimeMillis()
+    val pw : PrintWriter = new PrintWriter(new FileOutputStream(new File(outputFile), true))
+    pw.println("UDAF validation started....")
+
     val query_1_UDAF = "select class,mean_udaf(maths,class) from  student " +
       "group by class order by class"
     val query_1_Snappy = "select class,avg(maths) from student group by class order by class"
+
     val query_2_UDAF = "select class,mean_udaf(physics,class) from  student " +
       "group by class order by class"
     val query_2_Snappy = "select class,avg(physics) from student group by class order by class"
@@ -52,28 +75,65 @@ class UDAFValidation extends SnappySQLJob {
     snc.sql(createStudent)
     val sncDF1 = snc.sql(query_1_UDAF)
     val sncDF2 = snc.sql(query_1_Snappy)
-    validateResultSet(snc, sncDF1, sncDF2)
+    validateResultSet(snc, sncDF1, sncDF2, pw, query_1_UDAF)
     snc.sql(dropUDAF)
     snc.sql(createUDAF)
     val sncDF3 = snc.sql(query_2_UDAF)
     val sncDF4 = snc.sql(query_2_Snappy)
-    validateResultSet(snc, sncDF3, sncDF4)
+    validateResultSet(snc, sncDF3, sncDF4, pw, query_2_UDAF)
     snc.sql(dropUDAF)
+
+    try {
+      snc.sql(createUDAFWrongJarPath)
+    } catch {
+      case e : Exception => {
+        pw.println("Exception when wrong jar path given -> " + e.getMessage)
+        pw.println("*     *     *     *     *     *     *     *     *     *")
+        pw.flush()
+      }
+    }
+    snc.sql(dropUDAF)
+
+    try {
+      snc.sql(createUDAFWrongQualifiedClass)
+      snc.sql(query_1_UDAF)
+    } catch {
+      case e : Exception => {
+        pw.println("Exception when wrong qualified class path given -> " + e.getMessage)
+        pw.println("*     *     *     *     *     *     *     *     *     *")
+        pw.flush()
+      }
+    }
+    snc.sql(dropUDAF)
+
     snc.sql(dropStagingStudent)
     snc.sql(dropStudent)
+    pw.println("UDAF validation ends successfully...")
     println("UDAF validation ends successfully...")
+    pw.flush()
+    pw.close()
   }
 
-  def validateResultSet(snc : SnappyContext, sncDF1 : DataFrame, sncDF2 : DataFrame) : Unit = {
+  def validateResultSet(snc : SnappyContext, sncDF1 : DataFrame,
+                        sncDF2 : DataFrame, pw : PrintWriter, msg : String) : Unit = {
+    var isDiff1 = false
+    var isDiff2 = false
     val diff1 = sncDF1.except(sncDF2)
     val diff2 = sncDF2.except(sncDF1)
-    println(sncDF1.show())
-    println(sncDF2.show())
+    pw.println(sncDF1.show(10))
+    pw.println(sncDF2.show(10))
     if(diff1.count() > 0) {
       println("Difference found in sncDF2")
+      isDiff1 = true
     }
     if(diff2.count() > 0) {
       println("Difference found in sncDF1")
+      isDiff2 = true
     }
+    if(isDiff1 == false && isDiff2 == false) {
+      pw.println(msg + " is passed.")
+    }
+    pw.println("*     *     *     *     *     *     *     *     *     *")
+    pw.flush()
   }
 }
