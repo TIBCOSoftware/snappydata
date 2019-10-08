fd3bc06c1427d54889fa4996f1e897b3405e0b10
Neeraj Kumar <kneeraj@snappydata.io>
2019-10-08 19:35:22 +0530

    tmp change

--- cluster/src/main/scala/io/snappydata/gemxd/remotestore/RemoteDataService.scala
+++ cluster/src/main/scala/io/snappydata/gemxd/remotestore/RemoteDataService.scala
@@ -17,22 +17,91 @@
 
 package io.snappydata.gemxd.remotestore
 
-import com.gemstone.gemfire.internal.cache.DiskEntry
-import com.google.common.cache.CacheBuilder
+import java.io._
+import java.nio.file.{Files, Path, Paths}
+import java.util.concurrent.atomic.AtomicLong
 
-import scala.reflect.io.File
+import com.gemstone.gemfire.internal.cache.{DiskEntry, DiskStoreImpl}
+import com.google.common.cache.{Cache, CacheBuilder}
+import org.apache.spark.Logging
 
-class RemoteDataService {
+import scala.collection.mutable
+
+case class ExtendedStorageAttributes(ds: DiskStoreImpl,
+  extType: String, cacheDir: String, maxCacheSizeInMB: Long)
+
+final class RemoteDataService(extStorageAttrs: ExtendedStorageAttributes) extends Logging {
+
+  val ds: DiskStoreImpl = extStorageAttrs.ds
+  // TODO: Better toString
+  override def toString: String = s"${extStorageAttrs.extType}"
+
   private[this] lazy val entryCache = {
-    val x = CacheBuilder.newBuilder().maximumSize(10000).build[DiskEntry, File]()
+    // Keeping the cache size very big. Actual data cached will depend on the amount
+    // of disk space configured for keeping the cached data.
+    CacheBuilder.newBuilder().maximumSize(10000000).build[DiskEntry, String]()
   }
 
   lazy val client: RemoteDataClient = null
 
-  def getValues(diskptrs: Array[DiskEntry]): Array[Byte] = {
-    null
+  var currentCacheSize: AtomicLong = calculateInitialSizeUsed()
+
+  def calculateInitialSizeUsed(): AtomicLong = {
+    var sz = 0
+    var count = 0
+    val cacheDir = new File(extStorageAttrs.cacheDir)
+    for (file <- cacheDir.listFiles) {
+      // TODO: change when exact file pattern name is decided
+      if (file.isFile && file.getName.contains("oplog")) {
+        sz += file.length
+        count += 1
+      }
+    }
+    logDebug(s"cache directory has $count files of total size $sz for $this")
+    new AtomicLong(sz)
   }
+
+  // TODO: Remote Simultaneous access?? We can synchronize on filename or how?
+  def getValues(diskptrs: Array[DiskEntry]): Array[Array[Byte]] = diskptrs.map(ptr => {
+    val filename = RemoteDataService.getFileName(ds, ptr)
+    var cachedValFile = entryCache.getIfPresent(ptr)
+    var bytes: Array[Byte] = null
+    val len = ptr.getDiskId.getValueLength
+    if ( cachedValFile == null ) {
+      val offSet = ptr.getDiskId.getOffsetInOplog
+      val range = Array[Long](offSet, offSet + len)
+      bytes = client.getValues(filename, range)
+      // Write the bytes in the file and cache and update the cache
+      val bos = new BufferedOutputStream(new FileOutputStream(filename))
+      bos.write(bytes)
+      bos.close()
+      entryCache.put(ptr, filename)
+      currentCacheSize.addAndGet(bytes.size)
+    } else {
+      bytes = Files.readAllBytes(Paths.get(filename))
+    }
+    bytes
+  })
 }
 
-object RemoteDataService {
+object RemoteDataService extends Logging {
+
+  val remoteServices: mutable.Map[ExtendedStorageAttributes, RemoteDataService] =
+    new mutable.HashMap[ExtendedStorageAttributes, RemoteDataService]()
+
+  def apply(attrs: ExtendedStorageAttributes): RemoteDataService = {
+    synchronized(RemoteDataService.getClass) {
+      if (remoteServices.contains(attrs)) {
+        log.warn(s"Remote service for Disk Store ${attrs.ds} already initialized")
+        throw new RuntimeException(s"Remote service for Disk Store ${attrs.ds} already initialized")
+      }
+      val svc = new RemoteDataService(attrs)
+      logDebug(s"Created a remote service for Disk Store ${attrs.ds}")
+      remoteServices += (attrs -> svc)
+    }
+  }
+
+  def getFileName(ds: DiskStoreImpl, ptr: DiskEntry): String = {
+    null
+  }
 }
