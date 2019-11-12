235e22adbe7fcabc69fc873f6d48bff04d359c7e
Chandresh <cbhatt@snappydata.io>
2019-11-12 16:00:53 +0530

    change the NWSchemapath

--- dtests/src/test/scala/io/snappydata/hydra/hivemetastore/ExternalHiveMetaStore.scala
+++ dtests/src/test/scala/io/snappydata/hydra/hivemetastore/ExternalHiveMetaStore.scala
@@ -156,10 +156,10 @@ class ExternalHiveMetaStore extends SnappySQLJob {
     loadDataToHiveTbls(dataLocation + "orders.csv", "hive_orders", beelineClientConnection, schema)
     createHiveTable("hive_order_details(OrderID int,ProductID int,UnitPrice " +
       "double,Quantity smallint,Discount double)", beelineClientConnection, schema)
-//    loadDataToHiveTbls(dataLocation + "order_details.csv", "hive_order_details",
-//      beelineClientConnection, schema)
-    loadDataToHiveTbls(dataLocation + "order-details.csv", "hive_order_details",
+    loadDataToHiveTbls(dataLocation + "order_details.csv", "hive_order_details",
       beelineClientConnection, schema)
+//    loadDataToHiveTbls(dataLocation + "order-details.csv", "hive_order_details",
+//      beelineClientConnection, schema)
     createHiveTable("hive_products(ProductID int,ProductName string,SupplierID int," +
       "CategoryID int,QuantityPerUnit string,UnitPrice double,UnitsInStock smallint," +
       "UnitsOnOrder smallint,ReorderLevel smallint,Discontinued smallint)",
@@ -176,10 +176,10 @@ class ExternalHiveMetaStore extends SnappySQLJob {
       beelineClientConnection, schema)
     createHiveTable("hive_employee_territories(EmployeeID int," +
       "TerritoryID string)", beelineClientConnection, schema)
-//    loadDataToHiveTbls(dataLocation + "employee_territories.csv",
-//      "hive_employee_territories", beelineClientConnection, schema)
-    loadDataToHiveTbls(dataLocation + "employee-territories.csv",
+    loadDataToHiveTbls(dataLocation + "employee_territories.csv",
       "hive_employee_territories", beelineClientConnection, schema)
+//    loadDataToHiveTbls(dataLocation + "employee-territories.csv",
+//      "hive_employee_territories", beelineClientConnection, schema)
   }
 
   def createSnappyTblsAndLoadData(snc: SnappyContext, dataLocation: String,
