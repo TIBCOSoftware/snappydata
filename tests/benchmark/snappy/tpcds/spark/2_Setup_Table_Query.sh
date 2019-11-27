d8648886edb715ef48fb1b919e097b406ca490a0
Trilok Khairnar <tkhairnar@snappydata.io>
2019-11-27 15:28:36 +0000

    SNAP-3051: minor refactoring in tpc-ds scripts for spark

--- tests/benchmark/snappy/tpcds/spark/2_Setup_Table_Query.sh
+++ tests/benchmark/snappy/tpcds/spark/2_Setup_Table_Query.sh
@@ -5,15 +5,15 @@ source PerfRun.conf
 # Create slaves configuration files
 for element in "${slaves[@]}";
   do
-	echo $element >> $SnappyData/conf/slaves
+	echo $element >> $sparkHome/conf/slaves
   done
 echo "******************Created conf/slaves******************"
 
 #Start master and slaves from master machines
-ssh $master bash $SnappyData/sbin/start-all.sh
+ssh $master bash $sparkHome/sbin/start-all.sh
 
 #Execute Spark App
-bash $SnappyData/bin/spark-submit \
+bash $sparkHome/bin/spark-submit \
 --master spark://$master:7077 \
 --class io.snappydata.benchmark.snappy.tpcds.SparkApp \
 $appJar $sparkSqlProperties $dataDir $queries $queryPath $buckets_ColumnTable $ResultCollection $WarmupRuns $AverageRuns $cacheTables
