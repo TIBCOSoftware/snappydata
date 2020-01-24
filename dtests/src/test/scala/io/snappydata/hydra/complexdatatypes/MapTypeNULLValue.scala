559a4a3986876bc05046e5855cf3f439c9382bfc
Chandresh <cbhatt@snappydata.io>
2020-01-24 11:42:32 +0530

    Add the test case for NULL value in Map Type column

--- dtests/src/test/scala/io/snappydata/hydra/complexdatatypes/MapTypeNULLValue.scala
+++ dtests/src/test/scala/io/snappydata/hydra/complexdatatypes/MapTypeNULLValue.scala
@@ -17,7 +17,9 @@
 package io.snappydata.hydra.complexdatatypes
 
 import java.io.{File, FileOutputStream, PrintWriter}
+
 import com.typesafe.config.Config
+import io.snappydata.hydra.SnappyTestUtils
 import org.apache.spark.sql._
 
 class MapTypeNULLValue extends SnappySQLJob{
@@ -31,7 +33,7 @@ class MapTypeNULLValue extends SnappySQLJob{
     val sqlContext : SQLContext = spark.sqlContext
     val outputFile = "ValidateMapTypeNULLValue" + "_"  +
       System.currentTimeMillis() + jobConfig.getString("logFileName")
-    val pw : PrintWriter = new PrintWriter(new FileOutputStream(new File(outputFile), false)))
+    val pw : PrintWriter = new PrintWriter(new FileOutputStream(new File(outputFile), false))
 
     /**
       *  Test : NULL value in Map Type column.
@@ -42,14 +44,84 @@ class MapTypeNULLValue extends SnappySQLJob{
     /**
       * Test Case 1 : MapType Column is last column in the table.
       */
-
+      snc.sql(ComplexTypeUtils.createTableLastColumnMapType)
+      spark.sql(ComplexTypeUtils.createTableInSparkMapTypeLastColumn)
+      snc.sql(ComplexTypeUtils.insertNULLMapTypeLast)
+      spark.sql(ComplexTypeUtils.insertNULLMapTypeLast)
+      snc.sql(ComplexTypeUtils.insertNormalDataMapTypeLast)
+      spark.sql(ComplexTypeUtils.insertNormalDataMapTypeLast)
     /**
-      *  Test Case 2 : ArrayType Column is between (say middle)  the other data types in the table.
+      *  Test Case 2 : MapType Column is between (say middle)  the other data types in the table.
       */
-
-    /**
+      snc.sql(ComplexTypeUtils.createTableMiddleColumnMapType)
+      spark.sql(ComplexTypeUtils.createTableInSparkMapTypeMiddleColumn)
+      snc.sql(ComplexTypeUtils.insertNULLMapTypeMiddle)
+      spark.sql(ComplexTypeUtils.insertNULLMapTypeMiddle)
+      snc.sql(ComplexTypeUtils.insertNormalDataMapTypeMiddle)
+      spark.sql(ComplexTypeUtils.insertNormalDataMapTypeMiddle)
+     /**
       *  Test Case 3: ArrayType Column is the first column in the table.
       */
+      snc.sql(ComplexTypeUtils.createTableFirstColumnMapType)
+      spark.sql(ComplexTypeUtils.createTableInSparkMapTypeFirstColumn)
+      snc.sql(ComplexTypeUtils.insertNULLMapTypeFirst)
+      spark.sql(ComplexTypeUtils.insertNULLMapTypeFirst)
+      snc.sql(ComplexTypeUtils.insertNormalDataMapTypeFirst)
+      spark.sql(ComplexTypeUtils.insertNormalDataMapTypeFirst)
+    /**
+      * Validation Routine
+      */
+    val snappyDFLast = snc.sql(ComplexTypeUtils.selectMapLast)
+    val sparkDFLast = spark.sql(ComplexTypeUtils.selectMapLast)
+    val df1Last = snappyDFLast.collect()
+    val df2Last = sparkDFLast.collect()
+    var result1 = df1Last.mkString(",")
+    var result2 = df2Last.mkString(",")
+    pw.println(result1)
+    pw.println(result2)
+    if(df1Last.equals(df2Last)) {
+      pw.println("-- Insertion of NULL value in MapType last column OK --")
+    } else {
+      pw.println("-- Insertion of NULL value in MapType last column OK --")
+    }
+    pw.flush()
+    val snappyDFMiddle = snc.sql(ComplexTypeUtils.selectMapMiddle)
+    val sparkDFMiddle = spark.sql(ComplexTypeUtils.selectMapMiddle)
+    val df1Middle = snappyDFMiddle.collect()
+    val df2Middle = sparkDFMiddle.collect()
+    result1 = df1Middle.mkString(",")
+    result2 = df2Middle.mkString(",")
+    pw.println(result1)
+    pw.println(result2)
+    if(df1Middle.equals(df2Middle)) {
+      pw.println("-- Insertion of NULL value in MapType Middle column OK --")
+    } else {
+      pw.println("-- Insertion of NULL value in MapType Middle column OK --")
+    }
+    pw.flush()
+    val snappyDFFirst = snc.sql(ComplexTypeUtils.selectMapFirst)
+    val sparkDFFirst = spark.sql(ComplexTypeUtils.selectMapFirst)
+    val df1First = snappyDFFirst.collect()
+    val df2First = sparkDFFirst.collect()
+    result1 = df1First.mkString(",")
+    result2 = df2First.mkString(",")
+    pw.println(result1)
+    pw.println(result2)
+    if(df1First.equals(df2First)) {
+      pw.println("-- Insertion of NULL value in MapType First column OK --")
+    } else {
+      pw.println("-- Insertion of NULL value in MapType First column OK --")
+    }
+    pw.flush()
+    pw.close()
 
+    snc.sql(ComplexTypeUtils.dropTableMapLast)
+    snc.sql(ComplexTypeUtils.dropTableMapMiddle)
+    snc.sql(ComplexTypeUtils.dropTableMapFirst)
+    spark.sql(ComplexTypeUtils.dropTableMapLast)
+    spark.sql(ComplexTypeUtils.dropTableMapMiddle)
+    spark.sql(ComplexTypeUtils.dropTableMapFirst)
+    snc.sql(ComplexTypeUtils.dropDatabaseST)
+    spark.sql(ComplexTypeUtils.dropDatabaseST)
   }
 }
